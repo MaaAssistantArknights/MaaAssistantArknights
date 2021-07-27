539689d1b77a9a871a34aafe18d536b7d248ba92
MistEO <mistereo@hotmail.com>
2021-07-27 19:36:18 +0800

    添加第三方库到LFS，修改TestOcr项目

--- 3rdPart/include/OcrLiteNcnn/OcrUtils.h
+++ 3rdPart/include/OcrLiteNcnn/OcrUtils.h
@@ -1,8 +1,8 @@
 #ifndef __OCR_UTILS_H__
 #define __OCR_UTILS_H__
 
+#include <opencv2/core.hpp>
 #include "OcrStruct.h"
-#include <string>
 
 #include <sys/stat.h>
 /*#define __ENABLE_CONSOLE__ true
@@ -11,57 +11,56 @@
 }*/
 
 namespace ocr {
+    OCRLITE_EXPORT double getCurrentTime();
 
-    double getCurrentTime();
-
-    inline bool isFileExists(const std::string& name) {
+    OCRLITE_EXPORT inline bool isFileExists(const std::string& name) {
         struct stat buffer;
         return (stat(name.c_str(), &buffer) == 0);
     }
 
-    std::wstring strToWstr(std::string str);
+    OCRLITE_EXPORT std::wstring strToWstr(std::string str);
 
-    ScaleParam getScaleParam(cv::Mat& src, const float scale);
+    OCRLITE_EXPORT ScaleParam getScaleParam(cv::Mat& src, const float scale);
 
-    ScaleParam getScaleParam(cv::Mat& src, const int targetSize);
+    OCRLITE_EXPORT ScaleParam getScaleParam(cv::Mat& src, const int targetSize);
 
-    std::vector<cv::Point2f> getBox(const cv::RotatedRect& rect);
+    OCRLITE_EXPORT std::vector<cv::Point2f> getBox(const cv::RotatedRect& rect);
 
-    int getThickness(cv::Mat& boxImg);
+    OCRLITE_EXPORT int getThickness(cv::Mat& boxImg);
 
-    void drawTextBox(cv::Mat& boxImg, cv::RotatedRect& rect, int thickness);
+    OCRLITE_EXPORT void drawTextBox(cv::Mat& boxImg, cv::RotatedRect& rect, int thickness);
 
-    void drawTextBox(cv::Mat& boxImg, const std::vector<cv::Point>& box, int thickness);
+    OCRLITE_EXPORT void drawTextBox(cv::Mat& boxImg, const std::vector<cv::Point>& box, int thickness);
 
-    void drawTextBoxes(cv::Mat& boxImg, std::vector<TextBox>& textBoxes, int thickness);
+    OCRLITE_EXPORT void drawTextBoxes(cv::Mat& boxImg, std::vector<TextBox>& textBoxes, int thickness);
 
-    cv::Mat matRotateClockWise180(cv::Mat src);
+    OCRLITE_EXPORT cv::Mat matRotateClockWise180(cv::Mat src);
 
-    cv::Mat matRotateClockWise90(cv::Mat src);
+    OCRLITE_EXPORT cv::Mat matRotateClockWise90(cv::Mat src);
 
-    cv::Mat getRotateCropImage(const cv::Mat& src, std::vector<cv::Point> box);
+    OCRLITE_EXPORT cv::Mat getRotateCropImage(const cv::Mat& src, std::vector<cv::Point> box);
 
-    cv::Mat adjustTargetImg(cv::Mat& src, int dstWidth, int dstHeight);
+    OCRLITE_EXPORT cv::Mat adjustTargetImg(cv::Mat& src, int dstWidth, int dstHeight);
 
-    std::vector<cv::Point> getMinBoxes(const std::vector<cv::Point>& inVec, float& minSideLen, float& allEdgeSize);
+    OCRLITE_EXPORT std::vector<cv::Point> getMinBoxes(const std::vector<cv::Point>& inVec, float& minSideLen, float& allEdgeSize);
 
-    float boxScoreFast(const cv::Mat& inMat, const std::vector<cv::Point>& inBox);
+    OCRLITE_EXPORT float boxScoreFast(const cv::Mat& inMat, const std::vector<cv::Point>& inBox);
 
-    std::vector<cv::Point> unClip(const std::vector<cv::Point>& inBox, float perimeter, float unClipRatio);
+    OCRLITE_EXPORT std::vector<cv::Point> unClip(const std::vector<cv::Point>& inBox, float perimeter, float unClipRatio);
 
-    std::vector<int> getAngleIndexes(std::vector<Angle>& angles);
+    OCRLITE_EXPORT std::vector<int> getAngleIndexes(std::vector<Angle>& angles);
 
-    void saveImg(cv::Mat& img, const char* imgPath);
+    OCRLITE_EXPORT void saveImg(cv::Mat& img, const char* imgPath);
 
-    std::string getSrcImgFilePath(const char* path, const char* imgName);
+    OCRLITE_EXPORT std::string getSrcImgFilePath(const char* path, const char* imgName);
 
-    std::string getResultTxtFilePath(const char* path, const char* imgName);
+    OCRLITE_EXPORT std::string getResultTxtFilePath(const char* path, const char* imgName);
 
-    std::string getResultImgFilePath(const char* path, const char* imgName);
+    OCRLITE_EXPORT std::string getResultImgFilePath(const char* path, const char* imgName);
 
-    std::string getDebugImgFilePath(const char* path, const char* imgName, int i, const char* tag);
+    OCRLITE_EXPORT std::string getDebugImgFilePath(const char* path, const char* imgName, int i, const char* tag);
 
-    void printGpuInfo();
-
+    OCRLITE_EXPORT void printGpuInfo();
 }
+
 #endif //__OCR_UTILS_H__
