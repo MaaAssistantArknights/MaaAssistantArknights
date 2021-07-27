d3e66e09c0a40090fd6ff4ad49e5b2bdf91ed31b
MistEO <mistereo@hotmail.com>
2021-07-27 21:05:24 +0800

    更新OcrLite库，添加接口。删除多余的库文件

--- 3rdPart/include/OcrLiteNcnn/OcrLite.h
+++ 3rdPart/include/OcrLiteNcnn/OcrLite.h
@@ -31,6 +31,9 @@ namespace ocr {
             int padding, int maxSideLen,
             float boxScoreThresh, float boxThresh, float unClipRatio, bool doAngle, bool mostAngle);
 
+        OcrResult detect(const cv::Mat & mat,
+            int padding, int maxSideLen,
+            float boxScoreThresh, float boxThresh, float unClipRatio, bool doAngle, bool mostAngle);
     private:
         bool isOutputConsole = false;
         bool isOutputPartImg = false;
