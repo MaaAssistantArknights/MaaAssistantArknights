40cdee12e2042e37160ba39b3ef66fb342d241ce
MistEO <mistereo@hotmail.com>
2021-08-29 01:03:19 +0800

    重构Configer

---
+++ MeoAssistance/AbstractConfiger.cpp
@@ -0,0 +1,43 @@
+#include "AbstractConfiger.h"
+
+#include <memory>
+#include <sstream>
+#include <fstream>
+
+#include <json.h>
+
+#include "Logger.hpp"
+
+bool asst::AbstractConfiger::load(const std::string& filename)
+{
+	DebugTraceFunction;
+	DebugTrace("Configer::load | ", filename);
+
+	std::ifstream ifs(filename, std::ios::in);
+	if (!ifs.is_open()) {
+		return false;
+	}
+	std::stringstream iss;
+	iss << ifs.rdbuf();
+	ifs.close();
+	std::string content(iss.str());
+
+	auto&& ret = json::parser::parse(content);
+	if (!ret) {
+		DebugTrace("parse error", content);
+		return false;
+	}
+
+	json::value root = std::move(ret.value());
+
+	try {
+		parse(std::move(root));
+	}
+	catch (json::exception& e) {
+		DebugTraceError("Load config json error!", e.what());
+		return false;
+	}
+
+	DebugTrace("Load config succeed");
+	return true;
+}
--- MeoAssistance/InfrastConfiger.h
+++ MeoAssistance/InfrastConfiger.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "AbstractConfiger.h"
+
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -7,24 +9,23 @@
 #include "AsstDef.h"
 
 namespace asst {
-	class InfrastConfiger
+	class InfrastConfiger : public AbstractConfiger
 	{
 	public:
-		~InfrastConfiger() = default;
+		virtual ~InfrastConfiger() = default;
 
 		static InfrastConfiger& get_instance() {
 			static InfrastConfiger unique_instance;
 			return unique_instance;
 		}
 
-		bool load(const std::string& filename);
 		std::unordered_set<std::string> m_all_opers_name;				// 所有干员的名字
 		std::unordered_map<std::string, std::string> m_oper_name_feat;	// 根据关键字需要特征检测干员名：如果OCR识别到了key的内容但是却没有value的内容，则进行特征检测进一步确认
 		std::unordered_set<std::string> m_oper_name_feat_whatever;		// 无论如何都进行特征检测的干员名
 
 		std::unordered_map<std::string, std::vector<std::vector<OperInfrastInfo>>> m_infrast_combs;		// 各个设施内的可能干员组合
 
-	private:
+	protected:
 		InfrastConfiger() = default;
 		InfrastConfiger(const InfrastConfiger& rhs) = default;
 		InfrastConfiger(InfrastConfiger&& rhs) noexcept = default;
@@ -32,6 +33,6 @@ namespace asst {
 		InfrastConfiger& operator=(const InfrastConfiger& rhs) = default;
 		InfrastConfiger& operator=(InfrastConfiger&& rhs) noexcept = default;
 
-		bool _load(const std::string& filename);
+		virtual bool parse(json::value&& json) override;
 	};
 }
