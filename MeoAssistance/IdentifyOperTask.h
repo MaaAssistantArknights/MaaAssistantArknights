3af0a23a28d8c8b4f2dd844beecaae5c8ef033f0
MistEO <mistereo@hotmail.com>
2021-08-29 03:36:05 +0800

    完成干员识别读写配置文件

--- MeoAssistance/IdentifyOperTask.h
+++ MeoAssistance/IdentifyOperTask.h
@@ -11,27 +11,26 @@ namespace asst {
 		virtual ~IdentifyOperTask() = default;
 
 		virtual bool run() override;
-		//virtual void set_swipe_param(Rect begin, Rect end, int duration) {
-		//	m_swipe_begin = std::move(begin);
-		//	m_swipe_end = std::move(end);
-		//	m_swipe_duration = duration;
-		//}
-		void set_filename(const std::string& filename) {
-			m_filename = filename;
-		}
+
 	protected:
-		constexpr static int SwipeExtraDelay = 0;
+		constexpr static int SwipeDuration = 2000;
+		constexpr static int SwipeExtraDelay = 1000;
+
+		std::optional<std::unordered_set<OperInfrastInfo>> swipe_and_detect();
 
 		std::vector<TextArea> detect_opers(const cv::Mat& image,
 			std::unordered_map<std::string, std::string>& feature_cond,
 			std::unordered_set<std::string>& feature_whatever);
 		int detect_elite(const cv::Mat& image, const asst::Rect name_rect);
 
-		virtual bool swipe(bool reverse = false);
-		virtual bool keep_swipe(bool reverse = false);
+		bool swipe(bool reverse = false);
+		bool keep_swipe(bool reverse = false);
 
-		int m_swipe_duration = 2000;
-		bool m_keep_swipe = false;
-		std::string m_filename;
+		double m_cropped_height_ratio = 0;	// 图片裁剪出干员名的长条形图片 的高度比例（相比原图）
+		double m_cropped_upper_y_ratio = 0;	// 图片裁剪出干员名的长条形图片，上半部分干员名的裁剪区域y坐标比例（相比原图）
+		double m_cropped_lower_y_ratio = 0;	// 图片裁剪出干员名的长条形图片，下半部分干员名的裁剪区域y坐标比例（相比原图）
+		Rect m_swipe_begin;			// 边滑动边识别，单次滑动起始点（Rect内随机点）
+		Rect m_swipe_end;			// 边滑动边识别，单次滑动结束点（Rect内随机点）
+		bool m_keep_swipe = false;	// keep_swipe函数是否结束的标志位
 	};
 }
--- MeoAssistance/InfrastConfiger.h
+++ MeoAssistance/InfrastConfiger.h
@@ -23,7 +23,7 @@ namespace asst {
 		std::unordered_map<std::string, std::string> m_oper_name_feat;	// 根据关键字需要特征检测干员名：如果OCR识别到了key的内容但是却没有value的内容，则进行特征检测进一步确认
 		std::unordered_set<std::string> m_oper_name_feat_whatever;		// 无论如何都进行特征检测的干员名
 
-		std::unordered_map<std::string, std::vector<std::vector<OperInfrastInfo>>> m_infrast_combs;		// 各个设施内的可能干员组合
+		std::unordered_map<std::string, std::vector<std::vector<OperInfrastInfo>>> m_infrast_combs;		// 各个设施内的可能干员组合，注意值是vector，是有序的
 
 	protected:
 		InfrastConfiger() = default;
