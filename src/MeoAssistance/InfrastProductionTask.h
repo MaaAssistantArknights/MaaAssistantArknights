2b97fd26ac2e4f86dd7d4021efb086fc2213596c
MistEO <mistereo@hotmail.com>
2021-09-13 21:33:00 +0800

    优化生产设施换班流程，现在一类设施仅识别一次，不重复识别

--- src/MeoAssistance/InfrastProductionTask.h
+++ src/MeoAssistance/InfrastProductionTask.h
@@ -18,14 +18,15 @@ namespace asst {
 		}
 	protected:
 		constexpr static int SwipeMaxTimes = 17;
-		constexpr static int MaxOpers = 3;	// 单个制造站/贸易站最大干员数
+		constexpr static int MaxOperNumber = 3;		// 单个制造站/贸易站最大干员数
+		constexpr static int MaxStationNumber = 5;	// 设施最大数量（基建里最多只有5个制造站）
 
 		// 一边滑动一边识别
 		std::optional<std::unordered_map<std::string, OperInfrastInfo>> swipe_and_detect();
 		// 计算最优解干员组合
-		std::list<std::string> calc_optimal_comb(const std::unordered_map<std::string, OperInfrastInfo>& cur_opers_info) const;
+		std::vector<std::string> calc_optimal_comb(const std::unordered_map<std::string, OperInfrastInfo>& cur_opers_info) const;
 		// 一边滑动一边识别并点击干员名
-		bool swipe_and_select(std::list<std::string>& name_comb, int swipe_max_times = SwipeMaxTimes);
+		bool swipe_and_select(const std::vector<std::string>& name_comb, int swipe_max_times = SwipeMaxTimes);
 
 		std::string m_facility;	// 设施名（制造站、贸易站、控制中枢……）
 		std::unordered_map<std::string, OperInfrastInfo> m_all_opers_info;
