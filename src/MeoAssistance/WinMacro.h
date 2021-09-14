3854b059f0884eeefbe9b8b3e00c7208f9b27f51
MistEO <mistereo@hotmail.com>
2021-09-15 01:48:18 +0800

    初步搭建异步adb框架

--- src/MeoAssistance/WinMacro.h
+++ src/MeoAssistance/WinMacro.h
@@ -4,6 +4,13 @@
 #include <random>
 #include <optional>
 
+#include <memory>	// for pimpl
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <queue>
+#include <condition_variable>
+
 #include <Windows.h>
 
 #include "AsstDef.h"
@@ -17,7 +24,7 @@ namespace asst {
 	{
 	public:
 		WinMacro(const EmulatorInfo& info);
-		~WinMacro() = default;
+		~WinMacro();
 
 		bool captured() const noexcept;
 		bool show_window();
@@ -36,9 +43,24 @@ namespace asst {
 
 		const std::string ScreenFilename;
 	private:
-		bool find_handle();
+		void pipe_working_proc();
+		bool find_handle(); // TODO，可以考虑也做成异步的
 		std::optional<std::string> call_command(const std::string& cmd, bool use_pipe = true);
 		Point rand_point_in_rect(const Rect& rect);
+
+		int push_cmd(const std::string& cmd);
+		void wait(unsigned id);
+
+		bool m_thread_exit = false;
+		std::thread m_cmd_thread;
+		std::mutex m_cmd_queue_mutex;
+		std::condition_variable m_cmd_condvar;
+		std::queue<std::string> m_cmd_queue;
+		std::atomic<unsigned> m_completed_id = 0;
+		unsigned m_push_id = 0;						// push_id的自增总是伴随着queue的push，肯定是要上锁的，所以没必要原子
+
+		std::shared_mutex m_image_mutex;
+		std::shared_ptr<cv::Mat> m_cache_image;
 
 		EmulatorInfo m_emulator_info;
 		HWND m_handle = nullptr;
