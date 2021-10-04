3395e087c31c1350b6f1ab7782a18ee48356511e
MistEO <mistereo@hotmail.com>
2021-10-04 18:05:56 +0800

    feat.支持企鹅物流api的关卡json转识别用的关卡json

--- src/MeoAssistance/Identify.cpp
+++ src/MeoAssistance/Identify.cpp
@@ -7,6 +7,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/types_c.h>
 
+#include <json.h>
+
 namespace penguin {
 #include <penguin-stats-recognize/penguin_wasm.h>
 }
@@ -254,15 +256,14 @@ std::vector<TextArea> asst::Identify::find_text(const cv::Mat& image, const std:
 
 bool asst::Identify::penguin_load_server(const std::string& server)
 {
+    m_penguin_server = server;
     penguin::load_server(server.c_str());
     return true;
 }
 
 bool asst::Identify::penguin_load_json(const std::string& stage_path, const std::string& hash_path)
 {
-    // TODO：文件检查
-
-    auto load_json_string = [](const std::string& path) -> std::string {
+    auto load_file = [](const std::string& path) -> std::string {
         std::ifstream ifs(path, std::ios::in);
         if (!ifs.is_open()) {
             return std::string();
@@ -273,7 +274,45 @@ bool asst::Identify::penguin_load_json(const std::string& stage_path, const std:
         return iss.str();
     };
 
-    penguin::load_json(load_json_string(stage_path).c_str(), load_json_string(hash_path).c_str());
+    auto stage_parse_ret = json::parse(load_file(stage_path));
+    if (!stage_parse_ret) {
+        return false;
+    }
+    // stage_json 来自 https://penguin-stats.io/PenguinStats/api/v2/stages
+    // 和接口需要的json有点区别，这里做个转换
+    json::value stage_json = std::move(stage_parse_ret.value());
+    json::object cvt_stage_json;
+    try {
+        for (const json::value& stage_info : stage_json.as_array()) {
+            if (!stage_info.exist("dropInfos")) {   // 这种一般是以前的活动关，现在已经关闭了的
+                continue;
+            }
+            std::string key = stage_info.at("code").as_string();
+            json::value stage_dst;
+            stage_dst["stageId"] = stage_info.at("stageId");
+            std::vector<json::value> drops_vector;
+            for (const json::value& drop_info : stage_info.at("dropInfos").as_array()) {
+                if (drop_info.exist("itemId")) {
+                    if (drop_info.at("dropType").as_string() == "FURNITURE") {  // 幸运掉落，家具啥的，企鹅物流不接，忽略掉
+                        continue;
+                    }
+                    drops_vector.emplace_back(drop_info.at("itemId"));
+                }
+            }
+            stage_dst["drops"] = json::array(std::move(drops_vector));
+            std::string server = m_penguin_server.empty() ? "CN" : m_penguin_server;
+            stage_dst["existence"] = stage_info.at("existence").at(server).at("exist");
+
+            cvt_stage_json.emplace(std::move(key), std::move(stage_dst));
+        }
+    }
+    catch (json::exception& e) {
+        DebugTraceError(stage_path, "parse error", e.what());
+        return false;
+    }
+    std::string cvt_stage_string = cvt_stage_json.to_string();
+    //DebugTrace("stage_index | ", cvt_stage_string);
+    penguin::load_json(cvt_stage_string.c_str(), load_file(hash_path).c_str());
     return true;
 }
 
@@ -293,7 +332,9 @@ std::string asst::Identify::penguin_recognize(const cv::Mat& image)
 {
     std::vector<uchar> buf;
     cv::imencode(".png", image, buf);
-    return penguin::recognize(buf.data(), buf.size());
+    std::string res = penguin::recognize(buf.data(), buf.size());
+    DebugTrace("penguin_recognize | ", res);
+    return res;
 }
 
 /*
