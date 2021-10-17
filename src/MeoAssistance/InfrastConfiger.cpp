a6c91898daa6992ff56d9d28d60c6829989e6a9c
MistEO <mistereo@hotmail.com>
2021-10-17 20:46:31 +0800

    fix.修复基建同类技能识别不准确的问题（例如标准化b识别成了标准化a）

--- src/MeoAssistance/InfrastConfiger.cpp
+++ src/MeoAssistance/InfrastConfiger.cpp
@@ -9,10 +9,12 @@ bool asst::InfrastConfiger::parse(const json::value& json)
         std::vector<InfrastSkill> facility_skills;
         for (const json::value& skill_json : json.at(facility_name).at("skills").as_array()) {
             InfrastSkill skill;
-            skill.templ_name = skill_json.at("template").as_string();
+            std::string templ_name = skill_json.at("template").as_string();
+            skill.templ_name = templ_name;
             m_templ_required.emplace(skill.templ_name);
-
-            std::string id = skill_json.get("id", skill.templ_name);
+            // 默认id就是模板名（去掉格式）
+            std::string defalut_id = templ_name.substr(0, templ_name.find_last_of('.'));
+            std::string id = skill_json.get("id", defalut_id);
             skill.id = id;
             if (skill_json.exist("name")) {
                 for (const json::value& skill_names : skill_json.at("name").as_array()) {
--- src/MeoAssistance/InfrastSkillsImageAnalyzer.h
+++ src/MeoAssistance/InfrastSkillsImageAnalyzer.h
@@ -2,6 +2,7 @@
 #include "AbstractImageAnalyzer.h"
 
 #include "AsstDef.h"
+#include "InfrastConfiger.h"
 
 namespace asst {
     class InfrastSkillsImageAnalyzer : public AbstractImageAnalyzer
@@ -11,13 +12,18 @@ namespace asst {
         virtual ~InfrastSkillsImageAnalyzer() = default;
         virtual bool analyze() override;
 
+        const std::vector<std::vector<InfrastSkill>>& get_result() const noexcept {
+            return m_skills_result;
+        }
+
         constexpr static int MaxNumOfSkills = 2;    // 单个干员最多有几个基建技能
     protected:
         bool skills_detect();        // 检测出所有技能区域
         bool skills_split();         // 拆分成一个一个的区域
         bool skill_analyze();        // 识别每个技能区域是啥
 
-        std::vector<Rect> m_skills_detected;                 // skills_detect()的结果，每个Rect是单个干员的全部技能
-        std::vector<std::vector<Rect>> m_skills_splited;     // skills_split()的结果，每个Rect是单个技能
+        std::vector<Rect> m_skills_detected;                    // skills_detect()的结果，每个Rect是单个干员的全部技能
+        std::vector<std::vector<Rect>> m_skills_splited;        // skills_split()的结果，每个Rect是单个技能
+        std::vector<std::vector<InfrastSkill>> m_skills_result; // skill_analyze()的结果，最终结果
     };
 }
