d69626aacec36fdcad021a3de2c74ce1481ee0a1
MistEO <mistereo@hotmail.com>
2021-10-23 18:46:56 +0800

    fix.尝试修复界面启动时崩溃的问题

--- src/MeoAsstGui/ViewModels/RootViewModel.cs
+++ src/MeoAsstGui/ViewModels/RootViewModel.cs
@@ -17,7 +17,17 @@ namespace MeoAsstGui
 
         protected override void OnViewLoaded()
         {
-            CheckAndUpdateNow();
+            // 有个界面一启动就崩溃的问题，暂时找不到原因，而且日志也没生成
+            // 因为下一行就有日志生成了，所以可能是这里崩的
+            // 反正先try起来看看有没有用_(:з」∠)_
+            try
+            {
+                CheckAndUpdateNow();
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine("CheckAndUpdateNow Exception caught: {0}", e);
+            }
             InitProxy();
             InitViewModels();
             ShowUpdateOrDownload();
