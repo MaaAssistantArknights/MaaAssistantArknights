d35cbfa8e3403b24758d89c6f1c796488150a566
MistEO <mistereo@hotmail.com>
2021-10-28 22:38:15 +0800

    feat.完成办公室换班流程

---
+++ src/MeoAssistance/InfrastOfficeTask.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "InfrastProductionTask.h"
+
+namespace asst {
+    class InfrastOfficeTask : public InfrastProductionTask
+    {
+    public:
+        using InfrastProductionTask::InfrastProductionTask;
+        virtual ~InfrastOfficeTask() = default;
+        virtual bool run() override;
+
+        const static std::string FacilityName;
+        const static int MaxNumOfOpers = 1;
+    private:
+    };
+}
