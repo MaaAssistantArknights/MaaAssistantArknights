33f7e677d72f77d1f03ac41db0468eb56a65fc2d
MistEO <mistereo@hotmail.com>
2021-11-07 23:31:54 +0800

    chore.修改版本号，更新截图的脚本

--- tools/screen.bat
+++ tools/screen.bat
@@ -1,3 +1,5 @@
-adb connect 127.0.0.1:5555
-adb -s 127.0.0.1:5555 shell screencap /sdcard/screen.png
-adb -s 127.0.0.1:5555 pull /sdcard/screen.png .
+﻿set port=5555
+
+adb connect 127.0.0.1:%port%
+adb -s 127.0.0.1:%port% shell screencap /sdcard/screen.png
+adb -s 127.0.0.1:%port% pull /sdcard/screen.png .
