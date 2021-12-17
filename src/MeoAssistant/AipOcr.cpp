a2c49d2febb4f72f70fdb542f6f9528fd72fa934
MistEO <mistereo@hotmail.com>
2021-12-17 21:20:40 +0800

    feat.暂时废弃百度 OCR API

--- src/MeoAssistant/AipOcr.cpp
+++ src/MeoAssistant/AipOcr.cpp
@@ -8,6 +8,7 @@
 
 #include "AsstUtils.hpp"
 #include "Logger.hpp"
+#include "Resource.h"
 
 bool asst::AipOcr::request_access_token(const std::string& client_id, const std::string& client_secret)
 {
@@ -16,7 +17,7 @@ bool asst::AipOcr::request_access_token(const std::string& client_id, const std:
     m_access_token.clear();
 
     std::string_view cmd_fmt =
-        R"(curl -k \"https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s\")";
+        R"(curl -s -k "https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s")";
 
     constexpr int cmd_len = 256;
     char cmd[cmd_len] = { 0 };
@@ -42,22 +43,43 @@ bool asst::AipOcr::request_access_token(const std::string& client_id, const std:
     return true;
 }
 
-std::vector<asst::TextRect> asst::AipOcr::request_ocr_accurate(const cv::Mat& image, const TextRectProc& pred)
+bool asst::AipOcr::request_ocr_general(const cv::Mat& image, std::vector<TextRect>& out_result, const TextRectProc& pred)
 {
     LogTraceFunction;
 
-    std::string_view cmd_fmt =
-        R"(curl -k \"https://aip.baidubce.com/rest/2.0/ocr/v1/accurate?access_token=%s\" --data \"image=%s\" -H \"Content-Type:application/x-www-form-urlencoded\")";
-
-    return request_ocr_and_parse(cmd_fmt, image, pred);
-}
-
-std::vector<asst::TextRect> asst::AipOcr::request_ocr_and_parse(std::string_view cmd_fmt, const cv::Mat& image, const TextRectProc& pred)
-{
     if (m_access_token.empty()) {
-        return std::vector<asst::TextRect>();
+        auto& cfg = Resrc.cfg().get_options().aip_ocr;
+        request_access_token(cfg.client_id, cfg.client_secret);
     }
 
+    std::string_view cmd_fmt =
+        R"(curl -s -k "https://aip.baidubce.com/rest/2.0/ocr/v1/general?access_token=%s" --data "image=%s" -H "Content-Type:application/x-www-form-urlencoded")";
+
+    return request_ocr_and_parse(cmd_fmt, image, out_result, pred);
+}
+
+bool asst::AipOcr::request_ocr_accurate(const cv::Mat& image, std::vector<TextRect>& out_result, const TextRectProc& pred)
+{
+    LogTraceFunction;
+
+    if (m_access_token.empty()) {
+        auto& cfg = Resrc.cfg().get_options().aip_ocr;
+        request_access_token(cfg.client_id, cfg.client_secret);
+    }
+
+    std::string_view cmd_fmt =
+        R"(curl -s -k "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate?access_token=%s" --data "image=%s" -H "Content-Type:application/x-www-form-urlencoded")";
+
+    return request_ocr_and_parse(cmd_fmt, image, out_result, pred);
+}
+
+bool asst::AipOcr::request_ocr_and_parse(std::string_view cmd_fmt, const cv::Mat& image, std::vector<TextRect>& out_result, const TextRectProc& pred)
+{
+    if (m_access_token.empty()) {
+        return false;
+    }
+    // CreateProcess 最大只能接受 32767 chars，一张图片随便就超过了
+    // 这个功能暂时没法用，得集成下 libcurl 或者别的
     std::vector<uchar> buf;
     cv::imencode(".png", image, buf);
     auto* enc_msg = reinterpret_cast<unsigned char*>(buf.data());
@@ -66,7 +88,7 @@ std::vector<asst::TextRect> asst::AipOcr::request_ocr_and_parse(std::string_view
     size_t cmd_len = encoded.size() + cmd_fmt.size() + m_access_token.size();
     char* cmd = new char[cmd_len];
     memset(cmd, 0, cmd_len);
-    sprintf_s(cmd, cmd_len, cmd_fmt.data(), encoded.c_str(), m_access_token.c_str());
+    sprintf_s(cmd, cmd_len, cmd_fmt.data(), m_access_token.c_str(), encoded.c_str());
 
     Log.trace("call cmd:", cmd);
     std::string response = utils::callcmd(cmd);
@@ -75,21 +97,21 @@ std::vector<asst::TextRect> asst::AipOcr::request_ocr_and_parse(std::string_view
 
     auto parse_res = json::parse(response);
     if (!parse_res) {
-        return std::vector<asst::TextRect>();
+        return false;
     }
     try {
         auto json = parse_res.value();
-        return parse_response(json);
+        return parse_response(json, out_result, pred);
     }
     catch (...) {
-        return std::vector<asst::TextRect>();
+        return false;
     }
 }
 
-std::vector<asst::TextRect> asst::AipOcr::parse_response(const json::value& json, const TextRectProc& pred)
+bool asst::AipOcr::parse_response(const json::value& json, std::vector<TextRect>& out_result, const TextRectProc& pred)
 {
     if (!json.exist("words_result")) {
-        return std::vector<asst::TextRect>();
+        return false;
     }
 
     std::vector<TextRect> result;
@@ -115,7 +137,9 @@ std::vector<asst::TextRect> asst::AipOcr::parse_response(const json::value& json
         }
     }
 
-    Log.trace("OcrPack::recognize | raw : ", log_str_raw);
-    Log.trace("OcrPack::recognize | proc : ", log_str_proc);
-    return result;
+    Log.trace("AipOcr::parse_response | raw : ", log_str_raw);
+    Log.trace("AipOcr::parse_response | proc : ", log_str_proc);
+
+    out_result = result;
+    return true;
 }
--- src/MeoAssistant/AipOcr.h
+++ src/MeoAssistant/AipOcr.h
@@ -20,22 +20,23 @@ namespace asst
     public:
         ~AipOcr() = default;
 
-        AipOcr& get_instance()
+        static AipOcr& get_instance()
         {
             static AipOcr unique_instance;
             return unique_instance;
         }
 
         bool request_access_token(const std::string& client_id, const std::string& client_secret);
-        std::vector<TextRect> request_ocr_accurate(const cv::Mat& image, const TextRectProc& pred = nullptr);
+        bool request_ocr_general(const cv::Mat& image, std::vector<TextRect>& out_result, const TextRectProc& pred = nullptr);
+        bool request_ocr_accurate(const cv::Mat& image, std::vector<TextRect>& out_result, const TextRectProc& pred = nullptr);
 
     private:
         AipOcr() = default;
         AipOcr(const AipOcr&) = default;
         AipOcr(AipOcr&&) noexcept = default;
 
-        std::vector<TextRect> request_ocr_and_parse(std::string_view cmd_fmt, const cv::Mat& image, const TextRectProc& pred = nullptr);
-        std::vector<TextRect> parse_response(const json::value& json, const TextRectProc& pred = nullptr);
+        bool request_ocr_and_parse(std::string_view cmd_fmt, const cv::Mat& image, std::vector<TextRect>& out_result, const TextRectProc& pred = nullptr);
+        bool parse_response(const json::value& json, std::vector<TextRect>& out_result, const TextRectProc& pred = nullptr);
 
         std::string m_access_token;
     };
