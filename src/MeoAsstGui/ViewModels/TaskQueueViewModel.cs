36d37c1683ec5901d66194c7cd10b704cb57e91c
MistEO <mistereo@hotmail.com>
2021-12-21 22:11:52 +0800

    feat.选关新增支持CA5

--- src/MeoAsstGui/ViewModels/TaskQueueViewModel.cs
+++ src/MeoAsstGui/ViewModels/TaskQueueViewModel.cs
@@ -66,7 +66,7 @@ namespace MeoAsstGui
             StageList.Add(new CombData { Display = "龙门币-5", Value = "CE-5" });
             StageList.Add(new CombData { Display = "红票-5", Value = "AP-5" });
             StageList.Add(new CombData { Display = "狗粮-5", Value = "LS-5" });
-            //StageList.Add(new CombData { Display = "技能-5", Value = "CA-5" });
+            StageList.Add(new CombData { Display = "技能-5", Value = "CA-5" });
         }
 
         public void AddLog(string content, string color = "Gray", string weight = "Regular")
