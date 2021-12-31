edb0a0f7c799300efb0f5d88e23221c1a93f2ec3
MistEO <mistereo@hotmail.com>
2021-12-31 21:21:50 +0800

    perf.优化任务 sleep 的实现

--- src/MeoAssistant/AbstractTask.cpp
+++ src/MeoAssistant/AbstractTask.cpp
@@ -56,8 +56,8 @@ bool AbstractTask::sleep(unsigned millisecond)
 
     while (!need_exit() && duration < millisecond) {
         duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::steady_clock::now() - start)
-            .count();
+            std::chrono::steady_clock::now() - start).count();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         std::this_thread::yield();
     }
     m_callback(AsstMsg::EndOfSleep, callback_json, m_callback_arg);
