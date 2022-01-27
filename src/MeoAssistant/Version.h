1c6d57ba78ec1eabfd6fd6ff20c13f68d89d022c
MistEO <mistereo@hotmail.com>
2022-01-28 01:07:40 +0800

    feat.完成自动肉鸽的界面

--- src/MeoAssistant/Version.h
+++ src/MeoAssistant/Version.h
@@ -2,5 +2,5 @@
 
 namespace asst
 {
-    constexpr static const char* Version = "v2.9.0-beta";
+    constexpr static const char* Version = "v2.9.1";
 }
---
+++ src/MeoAsstGui/UserControl/RogueliekSettingsUserControl.xaml.cs
@@ -0,0 +1,26 @@
+// MeoAsstGui - A part of the MeoAssistantArknights project
+// Copyright (C) 2021 MistEO and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+
+using System.Windows.Controls;
+
+namespace MeoAsstGui
+{
+    /// <summary>
+    /// InfrastSettingsUserContril.xaml 的交互逻辑
+    /// </summary>
+    public partial class RoguelikeSettingsUserControl : UserControl
+    {
+        public RoguelikeSettingsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
