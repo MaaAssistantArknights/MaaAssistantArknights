acbdbff9f4f02a3bce3d21ee1a4e2698c7d34e29
zhangweijian97 <zhangweijian97@outlook.com>
2022-02-19 00:40:06 +0800

    chore.整理python定时demo

--- src/Python/timed_start/timed_sample.py
+++ src/Python/timed_start/timed_sample.py
@@ -1,10 +1,14 @@
-from operator import ne
+"""
+function: 定时启动 maa
+author: Black Cat Bon
+version: v2.0.1
+"""
+
 import threading
 import time
 import schedule
 
 import datetime
-from threading import Timer
 import json
 import pathlib
 import sys
@@ -12,23 +16,26 @@ import re
 
 from interface import Asst, Message
 
+
 def get_now_time():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 def job():
     # print("I'm running on thread %s" % threading.current_thread())
     print("现在时间：" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-    # asst.append_fight("LastBattle", 0, 0, 999)
-    # asst.append_recruit(3, [4, 5], [3, 4, 5], True, False)
-    # asst.append_infrast(1, ["Mfg", "Trade", "Control",
-    #                     "Power", "Reception", "Office", "Dorm"], "Money", 0.1)
-    # asst.append_visit()
-    # asst.append_mall(True)
+    asst.append_fight("LastBattle", 0, 0, 999)
+    asst.append_recruit(3, [4, 5], [3, 4, 5], True, False)
+    asst.append_infrast(1, ["Mfg", "Trade", "Control",
+                        "Power", "Reception", "Office", "Dorm"], "Money", 0.1)
+    asst.append_visit()
+    asst.append_mall(True)
     asst.append_award()
-    asst.set_penguin_id('18688523')
+    # asst.set_penguin_id('1234567')
 
     asst.start()
+
 
 def run_threaded(job_func):
     job_thread = threading.Thread(target=job_func)
@@ -41,10 +48,11 @@ def get_target_time(input_time):
         target_time = input_time
     else:
         while True:
-            input_str = input('Invalid time format, please input again!\n For example, 20:00 \n> ')
+            input_str = input(
+                'Invalid time format, please input again!\n For example, 20:00 \n> ')
             if (input_str == 'exit()'):
                 sys.exit()
-            
+
             matchObj = re.match('\d\d:\d\d', input_str)
 
             if matchObj:
@@ -56,16 +64,18 @@ def get_target_time(input_time):
 
     return target_time
 
+
 def get_target_port(input_port):
     matchObj = re.match('\d\d\d\d', input_port)
     if matchObj:
         target_time = input_port
     else:
         while True:
-            input_str = input('Invalid port format, please input again!\n For example, 5565 \n> ')
+            input_str = input(
+                'Invalid port format, please input again!\n For example, 5565 \n> ')
             if (input_str == 'exit()'):
                 sys.exit()
-            
+
             matchObj = re.match('\d\d\d\d', input_str)
 
             if matchObj:
@@ -88,18 +98,17 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         target_time = get_target_time(sys.argv[1])
         port = '5555'
-    elif len(sys.argv) == 3: 
+    elif len(sys.argv) == 3:
         target_time = get_target_time(sys.argv[1])
         port = get_target_port(sys.argv[2])
 
-    # 声明asst
+    # asst 的回调
     @Asst.CallBackType
     def my_callback(msg, details, arg):
         m = Message(msg)
         d = json.loads(details.decode('utf-8'))
 
         print(m, d, arg)
-
 
     dirname: str = (pathlib.Path.cwd()).__str__()
     asst = Asst(dirname=dirname, callback=my_callback)
@@ -118,6 +127,6 @@ if __name__ == "__main__":
 
     # print('下次定时任务开始于：' + target_time)
 
-    while 1:
+    while True:
         schedule.run_pending()
         time.sleep(1)
