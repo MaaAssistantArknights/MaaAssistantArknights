e49c3ee367c51b4e80475afabc8e09585aaf3cb9
MistEO <mistereo@hotmail.com>
2022-03-28 22:32:57 +0800

    fix.修复自动抄作业的一些bug

--- src/MeoAssistant/AbstractImageAnalyzer.cpp
+++ src/MeoAssistant/AbstractImageAnalyzer.cpp
@@ -2,6 +2,7 @@
 
 #include "AsstUtils.hpp"
 #include "Controller.h"
+#include "Logger.hpp"
 
 asst::AbstractImageAnalyzer::AbstractImageAnalyzer(const cv::Mat image)
     : m_image(image), m_roi(empty_rect_to_full(Rect(), image))
@@ -43,5 +44,28 @@ void asst::AbstractImageAnalyzer::set_roi(const Rect& roi) noexcept
 
 asst::Rect asst::AbstractImageAnalyzer::empty_rect_to_full(const Rect& rect, const cv::Mat image) noexcept
 {
-    return rect.empty() ? Rect(0, 0, image.cols, image.rows) : rect;
+    if (rect.empty()) {
+        return Rect(0, 0, image.cols, image.rows);
+    }
+    else {
+        Rect res = rect;
+        if (image.cols < res.x) {
+            Log.error("roi is out of range", image.cols, image.rows, res.to_string());
+            res.x = image.cols - res.width;
+        }
+        if (image.rows < res.y) {
+            Log.error("roi is out of range", image.cols, image.rows, res.to_string());
+            res.y = image.rows - res.height;
+        }
+
+        if (image.cols < res.x + res.width) {
+            Log.error("roi is out of range", res.to_string());
+            res.width = image.cols - res.x;
+        }
+        if (image.rows < res.y + res.height) {
+            Log.error("roi is out of range", res.to_string());
+            res.height = image.rows - res.y;
+        }
+        return res;
+    }
 }
