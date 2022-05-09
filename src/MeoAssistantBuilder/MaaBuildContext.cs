4de65cc66ec0ac4b88d684fdb1e0bf5743f50393
Liam Sho <conan_creeper@outlook.com>
2022-05-09 13:05:46 +0800

    feat.添加构建项目

---
+++ src/MeoAssistantBuilder/MaaBuildContext.cs
@@ -0,0 +1,28 @@
+using Cake.Common;
+using Cake.Common.Tools.VSWhere;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Frosting;
+
+namespace MeoAssistantBuilder;
+
+public class MaaBuildContext : FrostingContext
+{
+    public string MsBuildExecutable { get; set; }
+    
+    public MaaBuildContext(ICakeContext context) : base(context)
+    {
+        MsBuildExecutable = context.Argument<string>("tool", "");
+        if (MsBuildExecutable == "")
+        {
+            var vsPath = context.VSWhereLatest();
+            if (vsPath == null)
+            {
+                context.Log.Write(Verbosity.Normal, LogLevel.Fatal, "Can not find Visual Studio install path");
+                System.Environment.Exit(-1);
+            }
+            MsBuildExecutable = Path.Combine(vsPath.FullPath, "Msbuild\\Current\\Bin\\MsBuild.exe");
+        }
+        context.Log.Information($"Use MsBuild at: {MsBuildExecutable}");
+    }
+}
