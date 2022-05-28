23495b47ce703e4ad0e27693839d4c1f576ce62c
MistEO <mistereo@hotmail.com>
2022-05-29 04:48:09 +0800

    chore.关闭掉落识别的调试宏

--- src/MeoAssistant/AbstractImageAnalyzer.h
+++ src/MeoAssistant/AbstractImageAnalyzer.h
@@ -4,9 +4,9 @@
 
 #include "AsstTypes.h"
 
-#ifndef  ASST_DEBUG
-#define ASST_DEBUG
-#endif // ! ASST_DEBUG
+//#ifndef  ASST_DEBUG
+//#define ASST_DEBUG
+//#endif // ! ASST_DEBUG
 
 namespace asst
 {
