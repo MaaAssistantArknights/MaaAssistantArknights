1d9a0a09975216446afb590b66e9ee8318ccd36c
MistEO <mistereo@hotmail.com>
2022-06-06 00:44:58 +0800

    chore.去掉了个默认参数

--- src/MeoAssistant/StartGameTask.cpp
+++ src/MeoAssistant/StartGameTask.cpp
@@ -17,9 +17,3 @@ StartGameTask& StartGameTask::set_param(std::string client_type) noexcept
     m_client_type = std::move(client_type);
     return *this;
 }
-
-StartGameTask& StartGameTask::set_enable(bool enable) noexcept
-{
-    AbstractTask::set_enable(enable);
-    return *this;
-}
