3e595a16264526232dad4d81150fe6dadf795cce
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-06-05 22:39:39 +0800

    Ignore CRLF linter

--- tools/CopilotDesinger/copilot-designer/.eslintrc.js
+++ tools/CopilotDesinger/copilot-designer/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": ["error", { "endOfLine": "auto" }],
   },
   overrides: [
     {
