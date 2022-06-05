e7d4a8122718336fe572ea93f44fe75b72a2a7a1
MistEO <mistereo@hotmail.com>
2022-06-06 02:05:33 +0800

    chore.水了几个没什么意义的变量名

--- src/MeoAssistant/Controller.h
+++ src/MeoAssistant/Controller.h
@@ -71,7 +71,7 @@ namespace asst
             std::string remote_address;
             unsigned short remote_port = 0U;
         };
-        std::optional<SocketInfo> try_init_socket(const std::string& local_address, unsigned short try_port, unsigned short try_times = 10U);
+        std::optional<SocketInfo> try_to_init_socket(const std::string& local_address, unsigned short try_port, unsigned short try_times = 10U);
 
         using DecodeFunc = std::function<bool(std::vector<uchar>&)>;
         bool screencap();
