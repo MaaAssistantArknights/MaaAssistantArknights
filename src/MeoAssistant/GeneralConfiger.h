2dcdc48caab253e2b03755455102950d071f5bb4
MistEO <mistereo@hotmail.com>
2022-06-21 22:47:23 +0800

    chore: 移动arkplanner的默认模板到配置文件中，并简单修改了一下输出json格式

--- src/MeoAssistant/GeneralConfiger.h
+++ src/MeoAssistant/GeneralConfiger.h
@@ -24,6 +24,11 @@ namespace asst
         std::string cmd_format;             // 命令格式
     };
 
+    struct DepotExportTemplate
+    {
+        std::string ark_planner;
+    };
+
     struct Options
     {
         int task_delay = 0;                  // 任务间延时：越快操作越快，但会增加CPU消耗
@@ -34,6 +39,7 @@ namespace asst
         int adb_extra_swipe_duration = -1;   // 额外的滑动持续时间：adb有bug，同样的参数，偶尔会划得非常远。额外做一个短程滑动，把之前的停下来。若小于0，则关闭额外滑动功能
         PenguinReportCfg penguin_report;     // 企鹅数据汇报：每次到结算界面，汇报掉落数据至企鹅数据 https://penguin-stats.cn/
         AipOcrCfg aip_ocr;                   // 百度 OCR API 的配置
+        DepotExportTemplate depot_export_template;  // 仓库识别结果导出模板
     };
 
     struct AdbCfg
