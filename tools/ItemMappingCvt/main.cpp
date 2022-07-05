fc20f36808782126f95f9fe579652b16255bd687
MistEO <mistereo@hotmail.com>
2022-07-05 22:06:39 +0800

    chore: 更新更新脚本，将基建图标模板纳入管理

--- tools/ItemMappingCvt/main.cpp
+++ tools/ItemMappingCvt/main.cpp
@@ -4,30 +4,77 @@
 #include <opencv2/opencv.hpp>
 #include <meojson/json.hpp>
 
-bool trans_and_save_icon(const std::filesystem::path& input, const std::filesystem::path& output);
+bool update_items_data(const std::filesystem::path& input_dir, const std::filesystem::path& output_dir);
+bool cvt_single_item_template(const std::filesystem::path& input, const std::filesystem::path& output);
+
 bool update_stages_data(const std::filesystem::path& output);
+
+bool update_infrast_templates(const std::filesystem::path& input_dir, const std::filesystem::path& output_dir);
 
 int main()
 {
     const std::filesystem::path input_dir("Arknights-Bot-Resource");
+
+    std::cout << "------------Update Arknights-Bot-Resource------------" << std::endl;
     int git_ret = 0;
-    if (std::filesystem::exists(input_dir)) {
-        git_ret = system("git -C Arknights-Bot-Resource pull");
+    if (!std::filesystem::exists(input_dir)) {
+        git_ret = system("git clone https://github.com/yuanyan3060/Arknights-Bot-Resource.git --depth=1");
     }
     else {
-        git_ret = system("git clone https://github.com/yuanyan3060/Arknights-Bot-Resource.git --depth=1");
+        git_ret = system(("git -C " + input_dir.string() + " pull").c_str());
     }
     if (git_ret != 0) {
         std::cout << "git cmd failed" << std::endl;
         return -1;
     }
 
+    const auto solution_dir = std::filesystem::current_path().parent_path().parent_path();
+    const auto resource_dir = solution_dir / "resource";
+    const auto third_resource_dir = solution_dir / "3rdparty" / "resource";
+
+    /* Update items template and json  from Arknights-Bot-Resource*/
+    std::cout << "------------Update items template and json------------" << std::endl;
+    if (!update_items_data(input_dir, resource_dir)) {
+        std::cerr << "Update items data failed" << std::endl;
+        return -1;
+    }
+
+    /* Update levels.json from Arknights-Bot-Resource*/
+    std::cout << "------------Update levels.json------------" << std::endl;
+    if (!std::filesystem::copy_file(
+        input_dir / "levels.json",
+        third_resource_dir / "Arknights-Tile-Pos" / "levels.json",
+        std::filesystem::copy_options::overwrite_existing)) {
+        std::cerr << "update levels.json failed" << std::endl;
+        return -1;
+    }
+
+    /* Update infrast templates from Arknights-Bot-Resource*/
+    std::cout << "------------Update infrast templates------------" << std::endl;
+    if (!update_infrast_templates(input_dir / "building_skill", resource_dir / "template" / "infrast")) {
+        std::cerr << "Update infrast templates failed" << std::endl;
+        return -1;
+    }
+
+    /* Update stage.json from Penguin Stats*/
+    std::cout << "------------Update stage.json------------" << std::endl;
+    if (!update_stages_data(solution_dir / "resource" / "stages.json")) {
+        std::cerr << "Update stages data failed" << std::endl;
+        return -1;
+    }
+
+    std::cout << "------------All success------------" << std::endl;
+    return 0;
+}
+
+bool update_items_data(const std::filesystem::path& input_dir, const std::filesystem::path& output_dir)
+{
     const auto input_json_path = input_dir / "gamedata" / "excel" / "item_table.json";
 
     std::ifstream ifs(input_json_path, std::ios::in);
     if (!ifs.is_open()) {
         std::cout << "open json failed" << std::endl;
-        return -1;
+        return false;
     }
     std::stringstream iss;
     iss << ifs.rdbuf();
@@ -35,10 +82,8 @@ int main()
     auto parse_ret = json::parse(iss.str());
     if (!parse_ret) {
         std::cout << "parse json failed" << std::endl;
-        return -1;
+        return false;
     }
-
-    const auto solution_dir = std::filesystem::current_path().parent_path().parent_path();
 
     auto& input_json = parse_ret.value();
     json::value output_json;
@@ -100,28 +145,19 @@ int main()
         output["sortId"] = item_info["sortId"];
         output["classifyType"] = item_info["classifyType"];
 
-        static const auto output_icon_path = solution_dir / "resource" / "template" / "items";
+        static const auto output_icon_path = output_dir / "template" / "items";
 
-        trans_and_save_icon(input_icon_path, output_icon_path / output_filename);
+        cvt_single_item_template(input_icon_path, output_icon_path / output_filename);
     }
-    auto output_json_path = solution_dir / "resource" / "item_index.json";
+    auto output_json_path = output_dir / "item_index.json";
     std::ofstream ofs(output_json_path, std::ios::out);
     ofs << output_json.format();
+    ofs.close();
 
-    std::cout << "copy levels.json" << std::endl;;
-    auto third_resource_dir = solution_dir / "3rdparty" / "resource";
-    std::filesystem::copy_file(input_dir / "levels.json", third_resource_dir / "Arknights-Tile-Pos" / "levels.json", std::filesystem::copy_options::overwrite_existing);
-
-
-    /* Update stage.json from Penguin Stats*/
-    if (!update_stages_data(solution_dir / "resource" / "stages.json")) {
-        return -1;
-    }
-
-    return 0;
+    return true;
 }
 
-bool trans_and_save_icon(const std::filesystem::path& input, const std::filesystem::path& output)
+bool cvt_single_item_template(const std::filesystem::path& input, const std::filesystem::path& output)
 {
     cv::Mat src = cv::imread(input.string(), -1);
     if (src.empty()) {
@@ -159,7 +195,7 @@ bool trans_and_save_icon(const std::filesystem::path& input, const std::filesyst
     return true;
 }
 
-bool update_stages_data(const std::filesystem::path& output)
+bool update_stages_data(const std::filesystem::path& output_dir)
 {
     // 国内访问可以改成 .cn 的域名
     const std::string PenguinAPI = R"(https://penguin-stats.io/PenguinStats/api/v2/stages)";
@@ -184,10 +220,57 @@ bool update_stages_data(const std::filesystem::path& output)
     }
 
     auto& stage_json = parse_ret.value();
-    
-    std::ofstream ofs(output, std::ios::out);
+
+    std::ofstream ofs(output_dir, std::ios::out);
     ofs << stage_json.format();
     ofs.close();
 
+    return true;
+}
+
+bool update_infrast_templates(const std::filesystem::path& input_dir, const std::filesystem::path& output_dir)
+{
+    for (auto&& entry : std::filesystem::directory_iterator(input_dir)) {
+        if (entry.path().extension() != ".png") {
+            continue;
+        }
+        const std::string& stem = entry.path().stem().string();
+
+        const std::vector<std::string> BlackList = {
+            "[style]",
+            "bskill_dorm_",
+            "bskill_train_",
+            "bskill_ws_"
+        };
+
+        bool is_blacklist = false;
+        for (const auto& bl : BlackList) {
+            if (stem.find(bl) != std::string::npos) {
+                is_blacklist = true;
+                break;
+            }
+        }
+        if (is_blacklist) {
+            continue;
+        }
+
+        cv::Mat image = cv::imread(entry.path().string(), -1);
+        cv::Mat cvt;
+        cv::cvtColor(image, cvt, cv::COLOR_BGRA2BGR);
+        for (int c = 0; c != image.cols; ++c) {
+            for (int r = 0; r != image.rows; ++r) {
+                auto p = image.at<cv::Vec4b>(c, r);
+                if (p[3] != 255) {
+                    cvt.at<cv::Vec3b>(c, r) = cv::Vec3b(0, 0, 0);
+                }
+            }
+        }
+        std::string out_file = (output_dir / entry.path().filename()).string();
+
+        if (!std::filesystem::exists(out_file)) {
+            std::cout << "New infrast templ: " << out_file << std::endl;
+        }
+        cv::imwrite(out_file, cvt);
+    }
     return true;
 }
