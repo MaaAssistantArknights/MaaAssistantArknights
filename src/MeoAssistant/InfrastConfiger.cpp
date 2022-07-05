4402014e7a43ed4c87783ff34d1ddaa017c2db6f
MistEO <mistereo@hotmail.com>
2022-07-06 00:14:58 +0800

    feat: 重构基建名字过滤的逻辑，重新整理基建资源；新增支持`多萝西`组换班

--- src/MeoAssistant/InfrastConfiger.cpp
+++ src/MeoAssistant/InfrastConfiger.cpp
@@ -138,10 +138,9 @@ bool asst::InfrastConfiger::parse(const json::value& json)
                         comb.efficient.emplace(pd, 0);
                     }
                 }
-                if (necessary_json.contains("hash")) {
-                    comb.hash_filter = true;
-                    for (const auto& [key, value] : necessary_json.at("hash").as_object()) {
-                        comb.possible_hashs.emplace(key, value.as_string());
+                if (auto filter_opt = necessary_json.find<json::array>("filter")) {
+                    for (const auto& filter : filter_opt.value()) {
+                        comb.name_filter.emplace_back(filter.as_string());
                     }
                 }
                 group.necessary.emplace_back(std::move(comb));
@@ -193,10 +192,10 @@ bool asst::InfrastConfiger::parse(const json::value& json)
                         comb.efficient.emplace(pd, 0);
                     }
                 }
-                if (opt_json.contains("hash")) {
-                    comb.hash_filter = true;
-                    for (const auto& [key, value] : opt_json.at("hash").as_object()) {
-                        comb.possible_hashs.emplace(key, value.as_string());
+
+                if (auto filter_opt = opt_json.find<json::array>("filter")) {
+                    for (const auto& filter : filter_opt.value()) {
+                        comb.name_filter.emplace_back(filter.as_string());
                     }
                 }
                 group.optional.emplace_back(std::move(comb));
