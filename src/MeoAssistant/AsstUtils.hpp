a7b7114f9e7731b963705eab67b0743c14647174
zzyyyl <74587068+zzyyyl@users.noreply.github.com>
2022-07-14 13:56:56 +0800

    feat: 给字符串替换增加支持一些 map 类型，尽管它们速度较慢
    https://github.com/MaaAssistantArknights/MaaAssistantArknights/pull/1157#pullrequestreview-1038260917

--- src/MeoAssistant/AsstUtils.hpp
+++ src/MeoAssistant/AsstUtils.hpp
@@ -43,6 +43,27 @@ namespace asst::utils
         return str;
     }
 
+    template<typename map_t>
+    inline std::string string_replace_all_batch(const std::string& src, const map_t& replace_pairs)
+    {
+        // 以下两个 static_assert 保证了 map_t 的实际类型，它可以是:
+        //     initializer_list<pair<string, string>>
+        //     map<string, string>
+        //     unordered_map<string, string>
+        // 但不能是
+        //     initializer_list<pair<string_view, string_view>>
+        // 等其它类型
+        static_assert(std::is_base_of<typename map_t::value_type::first_type, std::string>::value,
+            "type `map_t::value_type::first_type` not allowed.");
+        static_assert(std::is_base_of<typename map_t::value_type::second_type, std::string>::value,
+            "type `map_t::value_type::second_type` not allowed.");
+        std::string str = src;
+        for (const auto& [old_value, new_value] : replace_pairs) {
+            _string_replace_all(str, old_value, new_value);
+        }
+        return str;
+    }
+
     inline std::vector<std::string> string_split(const std::string& str, const std::string& delimiter)
     {
         std::string::size_type pos1 = 0;
