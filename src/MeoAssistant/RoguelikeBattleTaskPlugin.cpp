b83bfd80c4b408e96137d1c02513777d11c1425c
zzyyyl <74587068+zzyyyl@users.noreply.github.com>
2022-07-18 10:23:30 +0800

    style: 对齐！

--- src/MeoAssistant/RoguelikeBattleTaskPlugin.cpp
+++ src/MeoAssistant/RoguelikeBattleTaskPlugin.cpp
@@ -702,7 +702,7 @@ std::pair<asst::Point, int> asst::RoguelikeBattleTaskPlugin::calc_best_direction
     switch (role) {
     case BattleRole::Support:
         attack_range = {
-            Point(-1, -1), Point(0, -1),Point(1, -1),Point(2, -1),
+            Point(-1, -1),Point(0, -1),Point(1, -1),Point(2, -1),
             Point(-1, 0), Point(0, 0), Point(1, 0), Point(2, 0),
             Point(-1, 1), Point(0, 1), Point(1, 1), Point(2, 1),
         };
