c1f4e7f33eddd3fe26ae71951263de096ab9027f
MistEO <mistereo@hotmail.com>
2022-07-29 22:40:15 +0800

    fix: 尝试修复肉鸽购买收藏品干员时卡住的问题
    fix https://github.com/MaaAssistantArknights/MaaAssistantArknights/issues/1364

--- src/MeoAssistant/RoguelikeShoppingTaskPlugin.cpp
+++ src/MeoAssistant/RoguelikeShoppingTaskPlugin.cpp
@@ -23,6 +23,8 @@ bool asst::RoguelikeShoppingTaskPlugin::verify(AsstMsg msg, const json::value& d
 
 bool asst::RoguelikeShoppingTaskPlugin::_run()
 {
+    LogTraceFunction;
+
     OcrWithFlagTemplImageAnalyzer analyzer;
     analyzer.set_task_info("Roguelike1TraderShopping", "Roguelike1TraderShoppingOcr");
 
@@ -34,6 +36,10 @@ bool asst::RoguelikeShoppingTaskPlugin::_run()
     const auto& order_goods_list = Resrc.roguelike_shopping().get_goods();
 
     for (const auto& goods : order_goods_list) {
+        if (need_exit()) {
+            return false;
+        }
+
         if (goods.restriction != BattleRole::Unknown) {
             // TODO
             continue;
@@ -47,7 +53,10 @@ bool asst::RoguelikeShoppingTaskPlugin::_run()
         }
         Log.info("Ready to buy", goods.name);
         m_ctrler->click(find_it->rect);
-        bool confirmed = ProcessTask(*this, { "Roguelike1TraderShoppingConfirm" })
+        bool confirmed = ProcessTask(*this, { "Roguelike1TraderShoppingConfirm", "Roguelike1ChooseOperFlag" })
+            .set_times_limit("Roguelike1TraderRandomShopping", 0)
+            .set_times_limit("Roguelike1StageTraderLeave", 0)
+            .set_times_limit("Roguelike1ExitThenAbandon", 0)
             .set_retry_times(5)
             .run();
 
