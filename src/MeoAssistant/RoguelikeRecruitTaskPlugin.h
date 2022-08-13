9916e45288dbb2c032938db0d43f0ae13745ebf4
et <et2002@qq.com>
2022-08-13 23:47:21 +0800

    feat: 添加肉鸽招募界面专用滑动

--- src/MeoAssistant/RoguelikeRecruitTaskPlugin.h
+++ src/MeoAssistant/RoguelikeRecruitTaskPlugin.h
@@ -15,6 +15,8 @@ namespace asst
 
     protected:
         virtual bool _run() override;
+        void swipe_to_the_left_of_operlist(int loop_times = 1); // 滑动到干员列表的最左侧
+        void slowly_swipe(ProcessTaskAction action); // 缓慢向干员列表的左侧/右侧滑动
 
     private:
         bool check_core_char();
