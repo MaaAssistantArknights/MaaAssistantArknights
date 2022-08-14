9471ebe2ebb57c9316285493fd2b4c21f81de2b8
et <et2002@qq.com>
2022-08-14 21:51:05 +0800

    feat: 肉鸽招募支持跨职业优先级

--- src/MeoAssistant/RoguelikeRecruitTaskPlugin.h
+++ src/MeoAssistant/RoguelikeRecruitTaskPlugin.h
@@ -5,6 +5,14 @@
 
 namespace asst
 {
+    // 干员招募信息
+    struct RoguelikeRecruitInfo
+    {
+        std::string name;           // 干员名字
+        int priority = 0;           // 招募优先级 (0-1000)
+        int page_index = 0;         // 所在页码 (用于判断翻页方向)
+        bool is_alternate = false;  // 是否后备干员 (允许重复招募、划到后备干员时不再往右划动)
+    };
     class RoguelikeRecruitTaskPlugin : public AbstractTaskPlugin
     {
     public:
@@ -20,6 +28,7 @@ namespace asst
 
     private:
         bool check_core_char();
+        bool check_char(const std::string& char_name, bool is_rtl = false);
         void select_oper(const BattleRecruitOperInfo& oper);
     };
 }
