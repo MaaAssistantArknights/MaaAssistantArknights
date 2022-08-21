f1ae099e36358fff1fd93020fc52f12299569719
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2022-08-21 15:12:15 +0800

    将 vue 版 copilot designer 移动回来

---
+++ tools/CopilotDesinger/copilot-designer/src/controls/ActionTypeSelector.vue
@@ -0,0 +1,32 @@
+<template>
+  <select class="form-control" style="width: auto" v-model="modelValue">
+    <option value="二倍速">二倍速</option>
+    <option value="部署">部署</option>
+    <option value="技能">技能</option>
+    <option value="撤退">撤退</option>
+    <option value="子弹时间">子弹时间</option>
+    <option value="技能用法">技能用法</option>
+  </select>
+</template>
+
+<script lang="ts">
+import { ActionType } from "@/interfaces/CopilotData";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    value: String as PropType<ActionType>,
+  },
+  computed: {
+    modelValue: {
+      get() {
+        return this.value ?? "";
+      },
+      set(value: ActionType) {
+        this.$emit("update", value);
+      },
+    },
+  },
+  emits: ["update"],
+});
+</script>
---
+++ tools/CopilotDesinger/copilot-designer/src/controls/DirectionSelector.vue
@@ -0,0 +1,31 @@
+<template>
+  <select class="form-control" style="width: auto" v-model="modelValue">
+    <option value="无"></option>
+    <option value="上">上</option>
+    <option value="下">下</option>
+    <option value="左">左</option>
+    <option value="右">右</option>
+  </select>
+</template>
+
+<script lang="ts">
+import { Direction } from "@/interfaces/CopilotData";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    value: String as PropType<Direction>,
+  },
+  computed: {
+    modelValue: {
+      get() {
+        return this.value ?? "";
+      },
+      set(value: Direction) {
+        this.$emit("update", value);
+      },
+    },
+  },
+  emits: ["update"],
+});
+</script>
