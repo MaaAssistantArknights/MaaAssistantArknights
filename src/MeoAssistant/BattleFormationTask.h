83bad44256a4bb711b222066d5994056ceccf85b
MistEO <mistereo@hotmail.com>
2022-09-02 00:05:06 +0800

    feat: 抄作业自动编队功能支持按职业区分选择
    fix https://github.com/MaaAssistantArknights/MaaAssistantArknights/issues/1136

--- src/MeoAssistant/BattleFormationTask.h
+++ src/MeoAssistant/BattleFormationTask.h
@@ -13,14 +13,17 @@ namespace asst
         void set_stage_name(std::string name);
 
     protected:
+        using OperGroup = std::vector<BattleDeployOper>;
         virtual bool _run() override;
 
         bool enter_selection_page();
-        bool select_opers_in_cur_page();
+        bool select_opers_in_cur_page(std::vector<OperGroup>& groups);
         void swipe_page();
         bool confirm_selection();
+        bool click_role_table(BattleRole role);
+        bool parse_formation();
 
         std::string m_stage_name;
-        std::unordered_map<std::string, std::vector<BattleDeployOper>> m_groups;
+        std::unordered_map<BattleRole, std::vector<OperGroup>> m_formation;
     };
 }
