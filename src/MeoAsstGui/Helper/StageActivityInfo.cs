55f3f489597c7858c55b9bd49840dae1b38f6560
ET <et2002@qq.com>
2022-09-02 17:29:59 +0800

    feat: 优化Gui关卡选择逻辑

---
+++ src/MeoAsstGui/Helper/StageActivityInfo.cs
@@ -0,0 +1,47 @@
+// <copyright file="StageActivityInfo.cs" company="MaaAssistantArknights">
+// MeoAsstGui - A part of the MeoAssistantArknights project
+// Copyright (C) 2021 MistEO and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+// </copyright>
+
+using System;
+
+namespace MeoAsstGui
+{
+    /// <summary>
+    /// Stage activity info
+    /// </summary>
+    public class StageActivityInfo
+    {
+        /// <summary>
+        /// Gets or sets the activity tip
+        /// </summary>
+        public string Tip { get; set; }
+
+        /// <summary>
+        /// Gets or sets the activity UTC expire time
+        /// </summary>
+        public DateTime UtcExpireTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the activity is a resource collection activity
+        /// </summary>
+        /// <remarks>
+        /// Sets to true indicates a resource collection activity,
+        /// when the activity expires, <seealso cref="StageManager"/> will continue to check stage open days.
+        /// </remarks>
+        public bool IsResourceCollection { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the activity is expired
+        /// </summary>
+        public bool IsExpired => DateTime.UtcNow >= UtcExpireTime;
+    }
+}
---
+++ src/MeoAsstGui/Helper/StageManager.cs
@@ -0,0 +1,153 @@
+// <copyright file="StageManager.cs" company="MaaAssistantArknights">
+// MeoAsstGui - A part of the MeoAssistantArknights project
+// Copyright (C) 2021 MistEO and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeoAsstGui
+{
+    /// <summary>
+    /// Stage manager
+    /// </summary>
+    public class StageManager
+    {
+        private readonly Dictionary<string, StageInfo> _stages;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StageManager"/> class.
+        /// </summary>
+        public StageManager()
+        {
+            // var sideStory = new StageActivityInfo()
+            // {
+            //    Tip = "SideStory「理想城：长夏狂欢季」活动",
+            //    UtcExpireTime = new DateTime(2022, 9, 1, 4, 0, 0).AddHours(-8),
+            //    IsResourceCollection = false,
+            // };
+            var resourceCollection = new StageActivityInfo()
+            {
+                Tip = "「夏日嘉年华」，“资源收集”限时全天开放",
+                UtcExpireTime = new DateTime(2022, 9, 8, 4, 0, 0).AddHours(-8),
+                IsResourceCollection = true,
+            };
+
+            _stages = new Dictionary<string, StageInfo>
+            {
+                // SideStory「理想城：长夏狂欢季」活动
+                // { "IC-9", new StageInfo { Display = "IC-9", Value = "IC-9", Activity = sideStory } },
+                // { "IC-8", new StageInfo { Display = "IC-8", Value = "IC-8", Activity = sideStory } },
+                // { "IC-7", new StageInfo { Display = "IC-7", Value = "IC-7", Activity = sideStory } },
+
+                // 「当前/上次」关卡导航
+                { string.Empty, new StageInfo { Display = Localization.GetString("DefaultStage"), Value = string.Empty } },
+
+                // 主线关卡
+                { "1-7", new StageInfo { Display = "1-7", Value = "1-7" } },
+
+                // 资源本
+                { "CE-6", new StageInfo("CE-6", "CETip", new[] { DayOfWeek.Tuesday, DayOfWeek.Thursday, DayOfWeek.Saturday, DayOfWeek.Sunday }, resourceCollection) },
+                { "AP-5", new StageInfo("AP-5", "APTip", new[] { DayOfWeek.Monday, DayOfWeek.Thursday, DayOfWeek.Saturday, DayOfWeek.Sunday }, resourceCollection) },
+                { "CA-5", new StageInfo("CA-5", "CATip", new[] { DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Sunday }, resourceCollection) },
+                { "LS-6", new StageInfo("LS-6", "LSTip", new DayOfWeek[] { }, resourceCollection) },
+
+                // 碳本没做导航，只显示关卡提示
+                { "SK-5", new StageInfo("SK-5", "SKTip", new[] { DayOfWeek.Monday, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday }, resourceCollection) { IsHidden = true } },
+
+                // 剿灭模式
+                { "Annihilation", new StageInfo { Display = Localization.GetString("Annihilation"), Value = "Annihilation" } },
+
+                // 芯片本
+                { "PR-A-1", new StageInfo("PR-A-1", "PR-ATip", new[] { DayOfWeek.Monday, DayOfWeek.Thursday, DayOfWeek.Friday, DayOfWeek.Sunday }, resourceCollection) },
+                { "PR-A-2", new StageInfo("PR-A-2", string.Empty, new[] { DayOfWeek.Monday, DayOfWeek.Thursday, DayOfWeek.Friday, DayOfWeek.Sunday }, resourceCollection) },
+                { "PR-B-1", new StageInfo("PR-B-1", "PR-BTip", new[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Friday, DayOfWeek.Saturday }, resourceCollection) },
+                { "PR-B-2", new StageInfo("PR-B-2", string.Empty, new[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Friday, DayOfWeek.Saturday }, resourceCollection) },
+                { "PR-C-1", new StageInfo("PR-C-1", "PR-CTip", new[] { DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Saturday, DayOfWeek.Sunday }, resourceCollection) },
+                { "PR-C-2", new StageInfo("PR-C-2", string.Empty, new[] { DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Saturday, DayOfWeek.Sunday }, resourceCollection) },
+                { "PR-D-1", new StageInfo("PR-D-1", "PR-DTip", new[] { DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Saturday, DayOfWeek.Sunday }, resourceCollection) },
+                { "PR-D-2", new StageInfo("PR-D-2", string.Empty, new[] { DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Saturday, DayOfWeek.Sunday }, resourceCollection) },
+
+                // 周一和周日的关卡提示
+                { "Pormpt1", new StageInfo { Tip = Localization.GetString("Pormpt1"), OpenDays = new[] { DayOfWeek.Monday }, IsHidden = true } },
+                { "Pormpt2", new StageInfo { Tip = Localization.GetString("Pormpt2"), OpenDays = new[] { DayOfWeek.Sunday }, IsHidden = true } },
+
+                // 老版本「当前/上次」关卡导航
+                // new StageInfo { Display = Localization.GetString("CurrentStage"), Value = string.Empty },
+                // new StageInfo { Display = Localization.GetString("LastBattle"), Value = "LastBattle" },
+            };
+        }
+
+        /// <summary>
+        /// Gets stage by name
+        /// </summary>
+        /// <param name="stage">Stage name</param>
+        /// <returns>Stage info</returns>
+        public StageInfo GetStageInfo(string stage)
+        {
+            _stages.TryGetValue(stage, out var stageInfo);
+            return stageInfo;
+        }
+
+        /// <summary>
+        /// Determine whether stage is open
+        /// </summary>
+        /// <param name="stage">Stage name</param>
+        /// <param name="dayOfWeek">Current day of week</param>
+        /// <returns>Whether stage is open</returns>
+        public bool IsStageOpen(string stage, DayOfWeek dayOfWeek)
+        {
+            return GetStageInfo(stage)?.IsStageOpen(dayOfWeek) == true;
+        }
+
+        /// <summary>
+        /// Gets open stage tips at specified day of week
+        /// </summary>
+        /// <param name="dayOfWeek">Day of week</param>
+        /// <returns>Open stages</returns>
+        public string GetStageTips(DayOfWeek dayOfWeek)
+        {
+            var builder = new StringBuilder();
+
+            var stages = _stages.Where(pair => pair.Value.OpenDays?.Count() > 0);
+            foreach (var item in stages)
+            {
+                if (item.Value.OpenDays.Contains(dayOfWeek) && !string.IsNullOrEmpty(item.Value.Tip))
+                {
+                    builder.AppendLine(item.Value.Tip);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Gets open stage list at specified day of week
+        /// </summary>
+        /// <param name="dayOfWeek">Day of week</param>
+        /// <returns>Open stage list</returns>
+        public IEnumerable<CombData> GetStageList(DayOfWeek dayOfWeek)
+        {
+            return _stages.Values.Where(stage => !stage.IsHidden && stage.IsStageOpen(dayOfWeek));
+        }
+
+        /// <summary>
+        /// Gets all stage list
+        /// </summary>
+        /// <returns>All stage list</returns>
+        public IEnumerable<CombData> GetStageList()
+        {
+            return _stages.Values.Where(stage => !stage.IsHidden);
+        }
+    }
+}
