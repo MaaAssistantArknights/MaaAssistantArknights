d73e741272fd94cfe9e4c19bc91e51c3818b3235
MistEO <mistereo@hotmail.com>
2022-09-20 23:47:29 +0800

    fix: 修复基建办公室和会客室的自定义换班逻辑错误
    fix https://github.com/MaaAssistantArknights/MaaAssistantArknights/issues/1882

--- src/MeoAssistant/InfrastOfficeTask.cpp
+++ src/MeoAssistant/InfrastOfficeTask.cpp
@@ -22,9 +22,6 @@ bool asst::InfrastOfficeTask::_run()
         if (need_exit()) {
             return false;
         }
-        if (!opers_detect_with_swipe()) {
-            return false;
-        }
         if (is_use_custom_config()) {
             bool name_select_ret = swipe_and_select_custom_opers();
             if (name_select_ret) {
@@ -36,6 +33,9 @@ bool asst::InfrastOfficeTask::_run()
             }
         }
 
+        if (!opers_detect_with_swipe()) {
+            return false;
+        }
         optimal_calc();
         if (!opers_choose()) {
             m_all_available_opers.clear();
