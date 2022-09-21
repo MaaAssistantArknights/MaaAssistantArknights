4269cea18a2900db50410e30df23ed3bff296d5b
MistEO <mistereo@hotmail.com>
2022-09-21 16:03:52 +0800

    fix: 修复自定义基建会清空单人房间的问题
    fix https://github.com/MaaAssistantArknights/MaaAssistantArknights/issues/1891

--- src/MeoAssistant/InfrastAbstractTask.cpp
+++ src/MeoAssistant/InfrastAbstractTask.cpp
@@ -193,10 +193,17 @@ bool asst::InfrastAbstractTask::swipe_and_select_custom_opers(bool is_dorm_order
     else {
         ProcessTask(*this, { "InfrastOperListTabWorkStatusUnClicked" }).run();
     }
-    swipe_to_the_left_of_operlist(2);
-    click_clear_button();
+    // 如果只选了一个人或不用自动填充，则不需要滑动回去，也不需要排序
+    if (m_current_room_custom_config.selected > 1 || m_current_room_custom_config.autofill) {
+        swipe_to_the_left_of_operlist(2);
 
-    order_opers_selection(opers_order);
+        // 如果只选了一个人没必要排序
+        if (m_current_room_custom_config.selected > 1) {
+            click_clear_button();
+
+            order_opers_selection(opers_order);
+        }
+    }
 
     return m_current_room_custom_config.names.empty();
 }
