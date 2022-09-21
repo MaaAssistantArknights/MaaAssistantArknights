f4e487a23253f4392114a79633aa97597ec018f9
MistEO <mistereo@hotmail.com>
2022-09-21 23:14:33 +0800

    fix: 尝试修复一个飞桨运行时的崩溃问题
    https://github.com/MaaAssistantArknights/MaaAssistantArknights/issues/1879

--- src/MeoAssistant/OcrPack.h
+++ src/MeoAssistant/OcrPack.h
@@ -15,7 +15,7 @@ namespace asst
 {
     class OcrPack final : public SingletonHolder<OcrPack>, public AbstractResource
     {
-        static constexpr size_t MaxBoxSize = 128;
+        static constexpr size_t MaxBoxSize = 256;
 
     public:
         virtual ~OcrPack() override;
