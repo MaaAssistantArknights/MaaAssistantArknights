f6d66e1c7d83b87cd754e19327572a3dc79d413a
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2022-10-01 12:45:06 +0800

    refactor: move functions to platorm

--- src/MeoAssistant/Utils/Platform/AsstPlatform.h
+++ src/MeoAssistant/Utils/Platform/AsstPlatform.h
@@ -1,12 +1,81 @@
 #pragma once
 
 #include <cstddef>
+#include <filesystem>
 #include <new>
+#include <string>
 #include <type_traits>
 #include <utility>
 
 namespace asst::platform
 {
+    std::string callcmd(const std::string&);
+
+    using os_string = std::filesystem::path::string_type;
+
+    inline std::filesystem::path path(const os_string& os_str)
+    {
+        return std::filesystem::path(os_str);
+    }
+
+#ifdef _WIN32
+    std::string path_to_crt_string(const std::filesystem::path&);
+    std::string path_to_ansi_string(const std::filesystem::path&);
+    os_string to_osstring(const std::string& utf8_str);
+    std::string from_osstring(const os_string&);
+
+    // Allow construct a path from utf8-string in win32
+    inline std::filesystem::path path(const std::string& utf8_str)
+    {
+        return std::filesystem::path(to_osstring(utf8_str));
+    }
+
+    inline std::string path_to_utf8_string(const std::filesystem::path& path)
+    {
+        return from_osstring(path.native());
+    }
+
+    inline std::string path_to_crt_string(const std::string& utf8_path)
+    {
+        return path_to_crt_string(path(utf8_path));
+    }
+
+    inline std::string path_to_ansi_string(const std::string& utf8_path)
+    {
+        return path_to_crt_string(path(utf8_path));
+    }
+
+#else
+
+    inline os_string to_osstring(const std::string& utf8_str)
+    {
+        return utf8_str;
+    }
+
+    inline std::string from_osstring(const os_string& os_str)
+    {
+        return os_str;
+    }
+
+    inline std::string path_to_utf8_string(const std::filesystem::path& path)
+    {
+        return path.native();
+    }
+
+    inline std::string path_to_ansi_string(const std::filesystem::path& path)
+    {
+        return path.native();
+    }
+
+    inline std::string path_to_crt_string(const std::filesystem::path& path)
+    {
+        return path.native();
+    }
+
+#endif
+
+    // --------- detail ------------
+
     extern const size_t page_size;
 
     void* aligned_alloc(size_t len, size_t align);
@@ -50,4 +119,4 @@ namespace asst::platform
         inline TElem* get() const { return _ptr; }
         inline size_t size() const { return _ptr ? (page_size / sizeof(TElem)) : 0; }
     };
-}
+} // namespace asst::platform
