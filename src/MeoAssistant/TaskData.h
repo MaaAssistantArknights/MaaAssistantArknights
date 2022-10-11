738b6f928c318a607f2519635538497c1a9520b5
zzyyyl <aysyxx53@hotmail.com>
2022-10-11 18:47:13 +0800

    feat: 增加一些模板任务生成逻辑
    除了 template，其余参数若留空则生成时继承模板任务的参数
    
    Co-authored-by: Horror Proton <107091537+horror-proton@users.noreply.github.com>

--- src/MeoAssistant/TaskData.h
+++ src/MeoAssistant/TaskData.h
@@ -18,8 +18,103 @@ namespace asst
     class TaskData final : public SingletonHolder<TaskData>, public AbstractConfigerWithTempl
     {
     private:
-        static std::shared_ptr<TaskInfo> generate_task_info(const std::shared_ptr<TaskInfo>& base_ptr,
-                                                            const std::string& task_prefix)
+        std::shared_ptr<MatchTaskInfo> _default_match_task_info()
+        {
+            auto match_task_info_ptr = std::make_shared<MatchTaskInfo>();
+            match_task_info_ptr->templ_name = "__INVALID__";
+            match_task_info_ptr->templ_threshold = TemplThresholdDefault;
+            match_task_info_ptr->special_threshold = 0;
+
+            return match_task_info_ptr;
+        }
+
+        std::shared_ptr<OcrTaskInfo> _default_ocr_task_info()
+        {
+            auto ocr_task_info_ptr = std::make_shared<OcrTaskInfo>();
+            ocr_task_info_ptr->full_match = false;
+
+            return ocr_task_info_ptr;
+        }
+
+        std::shared_ptr<HashTaskInfo> _default_hash_task_info()
+        {
+            auto hash_task_info_ptr = std::make_shared<HashTaskInfo>();
+            hash_task_info_ptr->dist_threshold = 0;
+            hash_task_info_ptr->bound = true;
+
+            return hash_task_info_ptr;
+        }
+
+        std::shared_ptr<TaskInfo> _default_task_info()
+        {
+            auto task_info_ptr = std::make_shared<TaskInfo>();
+            task_info_ptr->algorithm = AlgorithmType::MatchTemplate;
+            task_info_ptr->action = ProcessTaskAction::DoNothing;
+            task_info_ptr->cache = true;
+            task_info_ptr->max_times = INT_MAX;
+            task_info_ptr->pre_delay = 0;
+            task_info_ptr->rear_delay = 0;
+            task_info_ptr->roi = Rect();
+            task_info_ptr->sub_error_ignored = false;
+            task_info_ptr->rect_move = Rect();
+            task_info_ptr->specific_rect = Rect();
+
+            return task_info_ptr;
+        }
+
+        // 从模板任务生成
+        const std::shared_ptr<MatchTaskInfo> default_match_task_info_ptr = _default_match_task_info();
+        const std::shared_ptr<OcrTaskInfo> default_ocr_task_info_ptr = _default_ocr_task_info();
+        const std::shared_ptr<HashTaskInfo> default_hash_task_info_ptr = _default_hash_task_info();
+        const std::shared_ptr<TaskInfo> default_task_info_ptr = _default_task_info();
+
+        // 这是下面几个函数的封装
+        std::shared_ptr<TaskInfo> generate_task_info(const std::string& name, const json::value&,
+                                                     std::shared_ptr<TaskInfo> default_ptr);
+
+        std::shared_ptr<TaskInfo> generate_match_task_info(const std::string& name, const json::value&,
+                                                           std::shared_ptr<MatchTaskInfo> default_ptr);
+        std::shared_ptr<TaskInfo> generate_ocr_task_info(const std::string& name, const json::value&,
+                                                         std::shared_ptr<OcrTaskInfo> default_ptr);
+        std::shared_ptr<TaskInfo> generate_hash_task_info(const std::string& name, const json::value&,
+                                                          std::shared_ptr<HashTaskInfo> default_ptr);
+        // TaskInfo 的基础成员
+        bool append_base_task_info(std::shared_ptr<TaskInfo> task_info_ptr, const std::string& name,
+                                   const json::value& task_json, std::shared_ptr<TaskInfo> default_ptr,
+                                   std::string task_prefix = "");
+
+        static std::vector<std::string> append_prefix(const std::vector<std::string>& base_task_list,
+                                                      const std::string& task_prefix)
+        {
+            if (task_prefix.empty()) {
+                return base_task_list;
+            }
+            std::vector<std::string> task_list = {};
+            for (const std::string& base : base_task_list) {
+                std::string_view base_view = base;
+                size_t l = 0;
+                bool has_same_prefix = false;
+                // base 任务里已经存在相同前缀，则不加前缀
+                // https://github.com/MaaAssistantArknights/MaaAssistantArknights/pull/2116#issuecomment-1270115238
+                for (size_t r = base_view.find('@'); r != std::string_view::npos; r = base_view.find('@', l)) {
+                    if (task_prefix == base_view.substr(l, r - l)) {
+                        has_same_prefix = true;
+                        break;
+                    }
+                    l = r + 1;
+                }
+                if (has_same_prefix) {
+                    task_list.emplace_back(base);
+                }
+                else {
+                    task_list.emplace_back(task_prefix + "@" + base);
+                }
+            }
+            return task_list;
+        };
+        // 运行时动态生成任务
+        static std::shared_ptr<TaskInfo> _generate_task_info(const std::shared_ptr<TaskInfo>& base_ptr,
+                                                             const std::string& task_prefix)
         {
             std::shared_ptr<TaskInfo> task_info_ptr;
             switch (base_ptr->algorithm) {
@@ -36,37 +131,13 @@ namespace asst
                 task_info_ptr = std::make_shared<TaskInfo>(*base_ptr);
                 break;
             }
-            using tasklist_t = std::vector<std::string>;
-            auto generate_tasks = [&](tasklist_t& task_list, const tasklist_t& base_task_list) {
-                task_list = {};
-                for (const std::string& base : base_task_list) {
-                    std::string_view base_view = base;
-                    size_t l = 0;
-                    bool has_same_prefix = false;
-                    // base 任务里已经存在相同前缀，则不加前缀
-                    // https://github.com/MaaAssistantArknights/MaaAssistantArknights/pull/2116#issuecomment-1270115238
-                    for (size_t r = base_view.find('@'); r != std::string_view::npos; r = base_view.find('@', l)) {
-                        if (task_prefix == base_view.substr(l, r - l)) {
-                            has_same_prefix = true;
-                            break;
-                        }
-                        l = r + 1;
-                    }
-                    if (has_same_prefix) {
-                        task_list.emplace_back(base);
-                    }
-                    else {
-                        task_list.emplace_back(task_prefix + "@" + base);
-                    }
-                }
-            };
 
             task_info_ptr->name = task_prefix + "@" + base_ptr->name;
-            generate_tasks(task_info_ptr->sub, base_ptr->sub);
-            generate_tasks(task_info_ptr->next, base_ptr->next);
-            generate_tasks(task_info_ptr->exceeded_next, base_ptr->exceeded_next);
-            generate_tasks(task_info_ptr->on_error_next, base_ptr->on_error_next);
-            generate_tasks(task_info_ptr->reduce_other_times, base_ptr->reduce_other_times);
+            task_info_ptr->sub = append_prefix(base_ptr->sub, task_prefix);
+            task_info_ptr->next = append_prefix(base_ptr->next, task_prefix);
+            task_info_ptr->exceeded_next = append_prefix(base_ptr->exceeded_next, task_prefix);
+            task_info_ptr->on_error_next = append_prefix(base_ptr->on_error_next, task_prefix);
+            task_info_ptr->reduce_other_times = append_prefix(base_ptr->reduce_other_times, task_prefix);
 
             return task_info_ptr;
         }
@@ -103,7 +174,7 @@ namespace asst
             }
 
             std::string derived_task_name = name.substr(0, name_len);
-            auto task_info_ptr = generate_task_info(base_task_iter, derived_task_name);
+            auto task_info_ptr = _generate_task_info(base_task_iter, derived_task_name);
             if (task_info_ptr == nullptr) [[unlikely]] {
                 return nullptr;
             }
