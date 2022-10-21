2cc4019779c40b5414a1708bbd116c3c829534d9
gdlcf88 <gdlcf88@outlook.com>
2022-10-21 21:21:43 +0800

    feat: 实现MAA基础热键

---
+++ src/MeoAsstGui/Helper/MaaHotKeys/IMaaHotKeyManager.cs
@@ -0,0 +1,13 @@
+﻿using GlobalHotKey;
+
+namespace MeoAsstGui.MaaHotKeys
+{
+    public interface IMaaHotKeyManager
+    {
+        bool TryRegister(MaaHotKeyAction action, MaaHotKey hotKey);
+
+        void Unregister(MaaHotKeyAction action);
+
+        MaaHotKey GetOrNull(MaaHotKeyAction action);
+    }
+}
