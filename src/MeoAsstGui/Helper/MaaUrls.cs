0aa9afe19b7f6a596c1416c1ee359df10e615d8f
MistEO <mistereo@hotmail.com>
2022-10-30 04:05:44 +0800

    fix: C# warning

--- src/MeoAsstGui/Helper/MaaUrls.cs
+++ src/MeoAsstGui/Helper/MaaUrls.cs
@@ -1,4 +1,17 @@
-﻿using System.Collections.Generic;
+// <copyright file="MaaUrls.cs" company="MaaAssistantArknights">
+// MeoAsstGui - A part of the MeoAssistantArknights project
+// Copyright (C) 2021 MistEO and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+// </copyright>
+
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MeoAsstGui
