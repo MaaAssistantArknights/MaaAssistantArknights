bdb4f39c4cb92feadee5aaf8cbf3af9c70bc0a43
MistEO <mistereo@hotmail.com>
2022-11-16 01:27:26 +0800

    perf: 调整所有滑动任务参数

--- src/MeoAssistant/Task/Plugin/RoguelikeRecruitTaskPlugin.cpp
+++ src/MeoAssistant/Task/Plugin/RoguelikeRecruitTaskPlugin.cpp
@@ -248,7 +248,7 @@ bool asst::RoguelikeRecruitTaskPlugin::_run()
 
         // 如果划动过，先划回最左边
         if (i != 0) {
-            swipe_to_the_left_of_operlist(i / 2 + 1);
+            swipe_to_the_left_of_operlist(i + 1);
         }
 
         auto image = m_ctrler->get_image();
@@ -305,7 +305,7 @@ bool asst::RoguelikeRecruitTaskPlugin::_run()
         is_rtl = (selected_oper->page_index * 2) >= i;
         if (!is_rtl) {
             // 从左往右需要先划回最左边
-            swipe_to_the_left_of_operlist(i / 2 + 1);
+            swipe_to_the_left_of_operlist(i + 1);
         }
     }
 
@@ -367,7 +367,7 @@ bool asst::RoguelikeRecruitTaskPlugin::check_char(const std::string& char_name, 
         sleep(Task.get("RoguelikeCustom-HijackCoChar")->post_delay);
     }
     Log.info(__FUNCTION__, "| Cannot find oper `" + char_name + "`");
-    swipe_to_the_left_of_operlist(i / 2 + 1);
+    swipe_to_the_left_of_operlist(i + 1);
     return false;
 }
 
@@ -407,7 +407,7 @@ void asst::RoguelikeRecruitTaskPlugin::swipe_to_the_left_of_operlist(int loop_ti
     for (int i = 0; i != loop_times; ++i) {
         ProcessTask(*this, { "RoguelikeRecruitOperListSwipeToTheLeft" }).run();
     }
-    ProcessTask(*this, { "SleepAfterOperListSwipe" }).run();
+    ProcessTask(*this, { "SleepAfterOperListQuickSwipe" }).run();
 }
 
 void asst::RoguelikeRecruitTaskPlugin::slowly_swipe(bool to_left)
