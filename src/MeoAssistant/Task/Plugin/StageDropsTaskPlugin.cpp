e0e1e8264176ed5256ba2efc23a4f3be535be707
MistEO <mistereo@hotmail.com>
2022-11-25 20:55:36 +0800

    chore: 加点日志

--- src/MeoAssistant/Task/Plugin/StageDropsTaskPlugin.cpp
+++ src/MeoAssistant/Task/Plugin/StageDropsTaskPlugin.cpp
@@ -185,8 +185,6 @@ void asst::StageDropsTaskPlugin::drop_info_callback()
 
 void asst::StageDropsTaskPlugin::set_start_button_delay()
 {
-    LogTraceFunction;
-
     if (m_is_annihilation) {
         return;
     }
@@ -199,6 +197,7 @@ void asst::StageDropsTaskPlugin::set_start_button_delay()
             int64_t duration = time(nullptr) - last_start_time;
             int elapsed = Task.get("EndOfAction")->pre_delay + Task.get("PRTS")->post_delay;
             int64_t delay = duration * 1000 - elapsed;
+            Log.info(__FUNCTION__, "set StartButton2 post delay", delay);
             m_cast_ptr->set_post_delay("StartButton2", static_cast<int>(delay));
         }
     }
