c8dce2e908dc7e62e79a0daf4a677df0e3a243f3
MistEO <mistereo@hotmail.com>
2022-12-05 01:17:28 +0800

    feat: 新增仅MaaTouch支持的暂停下干员优化。下的更快！

--- src/MaaCore/Controller.h
+++ src/MaaCore/Controller.h
@@ -243,6 +243,13 @@ namespace asst
             {
                 return m_input_func(up_cmd(wait_ms, with_commit, contact));
             }
+            bool key_down(int key_code, int wait_ms = DefaultClickDelay, bool with_commit = true) {
+                return m_input_func(key_down_cmd(key_code, wait_ms, with_commit));
+            }
+            bool key_up(int key_code, int wait_ms = DefaultClickDelay, bool with_commit = true)
+            {
+                return m_input_func(key_up_cmd(key_code, wait_ms, with_commit));
+            }
             bool wait(int ms) { return m_input_func(wait_cmd(ms)); }
             void clear() noexcept { m_wait_ms_count = 0; }
 
@@ -286,6 +293,28 @@ namespace asst
             {
                 char buff[16] = { 0 };
                 sprintf(buff, "u %d\n", contact);
+                std::string str = buff;
+
+                if (with_commit) str += commit_cmd();
+                if (wait_ms) str += wait_cmd(wait_ms);
+                return str;
+            }
+
+            [[nodiscard]] std::string key_down_cmd(int key_code, int wait_ms = DefaultClickDelay, bool with_commit = true)
+            {
+                char buff[64] = { 0 };
+                sprintf(buff, "k %d d\n", key_code);
+                std::string str = buff;
+
+                if (with_commit) str += commit_cmd();
+                if (wait_ms) str += wait_cmd(wait_ms);
+                return str;
+            }
+
+            [[nodiscard]] std::string key_up_cmd(int key_code, int wait_ms = DefaultClickDelay, bool with_commit = true)
+            {
+                char buff[64] = { 0 };
+                sprintf(buff, "k %d u\n", key_code);
                 std::string str = buff;
 
                 if (with_commit) str += commit_cmd();
