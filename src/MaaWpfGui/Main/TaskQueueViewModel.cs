48686088a060595c2682634bb9efc83373806730
uye <2396806385@qq.com>
2023-01-04 23:06:58 +0800

    feat: 支持指定掉落多语言显示

--- src/MaaWpfGui/Main/TaskQueueViewModel.cs
+++ src/MaaWpfGui/Main/TaskQueueViewModel.cs
@@ -1816,8 +1816,6 @@ namespace MaaWpfGui
             }
         }
 
-        private static readonly string _DropsFilename = Environment.CurrentDirectory + "\\resource\\item_index.json";
-
         /// <summary>
         /// Gets or sets the list of all drops.
         /// </summary>
@@ -1825,8 +1823,7 @@ namespace MaaWpfGui
 
         private void InitDrops()
         {
-            string jsonStr = File.ReadAllText(_DropsFilename);
-            var reader = (JObject)JsonConvert.DeserializeObject(jsonStr);
+            var reader = Utils.GetItemList();
             foreach (var item in reader)
             {
                 var val = item.Key;
@@ -1838,13 +1835,26 @@ namespace MaaWpfGui
                 }
 
                 var dis = item.Value["name"].ToString();
-                if (dis.EndsWith("双芯片") || dis.EndsWith("寻访凭证") || dis.EndsWith("加固建材")
-                    || dis.EndsWith("许可") || dis == "资质凭证" || dis == "高级凭证" || dis == "演习券"
-                    || dis.Contains("源石") || dis == "D32钢" || dis == "双极纳米片" || dis == "聚合剂"
-                    || dis == "晶体电子单元" || dis == "龙骨" || dis == "芯片助剂")
+#pragma warning disable SA1009 // Closing parenthesis should be spaced correctly
+                if (
+                    val.Equals("3213") || val.Equals("3223") || val.Equals("3233") || val.Equals("3243") // 双芯片
+                    || val.Equals("3253") || val.Equals("3263") || val.Equals("3273") || val.Equals("3283")
+                    || val.Equals("7001") || val.Equals("7002") || val.Equals("7003") || val.Equals("7004") // 许可/凭证
+                    || val.Equals("4004") || val.Equals("4005")
+                    || val.Equals("3105") || val.Equals("3131") || val.Equals("3132") || val.Equals("3233") // 龙骨/加固建材
+                    || val.Equals("6001") // 演习券
+                    || val.Equals("3141") || val.Equals("4002") // 源石
+                    || val.Equals("32001") // 芯片助剂
+                    || val.Equals("30115") // 聚合剂
+                    || val.Equals("30125") // 双极纳米片
+                    || val.Equals("30135") // D32钢
+                    || val.Equals("30145") // 晶体电子单元
+                    || val.Equals("30155") // 烧结核凝晶
+                    )
                 {
                     continue;
                 }
+#pragma warning restore SA1009
 
                 AllDrops.Add(new CombData { Display = dis, Value = val });
             }
