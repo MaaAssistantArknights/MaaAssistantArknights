f4be05137123638b284de264a3331b59ee735210
MistEO <mistereo@hotmail.com>
2023-01-14 14:41:40 +0800

    perf: 优化战斗中识别的鲁棒性

--- src/MaaCore/Task/BattleHelper.h
+++ src/MaaCore/Task/BattleHelper.h
@@ -45,6 +45,7 @@ namespace asst
         bool use_skill(const std::string& name, bool keep_waiting = true);
         bool use_skill(const Point& loc, bool keep_waiting = true);
         bool check_pause_button(const cv::Mat& reusable = cv::Mat());
+        bool check_in_battle(const cv::Mat& reusable = cv::Mat());
         virtual bool wait_until_start();
         bool wait_until_end();
         bool use_all_ready_skill(const cv::Mat& reusable = cv::Mat());
