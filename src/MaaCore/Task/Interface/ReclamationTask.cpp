1e9b7287bad718ef6d414304d30d26fde5db433a
wangli <342970955@qq.com>
2023-02-01 18:03:00 +0800

    fix：修复循环点击退出关卡按钮导致的卡死问题
    纠正错别字
    
    更改while循环条件: true->!need_exit()

--- src/MaaCore/Task/Interface/ReclamationTask.cpp
+++ src/MaaCore/Task/Interface/ReclamationTask.cpp
@@ -5,6 +5,8 @@
 #include "Task/ProcessTask.h"
 #include "Utils/Logger.hpp"
 
+#include "Task/Reclamation/ReclamationBattlePlugin.h"
+
 
 asst::ReclamationTask::ReclamationTask(const AsstCallback& callback, Assistant* inst)
     : InterfaceTask(callback, inst, TaskType),
@@ -12,6 +14,7 @@ asst::ReclamationTask::ReclamationTask(const AsstCallback& callback, Assistant* 
 {
     m_reclamation_task_ptr->set_tasks({ "Reclamation@Begin" });
     m_reclamation_task_ptr->set_ignore_error(true);
+    m_reclamation_task_ptr->register_plugin<ReclamationBattlePlugin>();
     for (int i = 0; i != 100; ++i) {
         m_subtasks.emplace_back(m_reclamation_task_ptr);
     }
