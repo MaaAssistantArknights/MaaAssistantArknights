a77ce638d093bc2e836adb326267f6481117893d
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2023-02-19 19:07:31 +0800

    chore: update cmake config for macOS

--- cmake/macos.cmake
+++ cmake/macos.cmake
@@ -7,28 +7,23 @@ if (BUILD_XCFRAMEWORK)
 
     add_custom_command(OUTPUT OpenCV.xcframework
         COMMAND rm -rf OpenCV.xcframework
-        COMMAND rm -f libopencv_world.4.5.dylib
-        COMMAND cp ${OPENCV_DIRECTORY}/lib/libopencv_world.4.5.3.dylib libopencv_world.4.5.dylib
-        COMMAND xcodebuild -create-xcframework -library libopencv_world.4.5.dylib -output OpenCV.xcframework
+        COMMAND rm -f libopencv_world.4.6.dylib
+        COMMAND cp ${OpenCV_INSTALL_PATH}/lib/libopencv_world4.4.6.0.dylib libopencv_world.4.6.dylib
+        COMMAND xcodebuild -create-xcframework -library libopencv_world.4.6.dylib -output OpenCV.xcframework
     )
 
     add_custom_command(OUTPUT ONNXRuntime.xcframework
         COMMAND rm -rf ONNXRuntime.xcframework
-        COMMAND xcodebuild -create-xcframework -library ${FastDeploy_DIR}/third_libs/install/onnxruntime/lib/libonnxruntime.1.12.0.dylib -output ONNXRuntime.xcframework
+        COMMAND xcodebuild -create-xcframework -library "${PROJECT_SOURCE_DIR}/MaaDeps/vcpkg/installed/${MAADEPS_TRIPLET}/lib/libonnxruntime.1.12.1.dylib" -output ONNXRuntime.xcframework
     )
 
-    add_custom_command(OUTPUT Paddle2ONNX.xcframework
-        COMMAND rm -rf Paddle2ONNX.xcframework
-        COMMAND xcodebuild -create-xcframework -library ${FastDeploy_DIR}/third_libs/install/paddle2onnx/lib/libpaddle2onnx.1.0.5.dylib -output Paddle2ONNX.xcframework
-    )
-
-    add_custom_command(OUTPUT FastDeploy.xcframework
+    add_custom_command(OUTPUT MaaDerpLearning.xcframework
         COMMAND rm -rf FastDeploy.xcframework
-        COMMAND xcodebuild -create-xcframework -library ${FastDeploy_DIR}/lib/libfastdeploy.0.0.0.dylib -output FastDeploy.xcframework
+        COMMAND xcodebuild -create-xcframework -library "${PROJECT_SOURCE_DIR}/MaaDeps/vcpkg/installed/${MAADEPS_TRIPLET}/lib/libMaaDerpLearning.dylib" -output MaaDerpLearning.xcframework
     )
 
     add_custom_target(MaaXCFramework ALL
-        DEPENDS MaaCore Paddle2ONNX.xcframework MaaCore.xcframework OpenCV.xcframework ONNXRuntime.xcframework FastDeploy.xcframework
+        DEPENDS MaaCore MaaCore.xcframework OpenCV.xcframework MaaDerpLearning.xcframework
     )
 endif (BUILD_XCFRAMEWORK)
 
