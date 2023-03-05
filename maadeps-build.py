0fe438d34de0364c4ac66e506931af66ee5e3cdb
dantmnf <dantmnf@users.noreply.github.com>
2023-03-05 17:49:06 +0800

    chore: fix CI MaaDeps download

--- maadeps-build.py
+++ maadeps-build.py
@@ -8,6 +8,8 @@ basedir = Path(__file__).parent
 def main():
     os.chdir(basedir)
     subprocess.check_call(["git", "submodule", "update", "--init", "--recommend-shallow", "--remote", "MaaDeps"])
-    subprocess.check_call(["git", "submodule", "update", "--init", "--recommend-shallow", "MaaDeps/vcpkg"])
     ret = subprocess.call([sys.executable, "build.py", *sys.argv[1:]], cwd=Path(basedir, "MaaDeps"))
     sys.exit(ret)
+
+if __name__ == '__main__':
+    main()
