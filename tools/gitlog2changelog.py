4995c9f515ce4044a985726f26c7f5026e563e51
zzyyyl <aysyxx53@hotmail.com>
2023-03-06 23:50:57 +0800

    chore: 稍微高级一点的 changelog 生成器

--- tools/gitlog2changelog.py
+++ tools/gitlog2changelog.py
@@ -13,47 +13,75 @@ contributors = {
     "@[Hao Guan]": "@hguandl",
 }
 
+# 从哪个 tag 开始
 latest = os.popen("git describe --abbrev=0 --tags").read()[:-1]
-
 print("From:", latest, "\n")
 
+# 输出一张好看的 git log 图到控制台
 git_pretty_command = rf'git log {latest}..HEAD --pretty=format:"%C(yellow)%d%Creset %s %C(bold blue)@%an%Creset (%Cgreen%h%Creset)" --graph'
 os.system(git_pretty_command)
 
-git_command = rf'git log {latest}..HEAD --pretty=format:"%s %C(bold blue)@[%an%Creset]" --graph'
+# 获取详细的 git log 信息
+git_command = rf'git log {latest}..HEAD --pretty=raw'
 with os.popen(git_command) as fp: bf = fp._stream.buffer.read()
 try: gitlogs = bf.decode().strip()
 except: gitlogs = bf.decode("gbk").strip()
 
-commits = {}
-now_commit = [commits]
-lastpos = -2
+current_commit = None
+current_commit_status = ""
+raw_commits_info = {}
 
 for line in gitlogs.split("\n"):
-    star_pos = line.find("*")
-    if star_pos == -1:
-        continue
-    commit_log_re = re.search(r"[a-z]", line, re.I)
-    if not commit_log_re:
-        continue
-    commit_log = line[commit_log_re.start():]
-    if commit_log.startswith("Merge"):
+    line = re.sub(r"^ *", "", line)
+    if line.startswith("commit "):
+        current_commit = line[7:]
+        current_commit_status = ""
+        raw_commits_info[current_commit] = {}
+    if current_commit == None:
         continue
 
-    while star_pos <= lastpos:
-        if lastpos - star_pos == 1:
-            print("Fatal error!")
-            exit(0)
-        now_commit.pop()
-        lastpos -= 2
+    if line == "":
+        continue
+    elif line.startswith("parent "):
+        if "parent" not in raw_commits_info[current_commit]:
+            raw_commits_info[current_commit]["parent"] = []
+        raw_commits_info[current_commit]["parent"].append(line[7:])
+    elif line.startswith("author "):
+        raw_commits_info[current_commit].update({ "author": line[7:].split(' <', 1)[0] })
+    elif line.startswith("committer "):
+        raw_commits_info[current_commit].update({ "committer": line[10:].split(' <', 1)[0] })
+    elif line.startswith("gpgsig"):
+        current_commit_status = "gpgsig"
+    elif line.find("-----END PGP SIGNATURE-----") != -1:
+        current_commit_status = ""
+    elif line.startswith("commit "):
+        pass
+    elif line.startswith("tree "):
+        pass
+    elif current_commit_status == "":
+        if "message" in raw_commits_info[current_commit] and not raw_commits_info[current_commit]["message"].startswith("Merge"):
+            continue
+        raw_commits_info[current_commit]["message"] = line
 
-    if star_pos > lastpos:
-        if star_pos - lastpos != 2:
-            print(f"error!({line})")
-            exit(0)
-        now_commit[-1][commit_log] = {}
-        now_commit.append(now_commit[-1][commit_log])
-        lastpos += 2
+commits = {}
+
+def search_commits(commit_hash: str, to):
+    if commit_hash not in raw_commits_info:
+        return
+    raw_commit_info = raw_commits_info[commit_hash]
+    if "visited" in raw_commit_info:
+        return
+    raw_commit_info.update({"visited": True})
+    commit_key = raw_commit_info["message"] + f" @[{raw_commit_info['author']}]"
+    to[commit_key] = {}
+    search_commits(raw_commit_info["parent"][0], to)
+    if len(raw_commit_info["parent"]) == 2:
+        search_commits(raw_commit_info["parent"][1], to[commit_key])
+
+search_commits([x for x in raw_commits_info.keys()][0], commits)
+
+# print(json.dumps(commits, ensure_ascii=False, indent=2))
+# exit(0)
 
 def print_commits(commits: dict, indent: str = "", need_sort: bool = True) -> (str, list):
     if not commits: return ("", [])
@@ -146,15 +174,7 @@ def print_commits(commits: dict, indent: str = "", need_sort: bool = True) -> (s
     return ret_message, ret_contributor
 
 res = print_commits(commits)
-
-
 nightly = os.popen("git describe --tags").read()[:-1]
 
 with open("../changelog.md", "w", encoding="utf8") as f:
     f.write("## " + nightly + "\n" + res[0])
-
-# sorted_commits = {}
-# for x in sorted([x for x in commits.keys()]):
-#     sorted_commits[x] = commits[x]
-
-# print(json.dumps(sorted_commits, indent=2, ensure_ascii=False))
