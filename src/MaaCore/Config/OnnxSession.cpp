5725977bc950257d63333844708087484f1589ab
MistEO <mistereo@hotmail.com>
2023-03-08 21:29:39 +0800

    fix: build error on linux

--- src/MaaCore/Config/OnnxSession.cpp
+++ src/MaaCore/Config/OnnxSession.cpp
@@ -12,7 +12,11 @@ bool asst::OnnxSession::load(const std::filesystem::path& path)
     Log.info("load", path);
 
     std::string name = utils::path_to_utf8_string(path.stem());
+#ifdef _WIN32
     Ort::Session session(m_env, path.wstring().c_str(), m_options);
+#else
+    Ort::Session session(m_env, path.string().c_str(), m_options);
+#endif
 
     m_sessions.insert_or_assign(std::move(name), std::move(session));
 
