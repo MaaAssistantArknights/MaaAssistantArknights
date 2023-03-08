1d20ab3cda3eba9f184e68a58e576c45e69e4f2b
MistEO <mistereo@hotmail.com>
2023-03-08 18:35:26 +0800

    feat: 战斗中技能使用ort识别

---
+++ src/MaaCore/Config/OnnxSession.cpp
@@ -0,0 +1,20 @@
+#include "OnnxSession.h"
+
+#include <array>
+#include <filesystem>
+#include <string_view>
+
+#include "Utils/Logger.hpp"
+
+bool asst::OnnxSession::load(const std::filesystem::path& path)
+{
+    LogTraceFunction;
+    Log.info("load", path);
+
+    std::string name = utils::path_to_utf8_string(path.stem());
+    Ort::Session session(m_env, path.wstring().c_str(), m_options);
+
+    m_sessions.insert_or_assign(std::move(name), std::move(session));
+
+    return true;
+}
