38e7f87b810e0f3eff5e6cc8db42d07464f4a392
Liam Sho <conan_creeper@outlook.com>
2023-03-25 19:31:46 +0800

    doc: add docs

--- src/MaaWpfGui/Services/IHttpService.cs
+++ src/MaaWpfGui/Services/IHttpService.cs
@@ -21,14 +21,47 @@ namespace MaaWpfGui.Services
 {
     public interface IHttpService
     {
+        /// <summary>
+        /// Send HTTP GET request and get a string response
+        /// </summary>
+        /// <param name="uri">Target Uri</param>
+        /// <param name="extraHeader">Extra HTTP Request Headers</param>
+        /// <returns>Response string, null when failed</returns>
         Task<string> GetStringAsync(Uri uri, Dictionary<string, string> extraHeader = null);
 
+        /// <summary>
+        /// Send HTTP GET request and get a body stream response
+        /// </summary>
+        /// <param name="uri">Target Uri</param>
+        /// <param name="extraHeader">Extra HTTP Request Headers</param>
+        /// <returns>Response stream, null when failed</returns>
         Task<Stream> GetStreamAsync(Uri uri, Dictionary<string, string> extraHeader = null);
 
+        /// <summary>
+        /// Send HTTP GET request and get the original <see cref="HttpRequestMessage"/>
+        /// </summary>
+        /// <param name="uri">Target Uri</param>
+        /// <param name="extraHeader">Extra HTTP Request Headers</param>
+        /// <returns><see cref="HttpRequestMessage"/> object</returns>
         Task<HttpResponseMessage> GetAsync(Uri uri, Dictionary<string, string> extraHeader = null);
 
+        /// <summary>
+        /// Send HTTP POST request and a string reponse
+        /// </summary>
+        /// <param name="uri">Target Uri</param>
+        /// <param name="content">The POST body content, will be serialized by <see cref="System.Text.Json.JsonSerializer"/></param>
+        /// <param name="extraHeader">Extra HTTP Request Headers</param>
+        /// <typeparam name="T">The type of the POST body content</typeparam>
+        /// <returns>Response string, null when failed</returns>
         Task<string> PostAsJsonAsync<T>(Uri uri, T content, Dictionary<string, string> extraHeader = null);
 
+        /// <summary>
+        /// Download a file from the Web
+        /// </summary>
+        /// <param name="uri">The URI of the file</param>
+        /// <param name="fileName">On disk filename</param>
+        /// <param name="contentType">File content type</param>
+        /// <returns>True if success, False if failed</returns>
         Task<bool> DownloadFileAsync(Uri uri, string fileName, string contentType = null);
     }
 }
