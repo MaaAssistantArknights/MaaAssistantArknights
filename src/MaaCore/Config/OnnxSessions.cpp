da0b860f4ace64601824cf1cd275dd1b4e027ac7
MistEO <mistereo@hotmail.com>
2023-04-07 22:44:19 +0800

    feat: 惰性加载onnx模型

--- src/MaaCore/Config/OnnxSessions.cpp
+++ src/MaaCore/Config/OnnxSessions.cpp
@@ -9,12 +9,26 @@
 bool asst::OnnxSessions::load(const std::filesystem::path& path)
 {
     LogTraceFunction;
-    Log.info("load", path);
+    Log.info("record path", path);
+
+    if (!std::filesystem::exists(path)) {
+        Log.error("file not exist:", path);
+        return false;
+    }
 
     std::string name = utils::path_to_utf8_string(path.stem());
-    Ort::Session session(m_env, path.c_str(), m_options);
-
-    m_sessions.insert_or_assign(std::move(name), std::move(session));
+    m_model_paths.insert_or_assign(name, path);
 
     return true;
+}
+
+Ort::Session& asst::OnnxSessions::get(const std::string& key)
+{
+    if (m_sessions.find(key) == m_sessions.end()) {
+        const auto& path = m_model_paths.at(key);
+        LogTraceScope(std::string(__FUNCTION__) + " | lazy load: " + utils::path_to_utf8_string(path) + " as " + key);
+        Ort::Session session(m_env, m_model_paths.at(key).c_str(), m_options);
+        m_sessions.emplace(key, std::move(session));
+    }
+    return m_sessions.at(key);
 }
