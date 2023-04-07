ce14e23d9a4030943d479bd7a199f71925dcae6b
MistEO <mistereo@hotmail.com>
2023-04-07 22:44:19 +0800

    feat: 惰性加载onnx模型

--- src/MaaCore/Config/OnnxSessions.h
+++ src/MaaCore/Config/OnnxSessions.h
@@ -14,11 +14,12 @@ namespace asst
         virtual ~OnnxSessions() override = default;
         virtual bool load(const std::filesystem::path& path) override;
 
-        Ort::Session& get(const std::string& key) { return m_sessions.at(key); }
+        Ort::Session& get(const std::string& key);
 
     private:
         Ort::Env m_env;
         Ort::SessionOptions m_options;
         std::unordered_map<std::string, Ort::Session> m_sessions;
+        std::unordered_map<std::string, std::filesystem::path> m_model_paths;
     };
 }
