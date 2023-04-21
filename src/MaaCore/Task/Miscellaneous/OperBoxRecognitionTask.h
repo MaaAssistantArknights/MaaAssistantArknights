b772596ad5f0ee0e6939a1b9dde16132959f3fcc
GD-GK <869540029@qq.com>
2023-04-21 19:38:27 +0800

    统一风格

--- src/MaaCore/Task/Miscellaneous/OperBoxRecognitionTask.h
+++ src/MaaCore/Task/Miscellaneous/OperBoxRecognitionTask.h
@@ -15,9 +15,10 @@ namespace asst
     protected:
         virtual bool _run() override;
         void swipe_page();
-        std::vector<OperBoxInfo> own_opers;
         void callback_analyze_result(bool done);
-        std::unordered_set<std::string> get_own_oper_names();
         bool swipe_and_analyze();
+        std::unordered_set<std::string> get_own_oper_names();
+        
+        std::vector<OperBoxInfo> m_own_opers;
     };
 }
