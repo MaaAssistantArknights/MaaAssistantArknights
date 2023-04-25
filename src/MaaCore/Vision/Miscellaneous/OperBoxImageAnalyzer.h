037ee70e640fccdcefcced32ccbb44994be30bb3
GD-GK <44790784+GD-GK@users.noreply.github.com>
2023-04-25 20:08:48 +0800

    feat: 干员识别增加等级和精英度 (#4438)
    * core返回json object
    
    * fix:冲突
    
    * 等级ok
    
    * 等级和精英度
    
    * core:等级，精英度识别
    
    * 优化识别方式
    
    * fix:ci
    
    * 添加潜能模板，fix ci
    
    * 潜能识别
    
    * clang
    
    * fix:直接使用stoi
    
    * 修改识别完成条件
    
    * 修复等级判断条件
    
    * fix
    
    * 修改类继承,使用char_model,函数拆分
    
    * 缩小名字识别框,添加替换字符。权宜之计 （
    
    * 纠错，优化代码

--- src/MaaCore/Vision/Miscellaneous/OperBoxImageAnalyzer.h
+++ src/MaaCore/Vision/Miscellaneous/OperBoxImageAnalyzer.h
@@ -6,11 +6,13 @@ namespace asst
 {
     struct OperBoxInfo
     {
-        std::string name;
         std::string id;
+        std::string name;
         int level = 0; // 等级
         int elite = 0; // 精英度
         bool own = false;
+        int potential = 0; // 潜能
+        Rect rect;
     };
     class OperBoxImageAnalyzer final : public AbstractImageAnalyzer
     {
@@ -21,9 +23,19 @@ namespace asst
         const auto& get_result() const noexcept { return m_current_page_opers; }
 
     private:
-        bool analyzer_opers();
-
-        std::vector<OperBoxInfo> m_current_page_opers;
+        int level_num(const std::string& level);
+        bool analyzer_oper_box();
+        // 获取lv rect
+        bool lv_flag_analyzer();
+        bool names_analyzer();
+        bool level_analyzer();
+        bool elite_analyzer();
+        bool potential_analyzer();
+        // 按位置排序
+        // 1 - 2 - 3
+        // 4 - 5 - 6
+        void sort_oper_horizontal(std::vector<asst::OperBoxInfo> m_oper_boxs);
+        std::vector<asst::OperBoxInfo> m_current_page_opers;
 #ifdef ASST_DEBUG
         cv::Mat m_image_draw_oper;
 #endif
