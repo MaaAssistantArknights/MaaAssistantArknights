e98d505c3517ece44f90f10047be34a51aab1102
dantmnf <dantmnf@users.noreply.github.com>
2023-05-02 02:10:31 +0800

    chore: use TaskDialog for native MessageBox

---
+++ src/MaaWpfGui/Helper/WindowHandle.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Media3D;
+
+namespace MaaWpfGui.Helper
+{
+    public struct WindowHandle
+    {
+        private IntPtr _handle;
+
+        public IntPtr Handle => _handle;
+
+        public static WindowHandle None => new WindowHandle() { _handle = IntPtr.Zero };
+
+        public static implicit operator WindowHandle(IntPtr h) => new WindowHandle() { _handle = h };
+
+        public static implicit operator WindowHandle(Window w)
+        {
+            var interop = new WindowInteropHelper(w);
+            return new WindowHandle { _handle = interop.Handle };
+        }
+    }
+}
