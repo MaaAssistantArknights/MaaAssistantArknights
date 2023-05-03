9de445dbd9d1beb52b1cdf154715fbce813585aa
MistEO <mistereo@hotmail.com>
2023-05-03 20:20:39 +0800

    chore: 尝试优化MaaBuilder一些有的没的

--- tools/MaaBuilder/Builder/BuildParameters.cs
+++ tools/MaaBuilder/Builder/BuildParameters.cs
@@ -169,12 +169,12 @@ public partial class Build
             if (b.GitHubActions.Ref.StartsWith(ReleaseTagRefPrefix))
             {
                 var tag = $"v{b.GitHubActions.Ref.Replace(ReleaseTagRefPrefix, "")}";
-                var pattern = @"v((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)";
+                var pattern = @"^v((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)";
                 var match = Regex.Match(tag, pattern);
                 if (match.Success)
                 {
                     GhTag = tag;
-                    var preReleasePattern = @"v((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*))(-)";
+                    var preReleasePattern = @"^v((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*))(-)";
                     var preReleaseMatch = Regex.Match(tag, preReleasePattern);
                     IsPreRelease = preReleaseMatch.Success;
                 }
