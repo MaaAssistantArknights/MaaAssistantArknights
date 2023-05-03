62bce65af21af4163e27507f8221b71d9b2ab2f0
dantmnf <dantmnf@users.noreply.github.com>
2023-05-04 00:01:59 +0800

    chore: close MainWindow instead of shutdown

--- src/MaaWpfGui/Services/TrayIcon.cs
+++ src/MaaWpfGui/Services/TrayIcon.cs
@@ -93,16 +93,7 @@ namespace MaaWpfGui.Services
 
         private void App_exit(object sender, EventArgs e)
         {
-            if (Instances.TaskQueueViewModel.Running)
-            {
-                Instances.MainWindowManager.Show();
-                if (!Instances.TaskQueueViewModel.ConfirmExit())
-                {
-                    return;
-                }
-            }
-
-            System.Windows.Application.Current.Shutdown();
+            System.Windows.Application.Current.MainWindow.Close();
         }
 
         private void App_show(object sender, EventArgs e)
