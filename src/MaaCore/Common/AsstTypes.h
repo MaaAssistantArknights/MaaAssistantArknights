0453f31709b59ef7502f695d426c0fc6196a54eb
MistEO <mistereo@hotmail.com>
2023-06-29 10:11:09 +0800

    fix: 将 OCR replace 改为有序

--- src/MaaCore/Common/AsstTypes.h
+++ src/MaaCore/Common/AsstTypes.h
@@ -382,7 +382,7 @@ namespace asst
         bool is_ascii = false;         // 是否启用字符数字模型
         bool without_det = false;      // 是否不使用检测模型
         bool replace_full = false; // 匹配之后，是否将整个字符串replace（false是只替换match的部分）
-        std::unordered_map<std::string, std::string>
+        std::vector<std::pair<std::string, std::string>>
             replace_map; // 部分文字容易识别错，字符串强制replace之后，再进行匹配
     };
 
