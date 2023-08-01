199b675bf4aa7119bd28a8add2ff080c349066fb
tangge233 <2697992791@qq.com>
2023-08-01 18:33:49 +0800

    fix: 这次绝对没问题了

--- src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
@@ -538,7 +538,7 @@ namespace MaaWpfGui.ViewModels.UI
                 {
                     var isInChina = urls[i].Contains("s3.maa-org.net") || urls[i].Contains("maa-ota.annangela.cn");
 
-                    if (latencies[i].Equals(-1.0))
+                    if (latencies[i] < 0)
                     {
                         _logger.Warning("\turl: {CDNUrl} not available", urls[i]);
                         continue;
@@ -558,7 +558,7 @@ namespace MaaWpfGui.ViewModels.UI
                     }
                 }
 
-                if (latencies[selected].Equals(-1.0))
+                if (latencies[selected] < 0)
                 {
                     _logger.Error("All mirrors are not available");
                     return CheckUpdateRetT.NetworkError;
