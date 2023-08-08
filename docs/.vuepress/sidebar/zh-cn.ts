4b20fa0ed7bb9912d40dc49da20c3827db2665f3
SherkeyXD <253294679@qq.com>
2023-08-08 23:42:08 +0800

    docs: 更多侧边栏图标支持

--- docs/.vuepress/sidebar/zh-cn.ts
+++ docs/.vuepress/sidebar/zh-cn.ts
@@ -14,27 +14,32 @@ export const zhcnSidebar = sidebar({
       children: [
         {
           text: "功能介绍",
+          icon: "mdi:information-outline",
           link: "/1.1-详细介绍",
         },
         {
           text: "常见问题",
+          icon: "ph:question-fill",
           link: "/1.2-常见问题",
         },
         {
           text: "模拟器支持",
-          icon: "laptop-code",
+          icon: "mingcute:laptop-fill",
           collapsible: true,
           children: [
             {
               text: "Windows",
+              icon: "ri:windows-fill",
               link: "/1.3-模拟器支持",
             },
             {
               text: "Mac",
+              icon: "basil:apple-solid",
               link: "/1.4-Mac模拟器支持",
             },
             {
               text: "Linux",
+              icon: "teenyicons:linux-alt-solid",
               link: "/1.5-Linux模拟器支持",
             },
           ],
@@ -48,22 +53,27 @@ export const zhcnSidebar = sidebar({
       children: [
         {
           text: "Linux编译教程",
+          icon: "teenyicons:linux-alt-solid",
           link: "/2.1-Linux编译教程",
         },
         {
           text: "开发相关",
+          icon: "iconoir:developer",
           link: "/2.2-开发相关",
         },
         {
-          text: "Github IssueBot使用方法",
+          text: "IssueBot使用方法",
+          icon: "bxs:bot",
           link: "/2.3-IssueBot使用方法",
         },
         {
           text: "纯网页端PR教程",
+          icon: "mingcute:git-pull-request-fill",
           link: "/2.4-纯网页端PR教程",
         },
         {
           text: "外服适配教程",
+          icon: "ri:earth-fill",
           link: "/2.5-外服适配教程",
         },
       ],
@@ -75,30 +85,37 @@ export const zhcnSidebar = sidebar({
       children: [
         {
           text: "集成文档",
+          icon: "bxs:book",
           link: "/3.1-集成文档",
         },
         {
           text: "回调消息协议",
+          icon: "material-symbols:u-turn-left",
           link: "/3.2-回调消息协议",
         },
         {
           text: "战斗流程协议",
+          icon: "ph:sword-bold",
           link: "/3.3-战斗流程协议",
         },
         {
           text: "任务流程协议",
+          icon: "material-symbols:task",
           link: "/3.4-任务流程协议",
         },
         {
           text: "肉鸽辅助协议",
+          icon: "ri:game-fill",
           link: "/3.5-肉鸽辅助协议",
         },
         {
           text: "基建排班协议",
+          icon: "material-symbols:view-quilt-rounded",
           link: "/3.6-基建排班协议",
         },
         {
           text: "保全派驻协议",
+          icon: "game-icons:prisoner",
           link: "/3.7-保全派驻协议",
         },
       ],
--- docs/.vuepress/sidebar/zh-tw.ts
+++ docs/.vuepress/sidebar/zh-tw.ts
@@ -14,23 +14,27 @@ export const zhtwSidebar = sidebar({
       children: [
         {
           text: "功能介绍",
+          icon: "mdi:information-outline",
           link: "/zh-tw/1.1-詳細介紹",
         },
         {
           text: "常見問題",
+          icon: "ph:question-fill",
           link: "/zh-tw/1.2-常見問題",
         },
         {
           text: "模擬器支援",
-          icon: "laptop-code",
+          icon: "mingcute:laptop-fill",
           collapsible: true,
           children: [
             {
               text: "Windows",
+              icon: "ri:windows-fill",
               link: "/zh-tw/1.3-模擬器支援",
             },
             {
               text: "Mac",
+              icon: "basil:apple-solid",
               link: "/zh-tw/1.4-Mac模擬器支援",
             },
           ],
@@ -44,22 +48,27 @@ export const zhtwSidebar = sidebar({
       children: [
         {
           text: "Linux編譯教程",
+          icon: "teenyicons:linux-alt-solid",
           link: "/zh-tw/2.1-Linux編譯教程",
         },
         {
           text: "开发相关",
+          icon: "iconoir:developer",
           link: "/zh-tw/2.2-開發相關",
         },
         {
-          text: "Github IssueBot使用方法",
+          text: "IssueBot使用方法",
+          icon: "bxs:bot",
           link: "/zh-tw/2.3-IssueBot使用方法",
         },
         {
           text: "純網頁端PR教程",
+          icon: "mingcute:git-pull-request-fill",
           link: "/zh-tw/2.4-純網頁端PR教程",
         },
         {
           text: "外服適配教程",
+          icon: "ri:earth-fill",
           link: "/zh-tw/2.5-外服適配教程",
         },
       ],
@@ -70,31 +79,38 @@ export const zhtwSidebar = sidebar({
       collapsible: true,
       children: [
         {
-          text: "集成文件",
+          text: "集成文檔",
+          icon: "bxs:book",
           link: "/zh-tw/3.1-集成文件",
         },
         {
           text: "回调消息协议",
+          icon: "material-symbols:u-turn-left",
           link: "/zh-tw/3.2-回呼訊息協定",
         },
         {
           text: "戰鬥流程協定",
+          icon: "ph:sword-bold",
           link: "/zh-tw/3.3-戰鬥流程協定",
         },
         {
           text: "任务流程协议",
+          icon: "material-symbols:task",
           link: "/zh-tw/3.4-任務流程協定",
         },
         {
           text: "肉鸽輔助協定",
+          icon: "ri:game-fill",
           link: "/zh-tw/3.5-肉鸽輔助協定",
         },
         {
           text: "基建排班协议",
+          icon: "material-symbols:view-quilt-rounded",
           link: "/zh-tw/3.6-基建排班協議",
         },
         {
           text: "保全派駐協議",
+          icon: "game-icons:prisoner",
           link: "/zh-tw/3.7-保全派駐協議",
         },
       ],
