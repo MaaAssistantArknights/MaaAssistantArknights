902bcc4a6e6e91fd4eef344fd4e4dcffae49ffef
Sherkey <57581480+SherkeyXD@users.noreply.github.com>
2023-08-10 09:53:28 +0800

    feat(docs): 文档大升级 (#5885)
    + 迁移到[vuepress-hope](https://theme-hope.vuejs.press/zh/)主题
    该主题有很多好用的特性，包括但不限于mermaid流程图，charts/echarts图表，更美观的提示卡片等
      + 优化的侧边栏和导航栏
      + 内置图标支持
      + 使用卡片来优化阅读体验
    + 修复了jsonc和cpp的高亮问题

---
+++ docs/.vuepress/navbar/zh-tw.ts
@@ -0,0 +1,29 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const zhtwNavbar = navbar([
+  {
+    text: "主页",
+    icon: "ic:round-home",
+    link: "/zh-tw/",
+  },
+  {
+    text: "用户手册",
+    icon: "mdi:user",
+    link: "/zh-tw/1.1-詳細介紹",
+  },
+  {
+    text: "開發文檔",
+    icon: "ph:code-bold",
+    link: "/zh-tw/2.1-Linux編譯教程",
+  },
+  {
+    text: "協定文檔",
+    icon: "basil:document-solid",
+    link: "/zh-tw/3.1-集成文件",
+  },
+  {
+    text: "MAA 官網",
+    icon: "mdi:cow",
+    link: "https://maa.plus",
+  },
+]);
--- docs/.vuepress/plugins/sitemap.ts
+++
@@ -1,7 +0,0 @@
-import { sitemapPlugin } from "vuepress-plugin-sitemap2";
-
-export default function SitemapPlugin() {
-  return sitemapPlugin({
-    hostname: "https://maa.plus",
-  });
-}
