6e8b66ec90d3f909ad6037c923b46571a42f00bc
David <56174894+Constrat@users.noreply.github.com>
2023-08-15 18:41:57 +0200

    optimizations / general refactoring

--- tools/ResourceUpdater/Tasks_Sorter.py
+++ tools/ResourceUpdater/Tasks_Sorter.py
@@ -1,83 +1,104 @@
 from collections import OrderedDict
+from pathlib import Path
 import json
 
 # MODIFY dir TO IMPLEMENT GITHUB ACTIONS
 # Everything else is scalable and should work as-is
 # maybe \\ need to be changed to / if Linux
 
-dir = "C:\\Users\\David\\Downloads"
-path = dir + "\\MaaAssistantArknights\\resource\\global"
+path = str(Path(__file__).parents[2]) + "\\resource"
 
-CNJson = open(
-    dir + "\\MaaAssistantArknights\\resource\\tasks.json",
-    encoding="utf-8",
+CN = json.load(
+    open(path + "\\tasks.json", encoding="utf-8")
 )
-ENJson = open(
-    path + "\\YostarEN\\resource\\tasks.json",
-    encoding="utf-8",
+EN = json.load(
+    open(path + "\\global\\YostarEN\\resource\\tasks.json", encoding="utf-8")
 )
-JPJson = open(
-    path + "\\YostarJP\\resource\\tasks.json",
-    encoding="utf-8",
+JP = json.load(
+    open(path + "\\global\\YostarJP\\resource\\tasks.json", encoding="utf-8")
 )
-KRJson = open(
-    path + "\\YostarKR\\resource\\tasks.json",
-    encoding="utf-8",
+KR = json.load(
+    open(path + "\\global\\YostarKR\\resource\\tasks.json", encoding="utf-8")
 )
-TWJson = open(
-    path + "\\txwy\\resource\\tasks.json",
-    encoding="utf-8",
-)
-
-CN = json.load(CNJson)
-EN = json.load(ENJson)
-JP = json.load(JPJson)
-KR = json.load(KRJson)
-TW = json.load(TWJson)
+TW = json.load(open(path + "\\global\\txwy\\resource\\tasks.json", encoding="utf-8"))
 
 orderedEN = []
 orderedJP = []
 orderedKR = []
 orderedTW = []
 
-w = 0
-x = 0
-y = 0
-z = 0
+# v = 0
+# w = 0
+# x = 0
+# y = 0
+# z = 0
+
+# for i in CN:
+#     for e in EN:
+#         if i == e:
+#             orderedEN.insert(w, e)
+#             w += 1
+
+#     for j in JP:
+#         if i == j:
+#             orderedJP.insert(x, j)
+#             x += 1
+
+#     for k in KR:
+#         if i == k:
+#             orderedKR.insert(y, k)
+#             y += 1
+
+#     for t in TW:
+#         if i == t:
+#             orderedTW.insert(z, t)
+#             z += 1
+
+#     v += 1
 
 for i in CN:
     for e in EN:
         if i == e:
-            orderedEN.insert(w, e)
-            w += 1
+            orderedEN.append(e)
 
     for j in JP:
         if i == j:
-            orderedJP.insert(x, j)
-            x += 1
+            orderedJP.append(j)
 
     for k in KR:
         if i == k:
-            orderedKR.insert(y, k)
-            y += 1
+            orderedKR.append(k)
 
     for t in TW:
         if i == t:
-            orderedTW.insert(z, t)
-            z += 1
+            orderedTW.append(t)
 
 output = OrderedDict(sorted(EN.items(), key=lambda item: orderedEN.index(item[0])))
-with open(path + "\\YoStarEN\\resource\\tasks.json", "w", encoding="utf-8") as outfile:
+with open(
+    path + "\\global\\YoStarEN\\resource\\tasks.json", "w", encoding="utf-8"
+) as outfile:
     json.dump(output, outfile, indent=4, ensure_ascii=False)
 
 output = OrderedDict(sorted(JP.items(), key=lambda item: orderedJP.index(item[0])))
-with open(path + "\\YoStarJP\\resource\\tasks.json", "w", encoding="utf-8") as outfile:
+with open(
+    path + "\\global\\YoStarJP\\resource\\tasks.json", "w", encoding="utf-8"
+) as outfile:
     json.dump(output, outfile, indent=4, ensure_ascii=False)
 
 output = OrderedDict(sorted(KR.items(), key=lambda item: orderedKR.index(item[0])))
-with open(path + "\\YoStarKR\\resource\\tasks.json", "w", encoding="utf-8") as outfile:
+with open(
+    path + "\\global\\YoStarKR\\resource\\tasks.json", "w", encoding="utf-8"
+) as outfile:
     json.dump(output, outfile, indent=4, ensure_ascii=False)
 
 output = OrderedDict(sorted(TW.items(), key=lambda item: orderedTW.index(item[0])))
-with open(path + "\\txwy\\resource\\tasks.json", "w", encoding="utf-8") as outfile:
+with open(
+    path + "\\global\\txwy\\resource\\tasks.json", "w", encoding="utf-8"
+) as outfile:
     json.dump(output, outfile, indent=4, ensure_ascii=False)
+
+# print("Official: " + str(v).rjust(4, " ") + " tasks")
+# print("YostarEN: " + str(w).rjust(4, " ") + " tasks")
+# print("YostarJP: " + str(x).rjust(4, " ") + " tasks")
+# print("YostarKR: " + str(y).rjust(4, " ") + " tasks")
+# print("txwy:     " + str(z).rjust(4, " ") + " tasks")
