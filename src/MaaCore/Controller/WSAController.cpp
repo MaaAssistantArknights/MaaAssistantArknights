eb5f1d8ada4a9906da873017ba8ca36c7ca7544b
xinhang huang <h1318658200@outlook.com>
2023-08-25 01:05:45 +0800

    feat: MAACore增加对WSA的原生支持。

---
+++ src/MaaCore/Controller/WSAController.cpp
@@ -0,0 +1,582 @@
+
+#include "WSAController.h"
+
+#if defined(_WIN32)
+
+asst::WSAController::WSAController(const AsstCallback& callback, Assistant* inst, PlatformType type)
+    : InstHelper(inst), m_callback(callback), m_wgc(m_last_error), m_uuid("什么是UUID？")
+{
+    LogTraceFunction;
+
+    if (!winrt::Windows::Graphics::Capture::GraphicsCaptureSession::IsSupported()) {
+        json::value create_device_error = json::object {
+            { "uuid", m_uuid },
+            { "what", "NotSupported" },
+            { "why", "Require higher windows version" },
+            { "details",
+              json::object {
+                  { "Minium version supported: win10 1903", m_last_error },
+              } },
+        };
+        this->callback(AsstMsg::InternalError, create_device_error);
+    }
+
+    if (m_last_error != 0) {
+        json::value create_device_error = json::object {
+            { "uuid", m_uuid },
+            { "what", "DeviceFailed" },
+            { "why", "Cannot create D3D11 device." },
+            { "details",
+              json::object {
+                  { "GetLastError() = ", m_last_error },
+              } },
+        };
+        this->callback(AsstMsg::InternalError, create_device_error);
+    }
+
+    m_platform_io = PlatformFactory::create_platform(inst, type);
+
+    if (!m_platform_io) {
+        Log.error("platform not supported");
+        throw std::runtime_error("platform not supported");
+    }
+
+    m_inited = true;
+}
+
+asst::WSAController::~WSAController()
+{
+    LogTraceFunction;
+
+    m_inited = false;
+}
+
+bool asst::WSAController::connect([[maybe_unused]] const std::string& adb_path,
+                                  [[maybe_unused]] const std::string& address,
+                                  [[maybe_unused]] const std::string& config)
+{
+    LogTraceFunction;
+
+    bool result = m_wgc.prepare(m_resize_window, m_golden_border);
+
+    if (!result) {
+        json::value error = json::object { { "uuid", m_uuid },
+                                           { "what", "ConnectionFailed" },
+                                           { "detail", "cannot prepare for connection" } };
+        callback(AsstMsg::ConnectionInfo, error);
+        return false;
+    }
+
+    if (need_exit()) {
+        return false;
+    }
+
+    if (!m_wgc.start_capture()) {
+
+        json::value error = json::object { { "uuid", m_uuid },
+                                           { "what", "ConnectionFailed" },
+                                           { "detail", "capture failed" } };
+        callback(AsstMsg::ConnectionInfo, error);
+        return false;
+    }
+
+    m_touch.init(m_wgc.pub_wndwsa, m_wgc.pub_caption_height);
+
+    return true;
+}
+
+ void asst::WSAController::set_resize_window(bool enable) noexcept {
+     m_resize_window = enable;
+ }
+ 
+ void asst::WSAController::set_golden_border(bool enable) noexcept {
+     m_golden_border = enable;
+ }
+
+asst::WSAController::FrameBuffer::FrameBuffer(OUT DWORD& last_error)
+    : m_frame_pool(nullptr), m_session(nullptr), m_item(nullptr)
+{
+    LogTraceFunction;
+
+    HRESULT result = S_OK;
+    last_error = 0;
+
+    result = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, NULL, D3D11_CREATE_DEVICE_VIDEO_SUPPORT, nullptr, 0,
+                               D3D11_SDK_VERSION, m_native_device.put(), nullptr, m_context.put());
+    if (FAILED(result)) {
+        last_error = GetLastError();
+        return;
+    }
+}
+
+asst::WSAController::FrameBuffer::~FrameBuffer() {
+    LogTraceFunction;
+
+    end_capture();
+    m_frame_pool = nullptr;
+    m_item = nullptr;
+    m_device = nullptr;
+    m_staging_texture->Release();
+    m_context->Release();
+    m_native_device->Release();
+}
+
+bool asst::WSAController::FrameBuffer::prepare(bool resize_window, bool golden_border)
+{
+    LogTraceFunction;
+
+    m_golden_border = golden_border;
+
+    auto fill_error = [](std::string fn_name) {
+        Log.error(std::format("prepare() FAILED  function name [{}], GetLastError() = {}", fn_name, GetLastError()));
+    };
+
+    pub_wndwsa = FindWindow(arknights_classname.c_str(), nullptr);
+    if (pub_wndwsa == NULL) {
+        fill_error("FindWindow");
+        return false;
+    }
+
+    if (IsIconic(pub_wndwsa)) {
+        Log.trace("WSA window is iconic. Try to show WSA window.");
+        if (!ShowWindow(pub_wndwsa, SW_SHOW)) return false;
+    }
+    
+    SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+    auto wndStyle = GetWindowLong(pub_wndwsa, GWL_STYLE);
+    auto wndExStyle = GetWindowLong(pub_wndwsa, GWL_EXSTYLE);
+    auto dpi = GetDpiForWindow(pub_wndwsa);
+
+    RECT wantedSize = { .left = 0, .top = 0, .right = WindowWidthDefault, .bottom = WindowHeightDefault };
+    RECT wantedWindow = wantedSize, wantedClient = wantedSize;
+    if (resize_window) {
+        AdjustWindowRectExForDpi(&wantedWindow, wndStyle, false, wndExStyle, dpi);
+        wantedWindow.right -= wantedWindow.left;
+        wantedWindow.bottom -= wantedWindow.top;
+        wantedWindow.top = wantedWindow.left = 0;
+        SetWindowPos(pub_wndwsa, NULL, 0, 0, wantedWindow.right, wantedWindow.bottom, SWP_NOMOVE);
+
+        GetClientRect(pub_wndwsa, &wantedClient);
+        pub_caption_height = wantedClient.bottom - wantedClient.top - WindowHeightDefault;
+    }
+    else {
+        GetClientRect(pub_wndwsa, &wantedClient);
+        GetWindowRect(pub_wndwsa, &wantedWindow);
+        wantedWindow.bottom -= wantedWindow.top;
+        wantedWindow.right -= wantedWindow.left;
+        wantedWindow.left = wantedWindow.top = 0;
+        RECT testCaption = { 0, 0, 100, 100 };
+        AdjustWindowRectExForDpi(&testCaption, wndStyle, false, wndExStyle, dpi);
+
+        pub_caption_height = wantedClient.bottom - wantedClient.top - 720;
+        wantedSize.right = wantedClient.right;
+        wantedSize.bottom = wantedClient.bottom - pub_caption_height;
+    }
+    m_arknights_roi = cv::Rect { 0, pub_caption_height, wantedSize.right, wantedSize.bottom };
+
+    HRESULT result = S_OK;
+    auto factory = winrt::get_activation_factory<winrt::Windows::Graphics::Capture::GraphicsCaptureItem,
+                                                 IGraphicsCaptureItemInterop>();
+    result = factory->CreateForWindow(
+        pub_wndwsa, winrt::guid_of<winrt::Windows::Graphics::Capture::GraphicsCaptureItem>(), winrt::put_abi(m_item));
+    if (FAILED(result)) {
+        fill_error("IGraphicsCaptureItem::CreateForWindow");
+        return false;
+    }
+
+    auto dxgiDevice = m_native_device.as<IDXGIDevice>();
+    result = CreateDirect3D11DeviceFromDXGIDevice(dxgiDevice.get(),
+                                                  reinterpret_cast<IInspectable**>(winrt::put_abi(m_device)));
+    if (FAILED(result)) {
+        fill_error("CreateDirect3D11DeviceFromDXGIDevice");
+        return false;
+    }
+    
+    m_size = m_item.Size();
+    pub_size.first = wantedSize.right;
+    pub_size.second = wantedSize.bottom;
+    m_pitch = m_size.Width * 3ull;
+
+    m_front.create(m_size.Height, m_size.Width, CV_8UC3);
+    m_back.create(m_size.Height, m_size.Width, CV_8UC3);
+
+    {
+        D3D11_TEXTURE2D_DESC desc = { .Width = WindowWidthDefault,
+                                      .Height = WindowHeightDefault,
+                                      .MipLevels = 1,
+                                      .ArraySize = 1,
+                                      .Format = DXGI_FORMAT_B8G8R8A8_UNORM,
+                                      .SampleDesc = { .Count = 1, .Quality = 0 },
+                                      .Usage = D3D11_USAGE_STAGING,
+                                      .CPUAccessFlags = D3D11_CPU_ACCESS_READ | D3D11_CPU_ACCESS_WRITE,
+                                      .MiscFlags = 0 };
+        result = m_native_device->CreateTexture2D(&desc, nullptr, m_staging_texture.put());
+    }
+    if (FAILED(result)) {
+        fill_error("ID3D11Device::CreateTexture2D");
+        return false;
+    }
+
+    m_frame_pool = winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool::CreateFreeThreaded(
+        m_device, winrt::Windows::Graphics::DirectX::DirectXPixelFormat::B8G8R8A8UIntNormalized, 1, m_size);
+    if (m_frame_pool == nullptr) {
+        fill_error("winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool::CreateFreeThreaded");
+        return false;
+    }
+
+    m_session = m_frame_pool.CreateCaptureSession(m_item);
+    if (m_session == nullptr) {
+        fill_error("winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool::CreateCaptureSession");
+        return false;
+    }
+    m_session.IsBorderRequired(m_golden_border);
+    m_session.IsCursorCaptureEnabled(false);
+
+    return SUCCEEDED(result);
+}
+
+bool asst::WSAController::FrameBuffer::start_capture()
+{
+    LogTraceFunction;
+
+    if (end_capture()) return false;
+    m_frame_arrived = m_frame_pool.FrameArrived({ this, &FrameBuffer::process_frame });
+    m_session = m_frame_pool.CreateCaptureSession(m_item);
+    m_session.IsBorderRequired(m_golden_border);
+    m_session.IsCursorCaptureEnabled(false);
+    m_session.StartCapture();
+
+    return true;
+}
+
+bool asst::WSAController::FrameBuffer::end_capture()
+{
+    LogTraceFunction;
+
+    if (m_session) {
+        m_locker.lock();
+        m_session = nullptr;
+        m_frame_pool.FrameArrived(m_frame_arrived);
+        m_locker.unlock();
+        return true;
+    }
+
+    return false;
+}
+
+bool asst::WSAController::FrameBuffer::restart_capture()
+{
+    LogTraceFunction;
+
+    end_capture();
+
+    m_frame_pool.Recreate(m_device, winrt::Windows::Graphics::DirectX::DirectXPixelFormat::B8G8R8A8UIntNormalized, 1,
+                          m_size);
+
+    start_capture();
+
+    return true;
+}
+
+bool asst::WSAController::FrameBuffer::get_once(cv::Mat payload)
+{
+    m_locker.lock();
+    payload = m_front(m_arknights_roi).clone();
+    m_locker.unlock();
+
+    return true;
+}
+
+void asst::WSAController::FrameBuffer::process_frame(
+    winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool const& framePool,
+    [[maybe_unused]] winrt::Windows::Foundation::IInspectable const& object)
+{
+    m_locker.lock();
+    if (!m_session) {
+        m_locker.unlock();
+        return;
+    }
+    m_locker.unlock();
+
+    winrt::Windows::Graphics::Capture::Direct3D11CaptureFrame frame = framePool.TryGetNextFrame();
+    auto size = frame.ContentSize();
+
+    if (size != m_size) {
+        frame = nullptr;
+        restart_capture();
+        return;
+    }
+
+    winrt::com_ptr<Windows::Graphics::DirectX::Direct3D11::IDirect3DDxgiInterfaceAccess> dxgiAccess = {
+        frame.Surface().as<Windows::Graphics::DirectX::Direct3D11::IDirect3DDxgiInterfaceAccess>()
+    };
+    winrt::com_ptr<::ID3D11Resource> resource;
+    winrt::check_hresult(dxgiAccess->GetInterface(__uuidof(resource), resource.put_void()));
+    
+    m_context->CopyResource(m_staging_texture.get(), resource.get());
+
+    D3D11_MAPPED_SUBRESOURCE field;
+    UINT subresource = D3D11CalcSubresource(0, 0, 0);
+    m_context->Map(m_staging_texture.get(), subresource, D3D11_MAP_READ, 0, &field);
+
+    uchar* data = m_back.ptr();
+    uchar* source = (uchar*)field.pData;
+    for (size_t i = 0; i < m_size.Height; i++) {
+        for (size_t j = 0; j < m_size.Width; j++) {
+            data[i * m_pitch + j * 3] = source[i * field.RowPitch + j * 4 + 2];
+            data[i * m_pitch + j * 3 + 1] = source[i * field.RowPitch + j * 4 + 1];
+            data[i * m_pitch + j * 3 + 2] = source[i * field.RowPitch + j * 4];
+        }
+    }
+
+    m_locker.lock();
+    std::swap(m_front, m_back);
+    m_locker.unlock();
+    m_context->Unmap(m_staging_texture.get(), subresource);
+}
+
+void asst::WSAController::callback(AsstMsg msg, const json::value& details)
+{
+    if (m_callback) {
+        m_callback(msg, details, m_inst);
+    }
+}
+
+bool asst::WSAController::screencap(cv::Mat& image_payload, [[maybe_unused]] bool allow_reconnect)
+{
+    return m_wgc.get_once(image_payload);
+}
+
+bool asst::WSAController::start_game([[maybe_unused]] const std::string& client_type)
+{
+    LogTraceFunction;
+
+    platform::os_string env = GetEnvironmentStrings();
+    auto path_pos = env.find(platform::to_osstring("APPDATA"));
+    std::wostringstream wiss;
+    wiss << env.substr(path_pos + 7);
+    std::filesystem::path appdata_path = wiss.str();
+
+    auto whole_path = appdata_path / m_wsapath;
+    system(whole_path.string().c_str());
+
+    return false;
+}
+
+bool asst::WSAController::click(const Point& p)
+{
+    return  m_touch.click(p.x, p.y);
+}
+
+bool asst::WSAController::swipe(const Point& p1, const Point& p2, int duration, [[maybe_unused]] bool extra_swipe,
+                                [[maybe_unused]] double slope_in, [[maybe_unused]] double slope_out,
+                                [[maybe_unused]] bool with_pause)
+{
+    if (with_pause) {
+        return m_touch.swipe_with_pause(p1.x, p1.y, p2.x, p2.y, duration);
+    }
+    else {
+        return m_touch.swipe(p1.x, p1.y, p2.x, p2.y, duration);
+    }
+}
+
+bool asst::WSAController::press_esc()
+{
+    m_touch.press(VK_ESCAPE);
+    return true;
+}
+
+asst::WSAController::Toucher::~Toucher()
+{
+    m_running.store(false);
+    if (m_thread.joinable()) m_thread.join();
+}
+
+void asst::WSAController::Toucher::init(HWND hwnd, int caption_height)
+{
+    m_wnd = hwnd;
+    m_caption_height = caption_height;
+
+    m_running.store(true);
+    m_thread = std::thread([this]() { this->run(); });
+
+    std::mt19937_64 random_engine { std::random_device {}() };
+    std::uniform_real_distribution<double> dist(-5, 5);
+    for (int i = 0; i < 10; i++)
+        random_end.push_back(dist(random_engine));
+}
+
+bool asst::WSAController::Toucher::click(int x, int y)
+{
+    if (!m_inited) return false;
+
+    m_locker.lock();
+
+    if (m_msgs.size() > max_queue_length) {
+        m_locker.unlock();
+        return false;
+    }
+
+    m_msgs.push({ 0, x, y });
+
+    m_locker.unlock();
+
+    return true;
+}
+
+bool asst::WSAController::Toucher::swipe(double sx, double sy, double ex, double ey, int dur)
+{
+    if (!m_inited) return false;
+
+    if (dur < 40) {
+        return false;
+    }
+
+    m_locker.lock();
+
+    if (m_msgs.size() > max_queue_length) {
+        m_locker.unlock();
+        return false;
+    }
+
+    constexpr int nslices = 6;
+    auto dx = (ex - sx) / nslices, dy = (ey - sy) / nslices;
+    auto dur_slice = dur / nslices;
+
+    m_msgs.push({ 100, 0, 0 });
+    for (int i = 0; i < 5; i++)
+        m_msgs.push({ 1, (int)sx, (int)sy });
+    linear_interpolate(sx, sy, dx, dy, 0, dur_slice, nslices);
+
+    m_msgs.push({ 2, (int)sx, (int)sy });
+    m_locker.unlock();
+
+    return true;
+}
+
+bool asst::WSAController::Toucher::swipe_with_pause(double sx, double sy, double ex, double ey, int dur)
+{
+    if (!m_inited) return false;
+
+    if (dur < 40) {
+        return false;
+    }
+
+    m_locker.lock();
+
+    if (m_msgs.size() > max_queue_length) {
+        m_locker.unlock();
+        return false;
+    }
+
+    constexpr int nslices = 6;
+    constexpr int neslices = 6;
+    constexpr int extra_slice_size = 3;
+    ex += extra_slice_size, ey += extra_slice_size;
+    constexpr double slide_time_ratio = 3. / 5.;
+
+    auto dx = (ex - sx) / nslices, dy = (ey - sy) / nslices;
+    auto ux = std::abs(dx) / dx, uy = std::abs(dy) / dy;
+
+    auto dur_slice = dur * slide_time_ratio / nslices;
+
+    m_msgs.push({ 100, 0, 0 });
+    for (int i = 0; i < 5; i++)
+        m_msgs.push({ 1, (int)sx, (int)sy });
+    auto time_stamp = linear_interpolate(sx, sy, dx, dy, 0, dur_slice, nslices);
+
+    dx = -ux * extra_slice_size / neslices, dy = -uy * extra_slice_size / neslices;
+    time_stamp = linear_interpolate(sx, sy, dx, dy, time_stamp, dur_slice, neslices);
+    linear_interpolate(sx, sy, 0, 0, time_stamp, 0, 10);
+
+    m_msgs.push({ 2, (int)sx, (int)sy });
+    m_locker.unlock();
+
+    return true;
+}
+
+void asst::WSAController::Toucher::press(int key)
+{
+    m_msgs.push({ -1, key, 0 });
+}
+
+void asst::WSAController::Toucher::wait()
+{
+    while (!m_msgs.empty())
+        ;
+}
+
+void asst::WSAController::Toucher::run()
+{
+    SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+    using namespace std::chrono;
+    time_point<high_resolution_clock, milliseconds> tic;
+    time_point<high_resolution_clock, milliseconds> toc;
+
+    while (m_running.load()) {
+        if (m_msgs.empty()) {
+            Sleep(50);
+            continue;
+        }
+
+        m_locker.lock();
+
+        auto command = m_msgs.front();
+        LRESULT res = 0;
+        command.y += m_caption_height;
+        LPARAM coord = MAKELPARAM(command.x, command.y);
+
+        switch (command.type) {
+        case -1:
+            res = SendMessage(m_wnd, WM_KEYDOWN, command.x, 0);
+            Sleep(20);
+            res = SendMessage(m_wnd, WM_KEYUP, command.x, 0);
+            Sleep(20);
+            break;
+        case 0:
+            res = SendMessage(m_wnd, WM_LBUTTONDOWN, 0, coord);
+            Sleep(20);
+            res &= SendMessage(m_wnd, WM_LBUTTONUP, 0, coord);
+            Sleep(20);
+            break;
+        case 100:
+            tic = time_point_cast<milliseconds>(high_resolution_clock::now());
+            res = TRUE;
+            break;
+        case 200:
+            toc = time_point_cast<milliseconds>(high_resolution_clock::now());
+            while (toc - tic <= milliseconds { command.x }) {
+                toc = time_point_cast<milliseconds>(high_resolution_clock::now());
+            }
+            res = TRUE;
+            break;
+        case 1:
+            res = SendMessage(m_wnd, WM_LBUTTONDOWN, 0, coord);
+            break;
+        case 2:
+            res = SendMessage(m_wnd, WM_LBUTTONUP, 0, coord);
+            break;
+        case 3:
+            res = SendMessage(m_wnd, WM_MOUSEMOVE, 0, coord);
+            break;
+        }
+
+        m_msgs.pop();
+        m_locker.unlock();
+    }
+}
+
+double asst::WSAController::Toucher::linear_interpolate(double& sx, double& sy, double dx, double dy, double time_stamp,
+                                                      double dur_slice, double n)
+{
+    for (int i = 1; i <= n; i++) {
+        sx += dx, sy += dy;
+        m_msgs.push({ 200, (int)time_stamp, 0 });
+        m_msgs.push({ 3, (int)sx, (int)sy });
+        time_stamp += dur_slice;
+    }
+    return time_stamp;
+}
+
+#endif // _WIN32
---
+++ src/MaaCore/Controller/WSAController.h
@@ -0,0 +1,188 @@
+#pragma once
+
+#if defined(_WIN32)
+
+#include "ControllerAPI.h"
+
+#include <queue>
+#include <random>
+
+#include "Platform/PlatformFactory.h"
+
+#include <Windows.h>
+#include <d3d11.h>
+
+#include <Windows.Graphics.Capture.Interop.h>
+#include <windows.graphics.capture.h>
+#include <windows.graphics.directx.direct3d11.h>
+#include <windows.graphics.directx.direct3d11.interop.h>
+
+#include <winrt/windows.foundation.h>
+#include <winrt/windows.graphics.capture.h>
+#include <winrt/windows.graphics.directx.direct3d11.h>
+#include <winrt/windows.system.h>
+
+#include "Common/AsstMsg.h"
+#include "InstHelper.h"
+
+namespace asst
+{
+    class WSAController : public ControllerAPI, protected InstHelper
+    {
+    public:
+        WSAController(const AsstCallback& callback, Assistant* inst, PlatformType type);
+        WSAController(const WSAController&) = delete;
+        WSAController(WSAController&&) = delete;
+        virtual ~WSAController();
+
+        virtual bool connect([[maybe_unused]] const std::string& adb_path, [[maybe_unused]] const std::string& address,
+                             [[maybe_unused]] const std::string& config) override;
+
+        virtual void set_resize_window(bool enable) noexcept override;
+        virtual void set_golden_border(bool enable) noexcept override;
+
+        virtual bool inited() const noexcept override { return m_inited; }
+
+        virtual bool screencap(cv::Mat& image_payload, [[maybe_unused]] bool allow_reconnect = false) override;
+
+        virtual bool start_game([[maybe_unused]] const std::string& client_type) override;
+        virtual bool stop_game() override { return false; }
+
+        virtual bool click(const Point& p) override;
+
+        virtual bool swipe(const Point& p1, const Point& p2, int duration = 0, bool extra_swipe = false,
+                           double slope_in = 1, double slope_out = 1, bool with_pause = false) override;
+
+        virtual bool inject_input_event([[maybe_unused]] const InputEvent& event) override { return false; }
+
+        virtual bool press_esc() override;
+        virtual ControlFeat::Feat support_features() const noexcept override
+        {
+            // return ControlFeat::SWIPE_WITH_PAUSE | ControlFeat::PRECISE_SWIPE;
+            return ControlFeat::NONE;
+        }
+
+        virtual std::pair<int, int> get_screen_res() const noexcept override { return m_screen_size; }
+
+        WSAController& operator=(const WSAController&) = delete;
+        WSAController& operator=(WSAController&&) = delete;
+
+    protected:
+        void callback(AsstMsg msg, const json::value& details);
+
+        AsstCallback m_callback;
+
+        std::shared_ptr<asst::PlatformIO> m_platform_io = nullptr;
+
+        std::string m_uuid;
+        std::pair<int, int> m_screen_size = { 0, 0 };
+        int m_width = 0;
+        int m_height = 0;
+        bool m_server_started = false;
+        bool m_inited = false;
+        bool m_resize_window = false;
+        bool m_golden_border = false;
+
+        const std::filesystem::path m_wsapath =
+            R"(..\Local\Microsoft\WindowsApps\WsaClient.exe /launch wsa://com.hypergryph.arknights)";
+
+        class FrameBuffer
+        {
+        public:
+            FrameBuffer() = delete;
+            FrameBuffer(OUT DWORD& last_error);
+            ~FrameBuffer();
+
+            bool prepare(bool resize_window, bool golden_border);
+            bool start_capture();
+            bool end_capture();
+            bool restart_capture();
+            bool get_once(cv::Mat payload);
+
+        public:
+            std::pair<int, int> pub_size; // 输出大小
+            HWND pub_wndwsa;
+            int pub_caption_height;
+
+        private:
+            void process_frame(winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool const& framePool,
+                               [[maybe_unused]] winrt::Windows::Foundation::IInspectable const& object);
+
+        private:
+            winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool m_frame_pool;
+            winrt::Windows::Graphics::Capture::GraphicsCaptureSession m_session;
+            winrt::Windows::Graphics::Capture::GraphicsCaptureItem m_item;
+
+            winrt::com_ptr<ID3D11Device> m_native_device;
+            winrt::com_ptr<ID3D11DeviceContext> m_context;
+            winrt::com_ptr<ID3D11Texture2D> m_staging_texture;
+            winrt::Windows::Graphics::DirectX::Direct3D11::IDirect3DDevice m_device;
+
+            winrt::Windows::Graphics::SizeInt32 m_size; // 输入大小
+
+            const platform::os_string arknights_classname =
+                platform::to_osstring(std::string("com.hypergryph.arknights"));
+
+            std::mutex m_locker;
+            cv::Mat m_front, m_back;
+            winrt::event_token m_frame_arrived;
+            size_t m_pitch;
+            cv::Rect m_arknights_roi;
+            bool m_golden_border;
+
+        } m_wgc; // Windows Graphics Capture member class;
+        DWORD m_last_error;
+
+        class Toucher
+        {
+        public:
+            ~Toucher();
+
+            void init(HWND, int caption_height);
+
+            bool click(int x, int y);
+            bool swipe(double sx, double sy, double ex, double ey, int dur);
+            bool swipe_with_pause(double sx, double sy, double ex, double ey, int dur);
+            void press(int key);
+            void wait();
+
+            void run();
+
+        private:
+            struct MsgUnit
+            {
+                int type;
+                int x, y;
+                MsgUnit(std::initializer_list<int> list)
+                {
+                    auto beg = list.begin();
+                    type = *beg++;
+                    x = *beg++;
+                    y = *beg++;
+                }
+            };
+
+            double linear_interpolate(double& sx, double& sy, double dx, double dy, double time_stamp, double dur_slice,
+                                      double n);
+
+        private:
+            bool m_inited = false;
+
+            HWND m_wnd;
+            int m_caption_height;
+
+            std::thread m_thread;
+            std::mutex m_locker;
+            std::atomic_bool m_running;
+            std::queue<MsgUnit> m_msgs;
+            std::vector<double> random_end;
+
+            std::unique_ptr<IOHandler> m_shell;
+
+            const size_t max_queue_length = 1024;
+
+        } m_touch;
+    };
+} // namespace asst
+
+#endif // _WIN32
