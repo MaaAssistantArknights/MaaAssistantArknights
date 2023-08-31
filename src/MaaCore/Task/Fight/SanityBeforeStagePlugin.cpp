7d026ee55652ff780a4678d2dafc28a88607c0e3
status102 <status102@outlook.com>
2023-08-31 13:51:40 +0800

    feat: 增加刷理智每次打关卡之前时理智剩余量输出

---
+++ src/MaaCore/Task/Fight/SanityBeforeStagePlugin.cpp
@@ -0,0 +1,59 @@
+#include "SanityBeforeStagePlugin.h"
+
+#include <regex>
+
+#include "Utils/ImageIo.hpp"
+#include "Utils/Logger.hpp"
+#include "Vision/OCRer.h"
+#include "Controller/Controller.h"
+#include "Config/TaskData.h"
+
+bool asst::SanityBeforeStagePlugin::verify(AsstMsg msg, const json::value& details) const
+{
+    // SubTaskStart会在主任务的操作部分之前触发，想不通，但是如果哪天改了记得来这里修
+    if (msg != AsstMsg::SubTaskStart || details.get("subtask", std::string()) != "ProcessTask") {
+        return false;
+    }
+
+    const std::string task = details.at("details").at("task").as_string();
+    if (task == "FightBegin") {
+        //m_cur_info = std::move(details);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool asst::SanityBeforeStagePlugin::_run() {
+    LogTraceFunction;
+
+    get_sanity();
+
+    return true;
+}
+
+/// <summary>
+/// 获取 当前理智/最大理智
+/// </summary>
+void asst::SanityBeforeStagePlugin::get_sanity()
+{
+    LogTraceFunction;
+
+    // 直接摘抄博朗台部分，DrGrandetTaskPlugin
+    OCRer analyzer(ctrler()->get_image());
+    analyzer.set_task_info("SanityMatch");
+    analyzer.set_replace(Task.get<OcrTaskInfo>("NumberOcrReplace")->replace_map);
+    analyzer.set_use_char_model(false);
+
+    if (!analyzer.analyze()) {
+        return;
+    }
+    auto text = analyzer.get_result().front().text;
+
+    Log.info("Current Sanity:" + text);
+
+    json::value sanity_info = basic_info_with_what("SanityBeforeStage");
+    sanity_info["details"]["sanity"] = text;
+    callback(AsstMsg::SubTaskExtraInfo, sanity_info);
+}
