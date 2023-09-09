9ac8c487d4e776a9b5ccbc962a7a44d73429ed13
status102 <status102@outlook.com>
2023-09-09 14:01:56 +0800

    feat: 新增自动编队时，自动补充低信赖干员选项

--- src/MaaCore/Task/Miscellaneous/BattleFormationTask.h
+++ src/MaaCore/Task/Miscellaneous/BattleFormationTask.h
@@ -25,6 +25,7 @@ namespace asst
         void append_additional_formation(AdditionalFormation formation);
 
         void set_support_unit_name(std::string name);
+        void set_add_trust(bool add_trust);
 
         enum class DataResource
         {
@@ -37,8 +38,10 @@ namespace asst
         using OperGroup = std::vector<battle::OperUsage>;
 
         virtual bool _run() override;
-
+        // 追加附加干员（按部署费用等小分类）
         bool add_additional();
+        // 补充刷信赖的干员，从最小的开始
+        bool add_trust_operators();
 
         bool enter_selection_page();
         bool select_opers_in_cur_page(std::vector<OperGroup>& groups);
@@ -53,6 +56,10 @@ namespace asst
 
         std::string m_stage_name;
         std::unordered_map<battle::Role, std::vector<OperGroup>> m_formation;
+        // 编队中干员个数
+        int m_operators_in_formation = 0;
+        // 是否需要追加信赖干员
+        bool m_add_trust = false;
         std::string m_support_unit_name;
         DataResource m_data_resource = DataResource::Copilot;
         std::vector<AdditionalFormation> m_additional;
