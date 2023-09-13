3ef4601b1200d177fef310e347332f166c6ef224
枫雨 <35213527+moomiji@users.noreply.github.com>
2023-09-13 15:12:01 +0800

    fix: 超链接点击报错

--- src/MaaWpfGui/Views/UI/ErrorView.xaml.cs
+++ src/MaaWpfGui/Views/UI/ErrorView.xaml.cs
@@ -109,7 +109,7 @@ namespace MaaWpfGui.Views.UI
 
         private void Hyperlink_OnClick(object sender, RoutedEventArgs e)
         {
-            Process.Start(((Hyperlink)sender).NavigateUri.AbsoluteUri);
+            Process.Start(new ProcessStartInfo(((Hyperlink)sender).NavigateUri.AbsoluteUri) { UseShellExecute = true });
         }
 
         private void CopyToClipboard()
