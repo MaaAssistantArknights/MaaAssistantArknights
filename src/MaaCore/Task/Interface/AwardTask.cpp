f84096aeaddcf37a00ddc381af1e490e27b61a4a
broken_paint <youqii0522@gmail.com>
2023-09-19 18:33:31 +0800

    feat: 更改领取邮件功能

--- src/MaaCore/Task/Interface/AwardTask.cpp
+++ src/MaaCore/Task/Interface/AwardTask.cpp
@@ -4,9 +4,28 @@
 
 #include "Task/ProcessTask.h"
 
-asst::AwardTask::AwardTask(const AsstCallback& callback, Assistant* inst) : InterfaceTask(callback, inst, TaskType)
+#include "Utils/Logger.hpp"
+
+asst::AwardTask::AwardTask(const AsstCallback& callback, Assistant* inst) :
+    InterfaceTask(callback, inst, TaskType),
+    award_task_ptr(std::make_shared<ProcessTask>(m_callback, m_inst, TaskType)), 
+    mail_task_ptr(std::make_shared<ProcessTask>(m_callback, m_inst, TaskType))
 {
-    auto award_task_ptr = std::make_shared<ProcessTask>(m_callback, m_inst, TaskType);
+    LogTraceFunction;
+
     award_task_ptr->set_tasks({ "AwardBegin" });
+    mail_task_ptr->set_tasks({ "MailBegin" });
+
     m_subtasks.emplace_back(award_task_ptr);
+    m_subtasks.emplace_back(mail_task_ptr);
+}
+
+bool asst::AwardTask::set_params(const json::value& params)
+{
+    LogTraceFunction;
+
+    bool mail = params.get("mail", true);
+    mail_task_ptr->set_enable(mail);
+
+    return true;
 }
