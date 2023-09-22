ede0e2a146ec3dfadec1bef357663db3c5b17884
mole828 <mrx800@163.com>
2023-09-22 23:06:55 +0800

    feat: 在非烧水模式，取消刷直升按钮

--- src/MaaWpfGui/ViewModels/UI/SettingsViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/SettingsViewModel.cs
@@ -1587,7 +1587,7 @@ namespace MaaWpfGui.ViewModels.UI
         private string _roguelikeMode = ConfigurationHelper.GetValue(ConfigurationKeys.RoguelikeMode, "0");
 
         /// <summary>
-        /// 策略，往后打 / 刷一层就退
+        /// 策略，往后打 / 刷一层就退 / 烧开水
         /// </summary>
         public string RoguelikeMode
         {
@@ -1596,6 +1596,9 @@ namespace MaaWpfGui.ViewModels.UI
             {
                 SetAndNotify(ref _roguelikeMode, value);
                 ConfigurationHelper.SetValue(ConfigurationKeys.RoguelikeMode, value);
+
+                // 烧开水模式可选项
+                this.RoguelikeStartWithEliteTwoEnable = mapRoguelikeStartWithEliteTwoEnable(value);
             }
         }
 
@@ -1674,6 +1677,31 @@ namespace MaaWpfGui.ViewModels.UI
                 SetAndNotify(ref _roguelikeStartWithEliteTwo, value.ToString());
                 ConfigurationHelper.SetValue(ConfigurationKeys.RoguelikeStartWithEliteTwo, value.ToString());
             }
+        }
+
+        private string _roguelikeStartWithEliteTwoEnable = mapRoguelikeStartWithEliteTwoEnable(ConfigurationHelper.GetValue(ConfigurationKeys.RoguelikeMode, "0"));
+
+        public string RoguelikeStartWithEliteTwoEnable
+        {
+            get
+            {
+                return _roguelikeStartWithEliteTwoEnable;
+            }
+
+            set
+            {
+                SetAndNotify(ref _roguelikeStartWithEliteTwoEnable, value);
+            }
+        }
+
+        private static string mapRoguelikeStartWithEliteTwoEnable(string mode)
+        {
+            if (mode == "4")
+            {
+                return "Collapsed";
+            }
+
+            return "Visible";
         }
 
         private string _roguelikeUseSupportUnit = ConfigurationHelper.GetValue(ConfigurationKeys.RoguelikeUseSupportUnit, false.ToString());
