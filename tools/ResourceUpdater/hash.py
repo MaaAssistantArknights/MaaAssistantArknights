81691cca46a53542bc08aa4d00066d92262ecd81
AnnAngela <naganjue@vip.qq.com>
2023-10-12 17:35:12 +0800

    Update hash.py

--- tools/ResourceUpdater/hash.py
+++ tools/ResourceUpdater/hash.py
@@ -1,42 +1,48 @@
-import sys
-import os
 from pathlib import Path
 import hashlib
 
-if len(sys.argv) < 3:
-    print("Usage: python3 list.py <resource> <output>")
-    exit(1)
-
-resource = str(Path(__file__).parents[2]) + "/resource"
+resource = str(Path(__file__).parents[2])
 output = resource + "/hash.txt"
 
-def listfiles(path, prefix):
-    files = []
-    for f in os.listdir(path):
-        if not f.endswith('.png') and not f.endswith('.json'):
-            continue
-        full_path = path / f
-        sha512_hash = hashlib.sha512()
+files = [
+    "resource/Arknights-Tile-Pos/overview.json",
+    "resource/stages.json",
+    "resource/recruitment.json",
+    "resource/item_index.json",
+    "resource/battle_data.json",
+    "resource/infrast.json",
+    "resource/version.json",
+    "resource/global/YoStarJP/resource/recruitment.json",
+    "resource/global/YoStarJP/resource/item_index.json",
+    "resource/global/YoStarJP/resource/version.json",
+    "resource/global/YoStarEN/resource/recruitment.json",
+    "resource/global/YoStarEN/resource/item_index.json",
+    "resource/global/YoStarEN/resource/version.json",
+    "resource/global/YoStarKR/resource/recruitment.json",
+    "resource/global/YoStarKR/resource/item_index.json",
+    "resource/global/YoStarKR/resource/version.json",
+    "resource/global/txwy/resource/recruitment.json",
+    "resource/global/txwy/resource/item_index.json",
+    "resource/global/txwy/resource/version.json"
+]
+results = []
+for f in files:
+    full_path = path / f
+    sha512_hash = hashlib.sha512()
 
-        with open(full_path, "rb") as file:
-            # Read the file in chunks to handle large files
-            while True:
-                data = file.read(65536)  # 64KB chunks
-                if not data:
-                    break
-                sha512_hash.update(data)
+    with open(full_path, "rb") as file:
+        while True:
+            data = file.read(65536)
+            if not data:
+                break
+            sha512_hash.update(data)
         
-        # Append the SHA-512 hash to the filename
-        filename_with_hash = f"{prefix}{f}\t{sha512_hash.hexdigest()}"
-        files.append(filename_with_hash)
+    filename_with_hash = f"{prefix}{f}\t{sha512_hash.hexdigest()}"
+    results.append(filename_with_hash)
     return files
 
-files = listfiles(resource / "template" / "infrast", "resource/template/infrast/")
-files += listfiles(resource / "template" / "items", "resource/template/items/")
-files += listfiles(resource / "Arknights-Tile-Pos", "resource/Arknights-Tile-Pos/")
-
-context = '\n'.join(files)
+context = '\n'.join(results)
 print(context)
 
 with open(output, 'w') as f:
-    f.write(context)
+    f.write(results)
