8cf76efb1186d2fe307d9fb396dce532562270b5
status102 <status102@outlook.com>
2023-10-15 10:21:28 +0800

    refactor: 迁移使用了MaaCore的方法，统一合并至MaaService

--- src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
@@ -28,6 +28,7 @@ using MaaWpfGui.Constants;
 using MaaWpfGui.Helper;
 using MaaWpfGui.Main;
 using MaaWpfGui.Models;
+using MaaWpfGui.Services;
 using MaaWpfGui.States;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -52,9 +53,6 @@ namespace MaaWpfGui.ViewModels.UI
             _runningState = RunningState.Instance;
         }
 
-        [DllImport("MaaCore.dll")]
-        private static extern IntPtr AsstGetVersion();
-
         private static readonly ILogger _logger = Log.ForContext<VersionUpdateViewModel>();
 
         private static string AddContributorLink(string text)
@@ -68,7 +66,7 @@ namespace MaaWpfGui.ViewModels.UI
             return Regex.Replace(text, @"([^\[`]|^)@([^\s]+)", "$1[@$2](https://github.com/$2)");
         }
 
-        private readonly string _curVersion = Marshal.PtrToStringAnsi(AsstGetVersion());
+        private readonly string _curVersion = Marshal.PtrToStringAnsi(MaaService.AsstGetVersion());
         private string _latestVersion;
 
         private string _updateTag = ConfigurationHelper.GetValue(ConfigurationKeys.VersionName, string.Empty);
