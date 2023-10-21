c53350f299929ca29010ab6f2fd49c4571d93627
quyansiyuanwang <132176401+quyansiyuanwang@users.noreply.github.com>
2023-10-21 22:17:34 +0800

    perf & refactor: 重构 tools/AutoLocalization (#6995)
    其中包含部分更优化和跟规范的算法或结构
    
    ---------
    
    Co-authored-by: David <56174894+Constrat@users.noreply.github.com>

---
+++ tools/AutoLocalization/tests/__init__.py
@@ -0,0 +1,5 @@
+from _test_all import TestAll
+from test_translate import TestChatTranslator
+from test_xaml_load import TestXamlParser
+
+__all__ = ['TestAll', 'TestChatTranslator', 'TestXamlParser']
