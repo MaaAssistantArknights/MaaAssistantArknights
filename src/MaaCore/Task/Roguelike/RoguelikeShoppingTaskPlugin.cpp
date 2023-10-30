88e703494cc293ad7c54f63302259825726f7963
status102 <status102@outlook.com>
2023-10-30 22:23:26 +0800

    perf: 投资模式不进行商店购物

--- src/MaaCore/Task/Roguelike/RoguelikeShoppingTaskPlugin.cpp
+++ src/MaaCore/Task/Roguelike/RoguelikeShoppingTaskPlugin.cpp
@@ -38,6 +38,20 @@ bool asst::RoguelikeShoppingTaskPlugin::_run()
 {
     LogTraceFunction;
 
+    auto mode = m_roguelike_data->get_roguelike_mode();
+    bool bought = false;
+    if ((mode == RoguelikeMode::Investment) || shopping(bought)) {
+        if (!bought) {
+            // 如果什么都没买，即使有商品，说明也是不需要买的，这里强制离开商店，后面让 ProcessTask 继续跑
+            return ProcessTask(*this, { "RoguelikeTraderShoppingOver" }).run();
+        }
+    }
+
+    return true;
+}
+
+bool asst::RoguelikeShoppingTaskPlugin::shopping(bool& bought)
+{
     OCRer analyzer;
     analyzer.set_task_info("RoguelikeTraderShoppingOcr");
     auto image = ctrler()->get_image();
@@ -103,7 +117,6 @@ bool asst::RoguelikeShoppingTaskPlugin::_run()
         }
     }
 
-    bool bought = false;
     auto& all_goods = RoguelikeShopping.get_goods(m_roguelike_data->get_theme());
     std::vector<std::string> all_foldartal = m_roguelike_data->get_theme() == "Sami"
                                                  ? Task.get<OcrTaskInfo>("Sami@Roguelike@FoldartalGainOcr")->text
@@ -188,11 +201,5 @@ bool asst::RoguelikeShoppingTaskPlugin::_run()
         }
         break;
     }
-
-    if (!bought) {
-        // 如果什么都没买，即使有商品，说明也是不需要买的，这里强制离开商店，后面让 ProcessTask 继续跑
-        return ProcessTask(*this, { "RoguelikeTraderShoppingOver" }).run();
-    }
-
     return true;
 }
--- src/MaaCore/Task/Roguelike/RoguelikeShoppingTaskPlugin.h
+++ src/MaaCore/Task/Roguelike/RoguelikeShoppingTaskPlugin.h
@@ -13,5 +13,6 @@ namespace asst
 
     protected:
         virtual bool _run() override;
+        bool shopping(bool& bought);
     };
 }
