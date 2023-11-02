0a69f8aec2433a45f5dbde5cb0f8462eb879b02c
youqii <youqii0522@gmail.com>
2023-11-02 20:38:47 +0800

    fix: 修复变量初始化顺序

--- src/MaaCore/Task/Interface/InfrastTask.h
+++ src/MaaCore/Task/Interface/InfrastTask.h
@@ -45,8 +45,8 @@ namespace asst
         std::shared_ptr<InfrastControlTask> m_control_task_ptr = nullptr;
         std::shared_ptr<InfrastReceptionTask> m_reception_task_ptr = nullptr;
         std::shared_ptr<InfrastOfficeTask> m_office_task_ptr = nullptr;
-        std::shared_ptr<InfrastTrainingTask> m_training_task_ptr = nullptr;
         std::shared_ptr<InfrastProcessingTask> m_processing_task_ptr = nullptr;
+        std::shared_ptr<InfrastTrainingTask> m_training_task_ptr = nullptr;
         std::shared_ptr<InfrastDormTask> m_dorm_task_ptr = nullptr;
         std::shared_ptr<ReplenishOriginiumShardTaskPlugin> m_replenish_task_ptr = nullptr;
     };
