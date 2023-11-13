0972e4ba09a5663dd92cd9c21856264b539b02d8
status102 <status102@outlook.com>
2023-11-13 22:21:11 +0800

    feat: 新增肉鸽结算输出并保存截图至debug文件夹

---
+++ src/MaaCore/Task/Roguelike/RoguelikeSettlementTaskPlugin.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "AbstractRoguelikeTaskPlugin.h"
+
+namespace asst
+{
+    class RoguelikeSettlementTaskPlugin : public AbstractRoguelikeTaskPlugin
+    {
+    public:
+        using AbstractRoguelikeTaskPlugin::AbstractRoguelikeTaskPlugin;
+        virtual ~RoguelikeSettlementTaskPlugin() override = default;
+        virtual bool verify(AsstMsg msg, const json::value& details) const override;
+
+    private:
+        virtual bool _run() override;
+        bool get_settlement_info(json::value& info, const cv::Mat& image);
+        bool wait_for_whole_page(cv::Mat& image);
+
+        mutable bool m_game_pass = false;
+    };
+} // namespace asst
