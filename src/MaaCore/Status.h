da1d94954056b159eb4bfe21330556344ecdc9c9
David <56174894+Constrat@users.noreply.github.com>
2023-11-18 12:56:30 +0100

    Revert "fix: 移除Status中的变量"
    This reverts commit c39d4500db2abc6f814a72cf875aa9b7f70e209a.

--- src/MaaCore/Status.h
+++ src/MaaCore/Status.h
@@ -44,6 +44,8 @@ namespace asst
         static inline const std::string RoguelikeCharOverview = "RoguelikeOverview";
         static inline const std::string RoguelikeUseSupport = "RoguelikeUseSupport";
         static inline const std::string RoguelikeUseNonfriendSupport = "RoguelikeUseNonfriendSupport";
+        static inline const std::string RoguelikeFoldartalOverview = "RoguelikeFoldartalOverview";
+        static inline const std::string RoguelikeFoldartalFloor = "RoguelikeFoldartalFloor";
 
         static inline const std::string ProcessTaskLastTimePrefix = "#LastTime#";
 
