c2acd09b76ecc75ffc0c0aab7ff88b5512ba60ed
uye <99072975+ABA2396@users.noreply.github.com>
2023-12-11 18:13:13 +0800

    perf: 遇到问题时提前结束选取密文板 (#7501)
    Co-authored-by: status102 <status102@outlook.com>

--- src/MaaCore/Task/Roguelike/RoguelikeFoldartalUseTaskPlugin.h
+++ src/MaaCore/Task/Roguelike/RoguelikeFoldartalUseTaskPlugin.h
@@ -18,19 +18,28 @@ namespace asst
 
     private:
         // 遍历配置里的组合
-        bool search_enable_pair(std::vector<std::string>& list, const asst::RoguelikeFoldartalCombination& usage);
-        // 查询是否有能使用的板子对
-        bool board_pair(const std::string& up_board, const std::string& down_board);
+        void use_enable_pair(std::vector<std::string>& list, const asst::RoguelikeFoldartalCombination& usage);
+        // 使用板子对结果
+        enum class UseBoardResult
+        {
+            ClickFoldartalError,
+            UseBoardResultSuccess,
+            UpBoardNotFound,
+            DownBoardNotFound,
+            StageNotFound,
+            CanNotUseConfirm,
+            UnknownError,
+        };
         // 使用板子对
-        bool use_board(const std::string& up_board, const std::string& down_board);
+        UseBoardResult use_board(const std::string& up_board, const std::string& down_board) const;
         // 找到并点击指定板子
-        bool search_and_click_board(const std::string& board);
+        bool search_and_click_board(const std::string& board) const;
         // 找到并点击指定节点
-        bool search_and_click_stage();
+        bool search_and_click_stage() const;
         // 滑到最上面
-        void swipe_to_top();
-        // 往下滑
-        void slowly_swipe(bool to_down, int swipe_dist = 200);
+        void swipe_to_top() const;
+        // 缓慢滑动
+        void slowly_swipe(bool direction, int swipe_dist = 200) const;
         // 节点类型
         mutable std::string m_stage;
     };
