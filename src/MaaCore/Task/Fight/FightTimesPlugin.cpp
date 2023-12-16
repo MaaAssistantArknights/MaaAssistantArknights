e033ce0aac72668dccdd3aa423b31e8e89a0470e
status102 <status102@outlook.com>
2023-12-16 15:21:26 +0800

    fix: 修复部分情况下连续战斗次数无法正确调为1并导致卡在该流程的错误 (#7646)

--- src/MaaCore/Task/Fight/FightTimesPlugin.cpp
+++ src/MaaCore/Task/Fight/FightTimesPlugin.cpp
@@ -16,17 +16,17 @@ bool asst::FightTimesPlugin::_run()
 {
     const static std::string FightSeriesOpenTask = "FightSeries-Indicator";
     const static std::string FightSeriesValidTask = "FightSeries-Icon";
-    auto img = ctrler()->get_image();
-    // 目前也可以做成subtask，但是给以后糊屎留个地方
-    auto check_status_task = ProcessTask(*this, { FightSeriesOpenTask, FightSeriesValidTask });
-    check_status_task.set_reusable_image(img).set_retry_times(0);
 
-    if (!check_status_task.run()) { // 认为是外服，无法使用连续战斗
+    auto img = ctrler()->get_image();
+    auto is_valid_task = ProcessTask(*this, { FightSeriesOpenTask, FightSeriesValidTask });
+    is_valid_task.set_reusable_image(img).set_retry_times(0);
+
+    if (!is_valid_task.run()) { // 认为是外服，无法使用连续战斗
         inited = true;
         return true;
     }
 
-    if (check_status_task.get_last_task_name() == FightSeriesValidTask) { // 连续战斗次数选择列表未打开
+    if (is_valid_task.get_last_task_name() == FightSeriesValidTask) { // 连续战斗次数选择列表未打开
         auto task = ProcessTask(*this, { "FightSeries-Open" });
         task.set_reusable_image(img);
         if (!task.run()) {
