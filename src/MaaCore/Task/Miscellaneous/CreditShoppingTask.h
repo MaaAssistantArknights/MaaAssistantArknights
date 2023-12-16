4998789a77cf7b3e2b68cce952f4fcd22422b578
liupei <2000013169@stu.pku.edu.cn>
2023-12-17 00:43:49 +0800

    i18n: translation

--- src/MaaCore/Task/Miscellaneous/CreditShoppingTask.h
+++ src/MaaCore/Task/Miscellaneous/CreditShoppingTask.h
@@ -28,11 +28,11 @@ namespace asst
         virtual bool _run() override;
         bool m_force_shopping_if_credit_full = false; // 设置是否防止信用值溢出
         bool m_only_buy_discount = false; // 设置只购买折扣信用商品（未打折的白名单物品仍会购买）
-        bool m_reserve_max_credit = false; // 设置消耗信用点至300以下时停止购买商品
+        bool m_reserve_max_credit = false; // 设置消耗信用点至300以下时停止购买商品（仍然会购买白名单物品）
         bool m_info_credit_full = false; // 设置是否在不购买黑名单物品阶段通知信用点溢出
         int credit_ocr();
 
-        // 用于识别商品信息右上角的折扣信息（需要先点开具体信用商品,且设置sleep防止点开商品的延迟导致截图识别错误）
+        // 用于识别商品信息右上角的折扣信息（需要先点开具体信用商品,且设置sleep(time)防止点开商品的延迟导致截图识别错误）
         int discount_ocr(); 
 
         bool credit_shopping(bool white_list_enabled, bool credit_ocr_enabled);
