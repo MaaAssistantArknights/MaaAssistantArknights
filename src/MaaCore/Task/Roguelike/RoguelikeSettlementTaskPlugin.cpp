75e9f4d0d40474a3ffce30346a4e4f5c27eb87e3
status102 <status102@outlook.com>
2023-12-19 10:54:29 +0800

    fix: 改用views::join

--- src/MaaCore/Task/Roguelike/RoguelikeSettlementTaskPlugin.cpp
+++ src/MaaCore/Task/Roguelike/RoguelikeSettlementTaskPlugin.cpp
@@ -92,7 +92,7 @@ bool asst::RoguelikeSettlementTaskPlugin::get_settlement_info(json::value& info,
         const auto& task_replace = Task.get<OcrTaskInfo>(task_name)->replace_map;
 
         using ReplaceMap = std::vector<std::pair<std::string, std::string>>;
-        auto merge_view = ranges::join_view(std::array { number_replace, task_replace });
+        auto merge_view = std::array { number_replace, task_replace } | views::join;
         ocr.set_replace(ReplaceMap(merge_view.begin(), merge_view.end()));
         if (!ocr.analyze()) {
             Log.error(__FUNCTION__, "analyze battle data failed, task:", task_name);
