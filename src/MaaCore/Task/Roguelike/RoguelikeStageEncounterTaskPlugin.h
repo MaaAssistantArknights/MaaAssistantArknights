988e4c97a16d1b33d1bc0fd5483d870154db9795
zzyyyl <aysyxx53@hotmail.com>
2024-01-07 13:50:40 +0800

    perf: 将实现移入 .cpp 并稍作优化

--- src/MaaCore/Task/Roguelike/RoguelikeStageEncounterTaskPlugin.h
+++ src/MaaCore/Task/Roguelike/RoguelikeStageEncounterTaskPlugin.h
@@ -17,47 +17,8 @@ namespace asst
 
     protected:
         virtual bool _run() override;
-        static bool satisfies_condition(const asst::ChoiceRequire& requirement, int special_val)
-        {
-            if (requirement.vision.type != ComparisonType::Unsupported) {
-                int num = 0;
-                switch (requirement.vision.type) {
-                case ComparisonType::GreaterThan:
-                    return utils::chars_to_number(requirement.vision.value, num) && special_val > num;
-                case ComparisonType::LessThan:
-                    return utils::chars_to_number(requirement.vision.value, num) && special_val < num;
-                case ComparisonType::Equal:
-                    return utils::chars_to_number(requirement.vision.value, num) && special_val == num;
-                case ComparisonType::Unsupported:
-                    return false;
-                }
-            }
-            return false;
-        }
-
-        static int process_task(const asst::RoguelikeEvent& event, const int special_val)
-        {
-            for (const auto& requirement : event.choice_require) {
-                if (requirement.choose == -1) continue;
-                if (satisfies_condition(requirement, special_val)) {
-                    return requirement.choose;
-                }
-            }
-            return event.default_choose;
-        }
-
-        static int hp(const cv::Mat& image)
-        {
-            int hp_val = -1;
-            asst::OCRer analyzer(image);
-            analyzer.set_task_info("Roguelike@HpRecognition");
-            analyzer.set_replace(Task.get<OcrTaskInfo>("NumberOcrReplace")->replace_map);
-            analyzer.set_use_char_model(true);
-
-            if (!analyzer.analyze()) {
-                return -1;
-            }
-            return utils::chars_to_number(analyzer.get_result().front().text, hp_val) ? hp_val : 0;
-        }
+        static bool satisfies_condition(const asst::ChoiceRequire& requirement, int special_val);
+        static int process_task(const asst::RoguelikeEvent& event, const int special_val);
+        static int hp(const cv::Mat& image);
     };
 }
