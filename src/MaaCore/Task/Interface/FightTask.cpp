90c3ee6710d9aa59b1fa468dc2a8142e9f818a84
status102 <102887808+status102@users.noreply.github.com>
2024-01-13 12:03:54 +0800

    perf: 限制理智识别重试次数 fix #7998

--- src/MaaCore/Task/Interface/FightTask.cpp
+++ src/MaaCore/Task/Interface/FightTask.cpp
@@ -50,7 +50,7 @@ asst::FightTask::FightTask(const AsstCallback& callback, Assistant* inst)
     m_stage_drops_plugin_ptr->set_retry_times(0);
     m_dr_grandet_task_plugin_ptr = m_fight_task_ptr->register_plugin<DrGrandetTaskPlugin>();
     m_dr_grandet_task_plugin_ptr->set_enable(false);
-    m_fight_task_ptr->register_plugin<SanityBeforeStageTaskPlugin>();
+    m_fight_task_ptr->register_plugin<SanityBeforeStageTaskPlugin>()->set_retry_times(3);
     m_fight_task_ptr->register_plugin<FightTimesTaskPlugin>();
     m_medicine_plugin = m_fight_task_ptr->register_plugin<MedicineCounterTaskPlugin>();
 
