e22aa3a21721f2c6dd4c65323c91c8573c9e9f6b
SherkeyXD <253294679@qq.com>
2024-02-04 02:58:35 +0800

    docs: 升级依赖，调整视觉效果

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,35 +1,35 @@
-import { defineUserConfig } from 'vuepress';
-import Theme from './theme';
-import SearchPlugin from './plugins/search';
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
+import Theme from "./theme";
 
 export default defineUserConfig({
-  base: '/docs/',
+  base: "/docs/",
   lang: "zh-CN",
   title: "MaaAssistantArknights",
   description: "MAA",
   port: 3001,
 
   locales: {
-    '/': {
-      lang: 'zh-CN',
-      description: '开发者文档',
+    "/": {
+      lang: "zh-CN",
+      description: "开发者文档",
     },
-    '/zh-tw/': {
-      lang: 'zh-TW',
-      description: '開發者文件',
+    "/zh-tw/": {
+      lang: "zh-TW",
+      description: "開發者文件",
     },
-    '/en-us/': {
-      lang: 'en-US',
-      title: 'Documents'
+    "/en-us/": {
+      lang: "en-US",
+      title: "Documents",
     },
-    '/ja-jp/': {
-      lang: 'ja-JP',
-      description: 'ドキュメント'
+    "/ja-jp/": {
+      lang: "ja-JP",
+      description: "ドキュメント",
     },
-    '/ko-kr/': {
-      lang: 'ko-KR',
-      description: '문서'
-    }
+    "/ko-kr/": {
+      lang: "ko-KR",
+      description: "문서",
+    },
   },
 
   markdown: {
@@ -40,7 +40,39 @@ export default defineUserConfig({
 
   theme: Theme,
 
-  plugins: [
-    SearchPlugin(),
+  plugins: [],
+
+  head: [
+    ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
+    [
+      "link",
+      { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css?family=Noto+Sans+SC:300,400,500,700,900&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:400;500;700&display=swap",
+        rel: "stylesheet",
+      },
+    ],
+    [
+      "link",
+      {
+        href: "https://fonts.googleapis.com/css2?family=Jetbrains+Mono:wght@300,400,500,700;700&display=swap",
+        rel: "stylesheet",
+      },
+    ],
   ],
+
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
 });
