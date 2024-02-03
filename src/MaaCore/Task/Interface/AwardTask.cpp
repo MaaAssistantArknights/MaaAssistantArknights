e7371f73119bf4838f00d70da3c833c6cd59d0c4
SherkeyXD <253294679@qq.com>
2024-02-03 10:29:27 +0800

    feat: MaaCore部分添加RecruitingActivities支持

--- src/MaaCore/Task/Interface/AwardTask.cpp
+++ src/MaaCore/Task/Interface/AwardTask.cpp
@@ -9,15 +9,18 @@
 asst::AwardTask::AwardTask(const AsstCallback& callback, Assistant* inst)
     : InterfaceTask(callback, inst, TaskType),
       award_task_ptr(std::make_shared<ProcessTask>(m_callback, m_inst, TaskType)),
-      mail_task_ptr(std::make_shared<ProcessTask>(m_callback, m_inst, TaskType))
+      mail_task_ptr(std::make_shared<ProcessTask>(m_callback, m_inst, TaskType)),
+      recruit_task_ptr(std::make_shared<ProcessTask>(m_callback, m_inst, TaskType))
 {
     LogTraceFunction;
 
     award_task_ptr->set_tasks({ "AwardBegin" });
     mail_task_ptr->set_tasks({ "MailBegin" });
+    recruit_task_ptr->set_tasks({ "RecruitingActivitiesBegin" });
 
     m_subtasks.emplace_back(award_task_ptr);
     m_subtasks.emplace_back(mail_task_ptr);
+    m_subtasks.emplace_back(recruit_task_ptr);
 }
 
 bool asst::AwardTask::set_params(const json::value& params)
@@ -26,9 +29,11 @@ bool asst::AwardTask::set_params(const json::value& params)
 
     bool award = params.get("award", true);
     bool mail = params.get("mail", false);
+    bool recruit = params.get("recruit", false);
 
     award_task_ptr->set_enable(award);
     mail_task_ptr->set_enable(mail);
+    recruit_task_ptr->set_enable(recruit);
 
     return true;
 }
