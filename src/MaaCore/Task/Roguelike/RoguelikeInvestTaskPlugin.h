e186653d801e1184ee43abdd7da975e120b92474
status102 <102887808+status102@users.noreply.github.com>
2024-02-03 19:12:19 +0800

    fix: 修复投资超出设定上限

--- src/MaaCore/Task/Roguelike/RoguelikeInvestTaskPlugin.h
+++ src/MaaCore/Task/Roguelike/RoguelikeInvestTaskPlugin.h
@@ -13,9 +13,11 @@ namespace asst
 
     private:
         virtual bool _run() override;
+        std::optional<int> ocr_current_count(const auto& img, const auto& task_name);
         bool is_investment_available(const cv::Mat& image) const;
         bool is_investment_error(const cv::Mat& image) const;
         void stop_roguelike();
+
 
         int m_invest_count = 0;
     };
