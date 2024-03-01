7df2060c3c75c8b065f4129c9f04c1cea36c2e40
FreeSky-X <44790784+FreeSky-X@users.noreply.github.com>
2024-03-01 21:29:41 +0800

    perf: 优化仓库识别速度 (#8113)
    * perf:仓库识别
    
    * perf:仓储识别
    
    * fix:depot遗漏材料
    
    * fix:ci集成
    
    * fix:定位得分低
    
    * feat: update analyze_all_items, format code
    
    ---------
    
    Co-authored-by: Horror Proton <107091537+horror-proton@users.noreply.github.com>

--- src/MaaCore/Task/Miscellaneous/DepotRecognitionTask.cpp
+++ src/MaaCore/Task/Miscellaneous/DepotRecognitionTask.cpp
@@ -5,6 +5,7 @@
 #include <meojson/json.hpp>
 
 #include "Config/GeneralConfig.h"
+#include "Config/Miscellaneous/ItemConfig.h"
 #include "Config/TaskData.h"
 #include "Controller/Controller.h"
 #include "Task/ProcessTask.h"
@@ -25,29 +26,37 @@ bool asst::DepotRecognitionTask::swipe_and_analyze()
 {
     LogTraceFunction;
     m_all_items.clear();
-
+    // 获取item总长度
+    const auto& item_size = ItemData.get_ordered_material_item_id().size();
     size_t pre_pos = 0ULL;
+    cv::Mat index_roi {};
+    // size_t pre_pos = item_size;
     while (true) {
+        // 如果已经遍历完了所有item直接退出
+        if (pre_pos == item_size) {
+            break;
+        }
         DepotImageAnalyzer analyzer(ctrler()->get_image());
 
         auto future = std::async(std::launch::async, [&]() { swipe(); });
 
-        // 因为滑动不是完整的一页，有可能上一次识别过的物品，这次仍然在页面中
-        // 所以这个 begin pos 不能设置
-        // analyzer.set_match_begin_pos(pre_pos);
+        // 第一页不用检索item,后续都需要检索上一页最后一个item
+
+        analyzer.set_match_begin_pos(pre_pos);
+        analyzer.set_index_roi(index_roi);
         if (!analyzer.analyze()) {
             break;
         }
         size_t cur_pos = analyzer.get_match_begin_pos();
-        if (cur_pos == pre_pos || cur_pos == DepotImageAnalyzer::NPos) {
+        if (cur_pos == DepotImageAnalyzer::NPos) {
             break;
         }
         pre_pos = cur_pos;
-
+        index_roi = analyzer.get_index_roi();
         auto cur_result = analyzer.get_result();
         m_all_items.merge(std::move(cur_result));
 
-        future.wait();
+        future.get();
         callback_analyze_result(false);
     }
     return !m_all_items.empty();
