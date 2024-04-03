b4c180f2755b3960cb8e4465c98a83ee27d4529c
uye <99072975+ABA2396@users.noreply.github.com>
2024-04-03 15:31:20 +0800

    perf: Add missing headers & optimize the code

--- src/MaaWpfGui/Services/Notification/BarkNotificationProvider.cs
+++ src/MaaWpfGui/Services/Notification/BarkNotificationProvider.cs
@@ -1,8 +1,18 @@
+// <copyright file="BarkNotificationProvider.cs" company="MaaAssistantArknights">
+// MaaWpfGui - A part of the MaaCoreArknights project
+// Copyright (C) 2021 MistEO and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+// </copyright>
+
+#nullable enable
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.RightsManagement;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -13,16 +23,9 @@ using Serilog;
 
 namespace MaaWpfGui.Services.Notification
 {
-    public class BarkNotificationProvider : IExternalNotificationProvider
+    public class BarkNotificationProvider(IHttpService httpService) : IExternalNotificationProvider
     {
-        private readonly IHttpService _httpService;
-
         private readonly ILogger _logger = Log.ForContext<BarkNotificationProvider>();
-
-        public BarkNotificationProvider(IHttpService httpService)
-        {
-            _httpService = httpService;
-        }
 
         public async Task<bool> SendAsync(string title, string content)
         {
@@ -32,50 +35,63 @@ namespace MaaWpfGui.Services.Notification
                 _logger.Warning("Failed to send Bark notification, Bark send key is empty");
                 return false;
             }
+
             var apiBase = ConfigurationHelper.GetValue(ConfigurationKeys.ExternalNotificationBarkServer, string.Empty);
             if (string.IsNullOrWhiteSpace(apiBase))
             {
                 _logger.Warning("Failed to send Bark notification, Bark server address is empty");
                 return false;
             }
-            var response = await _httpService.PostAsJsonAsync(
+
+            var response = await httpService.PostAsJsonAsync(
                 new Uri(new Uri(apiBase), "/push"),
                 new BarkPostContent { Title = title, Content = content, SendKey = sendKey });
-            var data = JsonSerializer.Deserialize<BarkResponse>(response);
-            if (data.Code != 200)
+            if (response == null)
             {
-                _logger.Warning("Failed to send Bark notification: {Code} {Message}", data.Code, data.Message);
+                _logger.Warning("Failed to send Bark notification, response is null");
                 return false;
             }
 
-            return true;
+            var data = JsonSerializer.Deserialize<BarkResponse>(response);
+            if (data?.Code == 200)
+            {
+                return true;
+            }
+
+            _logger.Warning("Failed to send Bark notification: {Code} {Message}", data?.Code, data?.Message);
+            return false;
         }
 
         private class BarkPostContent
         {
+            // ReSharper disable UnusedAutoPropertyAccessor.Local
+            // ReSharper disable UnusedMember.Local
             [JsonPropertyName("device_key")]
-            public string SendKey { get; set; }
+            public string? SendKey { get; set; }
 
             [JsonPropertyName("title")]
-            public string Title { get; set; }
+            public string? Title { get; set; }
 
             [JsonPropertyName("body")]
-            public string Content { get; set; }
+            public string? Content { get; set; }
 
             [JsonPropertyName("icon")]
-            public string Icon { get => "https://cdn.jsdelivr.net/gh/MaaAssistantArknights/design@main/logo/maa-logo_256x256.png"; }
+            public static string Icon { get => "https://cdn.jsdelivr.net/gh/MaaAssistantArknights/design@main/logo/maa-logo_256x256.png"; }
+
+            // ReSharper restore UnusedAutoPropertyAccessor.Local
+            // ReSharper restore UnusedMember.Local
         }
 
         private class BarkResponse
         {
             [JsonPropertyName("code")]
-            public int Code { get; set; }
+            public int? Code { get; init; }
 
             [JsonPropertyName("message")]
-            public string Message { get; set; }
+            public string? Message { get; init; }
 
             [JsonPropertyName("timestamp")]
-            public long Timestamp { get; set; }
+            public long? Timestamp { get; init; }
         }
     }
 }
