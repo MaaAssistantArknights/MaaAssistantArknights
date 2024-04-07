f03c88f3e1d6afc7b10320a8e9ba82c5c6c9ffdc
HX3N <scarlet7518@gmail.com>
2024-04-07 20:02:33 +0900

    docs: reconstruction

--- docs/.vuepress/navbar/ko-kr.ts
+++ docs/.vuepress/navbar/ko-kr.ts
@@ -9,17 +9,17 @@ export const kokrNavbar = navbar([
   {
     text: "사용자 설명서",
     icon: "mdi:user",
-    link: "/ko-kr/1.1-사용자_설명서",
+    link: "/ko-kr/사용자설명서/사용자설명서",
   },
   {
     text: "개발 관련",
     icon: "ph:code-bold",
-    link: "/ko-kr/2.1-LINUX_튜토리얼",
+    link: "/ko-kr/개발문서/1.개발시작",
   },
   {
     text: "프로토콜 문서",
     icon: "basil:document-solid",
-    link: "/ko-kr/3.1-연동",
+    link: "/ko-kr/스키마/1.연동",
   },
   {
     text: "MAA 공식 홈페이지",
