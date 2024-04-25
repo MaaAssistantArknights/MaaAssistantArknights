da8b1b669f19f7af0b4cd071a97e55d0a597891e
status102 <102887808+status102@users.noreply.github.com>
2024-04-25 20:07:17 +0800

    fix: 增加对地图文件检查，避免只存在索引

--- src/MaaCore/Task/BattleHelper.cpp
+++ src/MaaCore/Task/BattleHelper.cpp
@@ -28,7 +28,7 @@ bool asst::BattleHelper::set_stage_name(const std::string& name)
 {
     LogTraceFunction;
 
-    if (!Tile.find(name)) {
+    if (auto result = Tile.find(name); !result || !json::open(result->second)) {
         return false;
     }
     m_stage_name = name;
@@ -60,7 +60,7 @@ bool asst::BattleHelper::calc_tiles_info(
 {
     LogTraceFunction;
 
-    if (!Tile.find(stage_name)) {
+    if (auto result = Tile.find(stage_name); !result || !json::open(result->second)) {
         return false;
     }
 
@@ -480,6 +480,7 @@ bool asst::BattleHelper::check_pause_button(const cv::Mat& reusable)
     ret &= battle_result_opt && battle_result_opt->pause_button;
     return ret;
 }
+
 bool asst::BattleHelper::check_skip_plot_button(const cv::Mat& reusable)
 {
     cv::Mat image = reusable.empty() ? m_inst_helper.ctrler()->get_image() : reusable;
