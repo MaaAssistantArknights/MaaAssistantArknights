f84ef77a652de7bac8da3a59cfad66a968baa09c
Constrat <56174894+Constrat@users.noreply.github.com.>
2024-05-03 14:21:11 +0200

    feat: first iteration logic

--- src/MaaCore/Task/Miscellaneous/StartGameTaskPlugin.cpp
+++ src/MaaCore/Task/Miscellaneous/StartGameTaskPlugin.cpp
@@ -9,7 +9,15 @@ bool StartGameTaskPlugin::_run()
     if (m_client_type.empty()) {
         return false;
     }
-    return ctrler()->start_game(m_client_type);
+
+    for (int i = 0; i < 4; i++) {
+        if (!ctrler()->start_game(m_client_type)) {
+            return false;
+        }
+        sleep(2000);
+    }
+
+    return true;
 }
 
 StartGameTaskPlugin& StartGameTaskPlugin::set_client_type(std::string client_type) noexcept
