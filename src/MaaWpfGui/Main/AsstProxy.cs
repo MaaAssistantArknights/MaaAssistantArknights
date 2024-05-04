6ffd2846d034bdfc7619ed5bb210b2d0a15935ed
uye <99072975+ABA2396@users.noreply.github.com>
2024-05-04 19:17:24 +0800

    fix: mumu模拟器连接失败
    fix #8986

--- src/MaaWpfGui/Main/AsstProxy.cs
+++ src/MaaWpfGui/Main/AsstProxy.cs
@@ -1577,12 +1577,6 @@ namespace MaaWpfGui.Main
             switch (Instances.SettingsViewModel.ConnectConfig)
             {
                 case "MuMuEmulator12":
-                    if (!Directory.Exists(Instances.SettingsViewModel.MuMuEmulator12Extras.EmulatorPath))
-                    {
-                        error = "MuMu Emulator 12 Path Not Found";
-                        return false;
-                    }
-
                     AsstSetConnectionExtrasMuMu12(Instances.SettingsViewModel.MuMuEmulator12Extras.Config);
                     break;
             }
