d0e3cba92ec45e751fc3dba3d83d169924dac0fe
uye <99072975+ABA2396@users.noreply.github.com>
2024-05-11 17:22:47 +0800

    Revert "perf: mumu12模拟器路径默认值置空"
    This reverts commit 76b03fdda94bffb37c643dc96cc28370520052a1.

--- src/MaaWpfGui/ViewModels/UI/SettingsViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/SettingsViewModel.cs
@@ -3312,7 +3312,7 @@ namespace MaaWpfGui.ViewModels.UI
                 }
             }
 
-            private static readonly string _configuredPath = ConfigurationHelper.GetValue(ConfigurationKeys.MuMu12EmulatorPath, string.Empty);
+            private static readonly string _configuredPath = ConfigurationHelper.GetValue(ConfigurationKeys.MuMu12EmulatorPath, @"C:\Program Files\Netease\MuMuPlayer-12.0");
             private string _emulatorPath = Directory.Exists(_configuredPath) ? _configuredPath : string.Empty;
 
             /// <summary>
