408be0a3c68d8e35d3e5ddffbd00604ae1e1e21e
Rbqwow <55343783+Rbqwow@users.noreply.github.com>
2024-05-17 19:18:16 +0800

    docs: revert zh-tw ja-jp ko-kr

---
+++ docs/.vuepress/navbar/ja-jp.ts
@@ -0,0 +1,29 @@
+import { navbar } from "vuepress-theme-hope";
+
+export const jajpNavbar = navbar([
+  {
+    text: "ホームページ",
+    icon: "ic:round-home",
+    link: "/ja-jp/",
+  },
+  {
+    text: "使用説明",
+    icon: "mdi:user",
+    link: "/ja-jp/使用説明",
+  },
+  {
+    text: "開発関連",
+    icon: "ph:code-bold",
+    link: "/ja-jp/開発関連",
+  },
+  {
+    text: "プロトコルドキュメント",
+    icon: "basil:document-solid",
+    link: "/ja-jp/プロトコルドキュメント",
+  },
+  {
+    text: "MAA 公式サイト",
+    icon: "mdi:cow",
+    link: "https://maa.plus",
+  },
+]);
