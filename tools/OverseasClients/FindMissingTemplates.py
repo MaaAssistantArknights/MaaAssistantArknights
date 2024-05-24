4c6c029423d4f3f8c5a5cf3c5f0f80c09a4760bd
Constrat <56174894+Constrat@users.noreply.github.com.>
2024-05-24 19:59:48 +0200

    chore(tools): updated missingtemplates py for overseas

--- tools/OverseasClients/FindMissingTemplates.py
+++ tools/OverseasClients/FindMissingTemplates.py
@@ -35,9 +35,6 @@ ignore_list_file_name = "ignore_list_of_templates.txt"
 # You may customize here
 regex_ignore_list = [
     #Wait for Overseas release
-    "Sami.png",
-    "MistCity",
-    "Dark",
     "Siege"
 ]
 
