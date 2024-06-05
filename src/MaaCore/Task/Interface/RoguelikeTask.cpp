fabf83f3963382247d02adb12a4a62bd4ea754f5
晓丶梦丶仁 <1020623818@qq.com>
2024-06-05 21:07:37 +0800

    feat: 添加放弃阵容完备度检测功能

--- src/MaaCore/Task/Interface/RoguelikeTask.cpp
+++ src/MaaCore/Task/Interface/RoguelikeTask.cpp
@@ -107,6 +107,9 @@ bool asst::RoguelikeTask::set_params(const json::value& params)
     // 是否生活队凹开局板子
     m_roguelike_config_ptr->set_start_foldartal(params.contains("start_foldartal_list"));
 
+    // 是否跳过阵容完备度检测
+    m_roguelike_config_ptr->set_recruitment_team_complete(params.get("Roguelike.RoguelikeSkipTeamCompleteCheck", false));
+
     if (auto opt = params.find<json::array>("start_foldartal_list"); opt) {
         std::vector<std::string> list;
         for (const auto& name : *opt) {
@@ -187,8 +190,6 @@ bool asst::RoguelikeTask::set_params(const json::value& params)
     m_custom_start_plugin_ptr->set_custom(RoguelikeCustomType::UseSupport,
                                           params.get("use_support", false) ? "1" : "0"); // 开局干员是否为助战干员
     m_custom_start_plugin_ptr->set_custom(RoguelikeCustomType::UseNonfriendSupport,
-                                          params.get("use_nonfriend_support", false) ? "1"
-                                                                                     : "0"); // 是否可以是非好友助战干员
-    m_roguelike_config_ptr->set_recruitment_team_complete(params.get("team_complete", false)); // 是否跳过阵容完备度检测
+                                          params.get("use_nonfriend_support", false) ? "1" : "0"); // 是否可以是非好友助战干员
     return true;
 }
