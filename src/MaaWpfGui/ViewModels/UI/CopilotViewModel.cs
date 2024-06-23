212209172fda8dc463a8c2dec68a7e1114ee8c16
uye <99072975+ABA2396@users.noreply.github.com>
2024-06-23 19:17:36 +0800

    perf: 优化自动战斗相关翻译

--- src/MaaWpfGui/ViewModels/UI/CopilotViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/CopilotViewModel.cs
@@ -115,7 +115,7 @@ namespace MaaWpfGui.ViewModels.UI
         /// <param name="content">The content.</param>
         /// <param name="color">The font color.</param>
         /// <param name="weight">The font weight.</param>
-        /// <param name="showTime">Wether show time.</param>
+        /// <param name="showTime">Whether show time.</param>
         public void AddLog(string content, string color = UiLogColor.Trace, string weight = "Regular", bool showTime = true)
         {
             Execute.OnUIThread(() =>
@@ -299,7 +299,7 @@ namespace MaaWpfGui.ViewModels.UI
             }
 
             // 一旦有由小写字母、数字、'-'组成的name则视为关卡名直接使用
-            var directName = names.FirstOrDefault(name => name.ToLower().All(c => (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-'));
+            var directName = names.FirstOrDefault(name => name.ToLower().All(c => c is >= 'a' and <= 'z' or >= '0' and <= '9' or '-'));
             if (!string.IsNullOrEmpty(directName))
             {
                 return directName;
@@ -309,10 +309,11 @@ namespace MaaWpfGui.ViewModels.UI
             return names.Select(str => regex.Match(str)).FirstOrDefault(result => result.Success)?.Value ?? string.Empty;
         }
 
-        private string _language = ConfigurationHelper.GetValue(ConfigurationKeys.Localization, LocalizationHelper.DefaultLanguage);
 
         private void ParseJsonAndShowInfo(string jsonStr)
         {
+            string language = Instances.SettingsViewModel.Language;
+
             try
             {
                 var json = (JObject?)JsonConvert.DeserializeObject(jsonStr);
@@ -331,14 +332,30 @@ namespace MaaWpfGui.ViewModels.UI
 
                 var doc = (JObject?)json["doc"];
                 string title = string.Empty;
-                bool languageIsGlobal = _language == "en-us" | _language == "ja-jp" | _language == "ko-kr";
+                bool languageIsGlobal = language is "zh-tw" or "en-us" or "ja-jp" or "ko-kr";
 
                 if (doc != null && doc.TryGetValue("title", out var titleValue))
                 {
                     title = titleValue.ToString();
-                    if (languageIsGlobal && doc.TryGetValue("title_global", out var titleGlobalValue))
+
+                    if (language == "zh-tw")
                     {
-                        title = titleGlobalValue.ToString();
+                        if (doc.TryGetValue("title_zh_tw", out var localizedTitleValueZhTw))
+                        {
+                            title = localizedTitleValueZhTw.ToString();
+                        }
+                    }
+                    else if (languageIsGlobal)
+                    {
+                        string localizedTitleKey = $"title_{language.Replace("-", "_")}";
+                        if (doc.TryGetValue(localizedTitleKey, out var localizedTitleValue))
+                        {
+                            title = localizedTitleValue.ToString();
+                        }
+                        else if (doc.TryGetValue("title_global", out var titleGlobalValue))
+                        {
+                            title = titleGlobalValue.ToString();
+                        }
                     }
 
                     CopilotTaskName = FindStageName((IsDataFromWeb ? string.Empty : _filename).Split(Path.DirectorySeparatorChar).LastOrDefault()?.Split('.').FirstOrDefault() ?? string.Empty, title);
@@ -359,9 +376,25 @@ namespace MaaWpfGui.ViewModels.UI
                 if (doc != null && doc.TryGetValue("details", out var detailsValue))
                 {
                     details = detailsValue.ToString();
-                    if (languageIsGlobal && doc.TryGetValue("details_global", out var detailsGlobalValue))
+
+                    if (language == "zh-tw")
                     {
-                        details = detailsGlobalValue.ToString();
+                        if (doc.TryGetValue("details_zh_tw", out var localizedDetailsValueZhTw))
+                        {
+                            details = localizedDetailsValueZhTw.ToString();
+                        }
+                    }
+                    else if (languageIsGlobal)
+                    {
+                        string localizedDetailsKey = $"details_{language.Replace("-", "_")}";
+                        if (doc.TryGetValue(localizedDetailsKey, out var localizedDetailsValue))
+                        {
+                            details = localizedDetailsValue.ToString();
+                        }
+                        else if (doc.TryGetValue("details_global", out var detailsGlobalValue))
+                        {
+                            details = detailsGlobalValue.ToString();
+                        }
                     }
                 }
 
@@ -394,10 +427,8 @@ namespace MaaWpfGui.ViewModels.UI
                 {
                     count++;
 
-                    var localizedName = DataHelper.GetLocalizedCharacterName((string?)oper["name"], _language);
-
-                    string operLog = languageIsGlobal ? $"{localizedName}, skill {oper["skill"]}" : $"{localizedName}, {oper["skill"]} 技能";
-                    AddLog(operLog, UiLogColor.Message, showTime: false);
+                    var localizedName = DataHelper.GetLocalizedCharacterName((string?)oper["name"], language);
+                    AddLog($"{localizedName}, {LocalizationHelper.GetString("CopilotSkill")} {oper["skill"]}", UiLogColor.Message, showTime: false);
                 }
 
                 if (json.TryGetValue("groups", out var groupsValue))
@@ -406,28 +437,17 @@ namespace MaaWpfGui.ViewModels.UI
                     {
                         count++;
                         string groupName = group["name"] + ": ";
-                        var operInfos = group["opers"]!.Cast<JObject>().Select(oper =>
-                        $"{DataHelper.GetLocalizedCharacterName((string?)oper["name"], _language)} {oper["skill"]}").ToList();
+                        var operInfos = group["opers"]!.Cast<JObject>()
+                            .Select(oper => $"{DataHelper.GetLocalizedCharacterName((string?)oper["name"], language)} {oper["skill"]}")
+                            .ToList();
 
                         AddLog(groupName + string.Join(" / ", operInfos), UiLogColor.Message, showTime: false);
                     }
                 }
 
-                if (languageIsGlobal)
-                {
-                    if (count > 1)
-                    {
-                        AddLog($"{count} operators in total", UiLogColor.Message, showTime: false);
-                    }
-                    else
-                    {
-                        AddLog($"{count} operator in total", UiLogColor.Message, showTime: false);
-                    }
-                }
-                else
-                {
-                    AddLog($"共 {count} 名干员", UiLogColor.Message, showTime: false);
-                }
+
+                AddLog(string.Format(LocalizationHelper.GetString("TotalOperatorsCount"), count), UiLogColor.Message, showTime: false);
+
 
                 if (json.TryGetValue("type", out var typeValue))
                 {
@@ -438,25 +458,25 @@ namespace MaaWpfGui.ViewModels.UI
 
                         if (json.TryGetValue("buff", out var buffValue))
                         {
-                            string buffLog = languageIsGlobal ? "Directional EC unit: " : "导能原件：";
-                            AddLog(buffLog + DataHelper.GetLocalizedCharacterName((string?)buffValue, _language), UiLogColor.Message, showTime: false);
+                            string buffLog = LocalizationHelper.GetString("DirectiveComponentTerm");
+                            AddLog(buffLog + DataHelper.GetLocalizedCharacterName((string?)buffValue, language), UiLogColor.Message, showTime: false);
                         }
 
                         if (json.TryGetValue("tool_men", out var toolMenValue))
                         {
-                            string toolMenLog = languageIsGlobal ? "Other Operators:\n" : "编队工具人：\n";
+                            string toolMenLog = LocalizationHelper.GetString("OtherOperators");
                             AddLog(toolMenLog + toolMenValue, UiLogColor.Message, showTime: false);
                         }
 
                         if (json.TryGetValue("equipment", out var equipmentValue) && equipmentValue is JArray equipmentJArray)
                         {
-                            string equipmentLog = languageIsGlobal ? "Tactical Equipment (horizontal):\n" : "开局装备（横向）：\n";
+                            string equipmentLog = LocalizationHelper.GetString("InitialEquipmentHorizontal") + '\n';
                             AddLog(equipmentLog + string.Join('\n', equipmentJArray.Select(i => (string?)i).Chunk(4).Select(i => string.Join(",", i))), UiLogColor.Message, showTime: false);
                         }
 
                         if (json.TryGetValue("strategy", out var strategyValue))
                         {
-                            string strategyLog = languageIsGlobal ? "Strategy: " : "开局策略：";
+                            string strategyLog = LocalizationHelper.GetString("InitialStrategy");
                             AddLog(strategyLog + strategyValue, UiLogColor.Message, showTime: false);
                         }
                     }
@@ -474,24 +494,26 @@ namespace MaaWpfGui.ViewModels.UI
                 File.Delete(TempCopilotFile);
                 File.WriteAllText(TempCopilotFile, json.ToString());
 
-                if (_taskType == "Copilot" && UseCopilotList)
+                if (_taskType != "Copilot" || !UseCopilotList)
                 {
-                    var value = json["difficulty"];
-                    var diff = value?.Type == JTokenType.Integer ? (int)value : 0;
-                    switch (diff)
-                    {
-                        case 0:
-                        case 1:
-                            AddCopilotTask();
-                            break;
-                        case 2:
-                            AddCopilotTask_Adverse();
-                            break;
-                        case 3:
-                            AddCopilotTask();
-                            AddCopilotTask_Adverse();
-                            break;
-                    }
+                    return;
+                }
+
+                var value = json["difficulty"];
+                var diff = value?.Type == JTokenType.Integer ? (int)value : 0;
+                switch (diff)
+                {
+                    case 0:
+                    case 1:
+                        AddCopilotTask();
+                        break;
+                    case 2:
+                        AddCopilotTask_Adverse();
+                        break;
+                    case 3:
+                        AddCopilotTask();
+                        AddCopilotTask_Adverse();
+                        break;
                 }
             }
             catch (Exception)
@@ -502,20 +524,6 @@ namespace MaaWpfGui.ViewModels.UI
 
         private async Task ParseCopilotSet(string jsonStr)
         {
-            void Log(string? name, string? description)
-            {
-                ClearLog();
-                if (!string.IsNullOrWhiteSpace(name))
-                {
-                    AddLog(name, UiLogColor.Message, showTime: false);
-                }
-
-                if (!string.IsNullOrWhiteSpace(description))
-                {
-                    AddLog(description, UiLogColor.Message, showTime: false);
-                }
-            }
-
             UseCopilotList = true;
             IsCopilotSet = false;
             try
@@ -547,6 +555,22 @@ namespace MaaWpfGui.ViewModels.UI
             catch (Exception)
             {
                 AddLog(LocalizationHelper.GetString("CopilotJsonError"), UiLogColor.Error, showTime: false);
+            }
+
+            return;
+
+            void Log(string? name, string? description)
+            {
+                ClearLog();
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    AddLog(name, UiLogColor.Message, showTime: false);
+                }
+
+                if (!string.IsNullOrWhiteSpace(description))
+                {
+                    AddLog(description, UiLogColor.Message, showTime: false);
+                }
             }
         }
 
