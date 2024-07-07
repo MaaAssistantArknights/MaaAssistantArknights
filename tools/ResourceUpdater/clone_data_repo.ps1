11cc3b04b9e76e594789d3ce4487cc6877fed7d5
Constrat <56174894+Constrat@users.noreply.github.com>
2024-07-07 21:28:16 +0200

    refactor: YoStar -> Yostar only for interfaces (#9548)

--- tools/ResourceUpdater/clone_data_repo.ps1
+++ tools/ResourceUpdater/clone_data_repo.ps1
@@ -11,8 +11,8 @@ git checkout
 
 cd..
 
-Write-Host "`nArknightsGameData_YoStar - Overseas"
-git clone --filter=blob:none --no-checkout --depth 1 --sparse https://github.com/Kengxxiao/ArknightsGameData_YoStar Overseas
+Write-Host "`nArknightsGameData_Yostar - Overseas"
+git clone --filter=blob:none --no-checkout --depth 1 --sparse https://github.com/Kengxxiao/ArknightsGameData_Yostar Overseas
 Set-Location Overseas
 git sparse-checkout set --no-cone /en_US/gamedata/excel/item_table.json /en_US/gamedata/excel/building_data.json /en_US/gamedata/excel/range_table.json /en_US/gamedata/excel/character_table.json /en_US/gamedata/excel/gacha_table.json /en_US/gamedata/excel/roguelike_topic_table.json /en_US/gamedata/excel/activity_table.json /ja_JP/gamedata/excel/item_table.json /ja_JP/gamedata/excel/building_data.json /ja_JP/gamedata/excel/range_table.json /ja_JP/gamedata/excel/character_table.json /ja_JP/gamedata/excel/gacha_table.json /ja_JP/gamedata/excel/roguelike_topic_table.json /ja_JP/gamedata/excel/activity_table.json /ko_KR/gamedata/excel/item_table.json /ko_KR/gamedata/excel/building_data.json /ko_KR/gamedata/excel/range_table.json /ko_KR/gamedata/excel/character_table.json /ko_KR/gamedata/excel/gacha_table.json /ko_KR/gamedata/excel/roguelike_topic_table.json /ko_KR/gamedata/excel/activity_table.json
 git checkout
