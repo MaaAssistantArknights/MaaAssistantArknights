73a7ab5c58766ee5ffff403e3887893ab20e52a5
soundofautumn <qjm1412@163.com>
2024-07-19 14:40:12 +0800

    test: 尝试优化无用字段

--- tools/TasksCommandTool/task/tests/test_cpp_tasks.py
+++ tools/TasksCommandTool/task/tests/test_cpp_tasks.py
@@ -1,9 +1,10 @@
 import json
 import math
 import os
+import unittest
 
 from .utils import TaskTest
-from ..Task import Task, set_base_task_warning
+from ..Task import Task, set_base_task_warning, _ORIGINAL_TASKS
 from ..TasksCli import json_path
 from ..debug import disable_tracing, enable_tracing
 
@@ -44,6 +45,27 @@ class CPPTaskTests(TaskTest):
                 continue
 
             self.assertEqual(expected[key], actual[key], f"Task: {task_name}, Key: {key}")
+
+    @unittest.skip("unfinished")
+    def test_unused_fields(self):
+        disable_tracing()
+        set_base_task_warning(False)
+        load_tasks()
+        with open(json_path, 'r', encoding='utf-8') as f:
+            tasks = json.load(f)
+            for name in _ORIGINAL_TASKS:
+                result = Task.get(name).interpret()
+                if result is not None:
+                    all_field = set(tasks[name].keys())
+                    used_field = set(result.to_simplified_dict().keys())
+                    unused_field = all_field - used_field
+                    if "baseTask" in unused_field:
+                        unused_field.remove("baseTask")
+                    if "next" in unused_field:
+                        unused_field.remove("next")
+                    if unused_field:
+                        print(f"Unused field(s) in task {result.name}: {unused_field}")
+        enable_tracing()
 
     def test_cpp_tasks(self):
         disable_tracing()
