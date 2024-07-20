d6a179ea433c38d025662a8d445e9927fb989c91
status102 <102887808+status102@users.noreply.github.com>
2024-07-20 16:48:27 +0800

    perf: 移除意料外的等待

--- src/MaaCore/Task/Roguelike/RoguelikeInvestTaskPlugin.h
+++ src/MaaCore/Task/Roguelike/RoguelikeInvestTaskPlugin.h
@@ -10,18 +10,18 @@ namespace asst
         using AbstractRoguelikeTaskPlugin::AbstractRoguelikeTaskPlugin;
         virtual ~RoguelikeInvestTaskPlugin() override = default;
         virtual bool verify(AsstMsg msg, const json::value& details) const override;
-        void set_invest_maximum(int value) { m_invest_maximum = value; }
+        void set_invest_maximum(int value) { m_maximum = value; }
         void set_stop_when_full(bool value) { m_stop_when_full = value; }
 
     private:
         virtual bool _run() override;
-        std::optional<int> ocr_current_count(const auto& img, const auto& task_name);
+        std::optional<int> ocr_count(const auto& img, const auto& task_name);
         bool is_investment_available(const cv::Mat& image) const;
         bool is_investment_error(const cv::Mat& image) const;
         void stop_roguelike();
 
         int m_invest_count = 0;
-        int m_invest_maximum = 0;
+        int m_maximum = 0;
         bool m_stop_when_full = false; // 存款满了就停止
     };
 } // namespace asst
