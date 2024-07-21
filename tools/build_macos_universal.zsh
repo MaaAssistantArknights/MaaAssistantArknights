184aeed0072e9d5aeade69e7055c20133074543a
Hao Guan <10684225+hguandl@users.noreply.github.com>
2024-07-21 22:32:49 +0800

    chore: 优化macOS编译脚本

--- tools/build_macos_universal.zsh
+++ tools/build_macos_universal.zsh
@@ -1,25 +1,38 @@
 #!/usr/bin/env zsh
 
+arch=$(arch)
 basedir=$(dirname "$0")/..
 pushd ${basedir}
 
 build_arch() {
     [[ $1 = "arm64" ]] && triplet="arm64-osx" || triplet="x64-osx"
-    python3 maadeps-download.py ${triplet}
 
-    cmake -B build-$1 -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES=$1
+    if [ ! -d ${basedir}/MaaDeps/runtime/maa-${triplet} ]; then
+        python3 maadeps-download.py ${triplet}
+    fi
+
+    cmake -B build-$1 -GNinja -DCMAKE_BUILD_TYPE=$2 -DCMAKE_OSX_ARCHITECTURES=$1
     cmake --build build-$1
     cmake --install build-$1 --prefix install-$1
 }
 
-build_arch "arm64"
-build_arch "x86_64"
+if [[ -n "${MAA_DEBUG}" ]]; then
+    build_arch "${arch}" "Debug"
+else
+    build_arch "arm64" "Release"
+    build_arch "x86_64" "Release"
+fi
 
 rm -rf build
 mkdir build
-for LIB_NAME in $(ls install-arm64 | grep .dylib); do
-    lipo -create install-arm64/$LIB_NAME install-x86_64/$LIB_NAME -output build/$LIB_NAME
+for LIB_NAME in $(ls install-"${arch}" | grep .dylib); do
+    if [[ -n "${MAA_DEBUG}" ]]; then
+        mv install-"${arch}"/$LIB_NAME build/$LIB_NAME
+    else
+        lipo -create install-arm64/$LIB_NAME install-x86_64/$LIB_NAME -output build/$LIB_NAME
+    fi
 done
+cp build-"${arch}"/compile_commands.json build
 
 pushd build
 xcodebuild -create-xcframework -library libMaaCore.dylib -headers ../include -output MaaCore.xcframework
