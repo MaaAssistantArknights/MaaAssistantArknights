eaa8d4e59e981b52620270e06169d3207739e665
SherkeyXD <57581480+SherkeyXD@users.noreply.github.com>
2024-07-22 19:40:37 +0800

    docs: 修复 docsearch 路径错误的问题

--- docs/.vuepress/plugins/search.ts
+++ docs/.vuepress/plugins/search.ts
@@ -1,47 +1,9 @@
 export default {
     appId: "99JM20SIFG",
-    apiKey: "32ad931040facd36d2e99c3cb8e425e1",
+    apiKey: "7596a5a8c95cd64d4cf3050c9a4f878e",
     indexName: "maa",
     // 这里应该把所有本地化配置都放入 locales 中
     locales: {
-        "/": {
-            placeholder: "搜索",
-            translations: {
-                button: {
-                    buttonText: "搜索",
-                },
-                modal: {
-                    searchBox: {
-                        resetButtonTitle: "清除查询条件",
-                        cancelButtonText: "取消",
-                    },
-                    startScreen: {
-                        recentSearchesTitle: "搜索历史",
-                        noRecentSearchesText: "没有搜索历史",
-                        saveRecentSearchButtonTitle: "保存至搜索历史",
-                        removeRecentSearchButtonTitle: "从搜索历史中移除",
-                        favoriteSearchesTitle: "收藏",
-                        removeFavoriteSearchButtonTitle: "从收藏中移除",
-                    },
-                    errorScreen: {
-                        titleText: "无法获取结果",
-                        helpText: "你可能需要检查你的网络连接",
-                    },
-                    footer: {
-                        selectText: "选择",
-                        navigateText: "切换",
-                        closeText: "关闭",
-                        searchByText: "搜索提供者",
-                    },
-                    noResultsScreen: {
-                        noResultsText: "无法找到相关结果",
-                        suggestedQueryText: "你可以尝试查询",
-                        reportMissingResultsText: "你认为该查询应该有结果？",
-                        reportMissingResultsLinkText: "点击反馈",
-                    },
-                },
-            },
-        },
         "/zh-cn/": {
             placeholder: "搜索",
             translations: {
