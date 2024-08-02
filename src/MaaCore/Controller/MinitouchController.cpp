2762ef73a7766aa526b34377a85898a9d5b991cc
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2024-08-02 13:32:51 +0800

    feat: fallback to General config

--- src/MaaCore/Controller/MinitouchController.cpp
+++ src/MaaCore/Controller/MinitouchController.cpp
@@ -368,7 +368,17 @@ bool asst::MinitouchController::connect(const std::string& adb_path, const std::
     auto adb_ret = Config.get_adb_cfg(config);
 
     if (!adb_ret) {
+        json::value info = get_info_json()
+                           | json::object {
+                                 { "what", "ConnectFailed" },
+                                 { "why", "ConfigNotFound" },
+                             };
+        callback(AsstMsg::ConnectionInfo, info);
+#ifdef ASST_DEBUG
         return false;
+#endif
+        Log.error("config ", config, "not found");
+        adb_ret = Config.get_adb_cfg("General");
     }
 
     const auto& adb_cfg = adb_ret.value();
