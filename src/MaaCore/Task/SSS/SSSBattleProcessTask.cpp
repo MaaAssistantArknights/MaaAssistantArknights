b4af47aa806db6f759acf8bd8641158f78874f34
晓丶梦丶仁 <1020623818@qq.com>
2024-08-20 18:54:06 +0800

    fix: 删除一个意义不明的if

--- src/MaaCore/Task/SSS/SSSBattleProcessTask.cpp
+++ src/MaaCore/Task/SSS/SSSBattleProcessTask.cpp
@@ -213,9 +213,6 @@ bool asst::SSSBattleProcessTask::check_and_do_strategy(const cv::Mat& reusable)
             if (strategy.core_deployed && core.role != Role::Drone && core.role != Role::Unknown) {
                 strategy.core_deployed = false;
                 for (auto& strategy_reset : m_sss_combat_data.strategies) {
-                    if (core.name == strategy_reset.core) {
-                        continue;
-                    }
                     if (strategy.location == strategy_reset.location) {
                         for (auto& same_location_strategy : (it->second)) {
                             if ((*same_location_strategy).index == strategy_reset.index) {
