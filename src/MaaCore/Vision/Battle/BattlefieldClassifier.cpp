4233d012d04e25481a63d67de668c8e1a16230b5
uye <99072975+ABA2396@users.noreply.github.com>
2024-10-13 18:37:02 +0800

    feat: 自动截图

--- src/MaaCore/Vision/Battle/BattlefieldClassifier.cpp
+++ src/MaaCore/Vision/Battle/BattlefieldClassifier.cpp
@@ -8,6 +8,7 @@
 
 #include "Config/OnnxSessions.h"
 #include "Config/TaskData.h"
+#include "Utils/ImageIo.hpp"
 #include "Utils/Logger.hpp"
 
 using namespace asst;
@@ -88,6 +89,46 @@ BattlefieldClassifier::SkillReadyResult BattlefieldClassifier::skill_ready_analy
             cv::Scalar(0, 165, 255),
             2);
     }
+
+    // 为重新训练模型截图
+    static Point last_base_point = { -1, -1 };
+    static auto last_save_time = std::chrono::steady_clock::now();
+    static bool last_ready = false;
+    const auto now = std::chrono::steady_clock::now();
+    const auto duration_since_last_save =
+        std::chrono::duration_cast<std::chrono::seconds>(now - last_save_time).count();
+
+    auto need_save = false;
+    // 如果相同点且结果不同，保存
+    if (last_base_point == m_base_point && last_ready != ready) {
+        need_save = true;
+    }
+    // 如果不同点且 ready，保存
+    else if (last_base_point != m_base_point && ready) {
+        need_save = true;
+    }
+    // 来点随机截图
+    else if (duration_since_last_save > 10) {
+        last_save_time = now;
+        need_save = true;
+    }
+
+    if (need_save) {
+        std::filesystem::path relative_path;
+        if (ready) {
+            relative_path = utils::path("debug") / utils::path("skill_ready") / utils::path("y") /
+                            (utils::get_time_filestem() + "_" + std::to_string(m_base_point.x) + "_" + std::to_string(m_base_point.y) + ".png");
+        }
+        else {
+            relative_path = utils::path("debug") / utils::path("skill_ready") / utils::path("n") /
+                            (utils::get_time_filestem() + "_" + std::to_string(m_base_point.x) + "_" + std::to_string(m_base_point.y) + ".png");
+        }
+        last_base_point = m_base_point;
+        last_ready = ready;
+        Log.trace("Save image", relative_path);
+        asst::imwrite(relative_path, image);
+    }
+
 #endif
 
     return SkillReadyResult {
