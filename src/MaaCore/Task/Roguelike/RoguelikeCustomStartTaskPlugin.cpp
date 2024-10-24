cb7fda32bd238790e1e057b97915e1086b879ab3
Weiyou Wang <44151844+Alan-Charred@users.noreply.github.com>
2024-10-24 20:16:48 +1100

    fix: set_squad 提前

--- src/MaaCore/Task/Roguelike/RoguelikeCustomStartTaskPlugin.cpp
+++ src/MaaCore/Task/Roguelike/RoguelikeCustomStartTaskPlugin.cpp
@@ -102,6 +102,8 @@ bool asst::RoguelikeCustomStartTaskPlugin::hijack_squad()
         }
         const auto& rect = analyzer.get_result().front().rect;
         ctrler()->click(rect);
+
+        m_config->set_squad(m_customs[RoguelikeCustomType::Squad]);
         return true;
     }
     ProcessTask(*this, { "SwipeToTheLeft" }).run();
@@ -156,6 +158,5 @@ bool asst::RoguelikeCustomStartTaskPlugin::hijack_core_char()
     m_config->set_use_support(m_customs[RoguelikeCustomType::UseSupport] == "1");
     m_config->set_use_nonfriend_support(m_customs[RoguelikeCustomType::UseNonfriendSupport] == "1");
     m_config->set_core_char(char_name);
-    m_config->set_squad(m_customs[RoguelikeCustomType::Squad]);
     return true;
 }
