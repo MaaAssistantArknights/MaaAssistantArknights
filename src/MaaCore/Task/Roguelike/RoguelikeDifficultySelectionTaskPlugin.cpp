41f534f0e3072c1474a6605a71620a399a3cea2d
uye <99072975+ABA2396@users.noreply.github.com>
2024-10-25 17:06:18 +0800

    feat: 支持肉鸽选难度

--- src/MaaCore/Task/Roguelike/RoguelikeDifficultySelectionTaskPlugin.cpp
+++ src/MaaCore/Task/Roguelike/RoguelikeDifficultySelectionTaskPlugin.cpp
@@ -1,5 +1,6 @@
 #include "RoguelikeDifficultySelectionTaskPlugin.h"
 
+#include "Config/TaskData.h"
 #include "Status.h"
 #include "Task/ProcessTask.h"
 #include "Utils/Logger.hpp"
@@ -36,15 +37,25 @@ bool asst::RoguelikeDifficultySelectionTaskPlugin::_run()
     // todo:以后可以根据传入的难度值选择难度?
 
     // 当前难度
-    int difficulty = m_config->get_difficulty();
+    int difficulty = 5;
     // 是否不进行作战
     bool no_battle = m_config->get_only_start_with_elite_two() || m_config->get_first_floor_foldartal();
     if (m_config->get_theme() != RoguelikeTheme::Phantom && mode == RoguelikeMode::Collectible && !no_battle) {
         if (difficulty == INT_MAX) {
             ProcessTask(*this, { m_config->get_theme() + "@Roguelike@ChooseDifficulty_Hardest" }).run();
         }
-        else {
+        else if (difficulty == 0) {
             ProcessTask(*this, { m_config->get_theme() + "@Roguelike@ChooseDifficulty_Easiest" }).run();
+        }
+        else {
+            ProcessTask(*this, { m_config->get_theme() + "@Roguelike@ChooseDifficulty_Hardest" }).run();
+            //  难度识别内容为 20 ~ difficulty
+            std::vector<std::string> difficulty_list;
+            for (int i = 20; i >= difficulty; --i) {
+                difficulty_list.push_back(std::to_string(i));
+            }
+            Task.get<OcrTaskInfo>(m_config->get_theme() + "@Roguelike@ChooseDifficulty_specified")->text = difficulty_list;
+            ProcessTask(*this, { m_config->get_theme() + "@Roguelike@ChooseDifficulty_specified" }).run();
         }
         ProcessTask(*this, { m_config->get_theme() + "@Roguelike@ChooseDifficultyConfirm" }).run();
     }
