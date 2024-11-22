47d8b0f60418e01034d46845c053bd9ff732d2fa
status102 <102887808+status102@users.noreply.github.com>
2024-11-22 21:44:20 +0800

    chore: format & nullable
    [skip changelog]

--- src/MaaWpfGui/WineCompat/MaaDesktopIntegration.cs
+++ src/MaaWpfGui/WineCompat/MaaDesktopIntegration.cs
@@ -1,4 +1,4 @@
-// <copyright file="MaaWineBridge.cs" company="MaaAssistantArknights">
+// <copyright file="MaaDesktopIntegration.cs" company="MaaAssistantArknights">
 // MaaWpfGui - A part of the MaaCoreArknights project
 // Copyright (C) 2021 MistEO and Contributors
 //
