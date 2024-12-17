ea6c4cad25b23a94e7f5c97432a62e70977c26b4
uye <99072975+ABA2396@users.noreply.github.com>
2024-12-17 13:13:41 +0800

    Revert "chore: 改进Python脚本中incremental_path参数说明"
    This reverts commit 5639004cb2bbfd3f649f4eb82bec0c2f6dc084bb.

--- src/Python/sample.py
+++ src/Python/sample.py
@@ -24,22 +24,18 @@ if __name__ == "__main__":
     # 设置更新器的路径和目标版本并更新
     Updater(path, Version.Stable).update()
 
-    # 加载 dll 及资源
+    # 外服需要再额外传入增量资源路径，例如
+    # incremental_path=path / 'resource' / 'global' / 'YoStarEN'
     #
-    # incremental_path 参数表示增量资源所在路径。两种用法举例：
-    # 1. 传入外服的增量资源路径：
-    #     Asst.load(path=path, incremental_path=path / 'resource' / 'global' / 'YoStarEN')
-    # 2. 加载活动关导航（需额外下载）：
-    #     # 下载活动关导航
-    #     import urllib.request
-    #     ota_tasks_url = 'https://ota.maa.plus/MaaAssistantArknights/api/resource/tasks.json'
-    #     ota_tasks_path = path / 'cache' / 'resource' / 'tasks.json'
-    #     ota_tasks_path.parent.mkdir(parents=True, exist_ok=True)
-    #     with open(ota_tasks_path, 'w', encoding='utf-8') as f:
-    #         with urllib.request.urlopen(ota_tasks_url) as u:
-    #             f.write(u.read().decode('utf-8'))
-    #     # 加载
-    #     Asst.load(path=path, incremental_path=path / 'cache')
+    # 活动关导航需额外下载，可通过增量资源路径加载。
+    # import urllib.request
+    # ota_tasks_url = 'https://ota.maa.plus/MaaAssistantArknights/api/resource/tasks.json'
+    # ota_tasks_path = path / 'cache' / 'resource' / 'tasks.json'
+    # ota_tasks_path.parent.mkdir(parents=True, exist_ok=True)
+    # with open(ota_tasks_path, 'w', encoding='utf-8') as f:
+    #     with urllib.request.urlopen(ota_tasks_url) as u:
+    #         f.write(u.read().decode('utf-8'))
+    # Asst.load(path=path, incremental_path=path / 'cache')
     Asst.load(path=path)
 
     # 若需要获取详细执行信息，请传入 callback 参数
