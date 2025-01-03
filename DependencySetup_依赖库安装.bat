e203728fb5fdac6bb6074ca4ac0aec024e08f15a
Constrat <56174894+Constrat@users.noreply.github.com>
2025-01-03 22:54:45 +0100

    i18n: DependencySetup [skip changelog]

--- DependencySetup_依赖库安装.bat
+++ DependencySetup_依赖库安装.bat
@@ -5,6 +5,7 @@ setlocal enabledelayedexpansion
 openfiles >nul 2>&1
 if %errorlevel% neq 0 (
     echo 正在获取管理员权限...
+    echo Obtaining administrator privileges...
     powershell -Command "Start-Process cmd.exe -ArgumentList '/c %~dpnx0' -Verb RunAs"
     exit /b
 )
