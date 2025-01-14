b58faa82309e2743183ad740373c13ccc51b5ec4
BxFS <15606086+BxFS@users.noreply.github.com>
2025-01-15 00:27:42 +0800

    fix: post rebase fixes

--- src/MaaWpfGui/Main/AsstProxy.cs
+++ src/MaaWpfGui/Main/AsstProxy.cs
@@ -2490,7 +2490,6 @@ namespace MaaWpfGui.Main
             bool monthlySquadAutoIterate,
             bool monthlySquadCheckComms,
             bool deepExplorationAutoIterate,
-            bool deepExplorationAutoIterate,
             bool stopAtMaxLevel,
             bool startWithSeed)
         {
