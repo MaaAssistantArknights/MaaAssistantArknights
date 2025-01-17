aa862bc5465d3fc5cebf9b8e999b6528dc90ebb5
status102 <102887808+status102@users.noreply.github.com>
2025-01-17 18:25:05 +0800

    rft: 拆分烧水奖励领取

---
+++ src/MaaCore/Task/Roguelike/RoguelikeLastRewardSelectTaskPlugin.cpp
@@ -0,0 +1,76 @@
+#include "RoguelikeLastRewardSelectTaskPlugin.h"
+
+#include "Config/TaskData.h"
+#include "Controller/Controller.h"
+#include "Task/ProcessTask.h"
+#include "Utils/Logger.hpp"
+#include "Vision/Miscellaneous/PipelineAnalyzer.h"
+
+bool asst::RoguelikeLastRewardSelectTaskPlugin::verify(AsstMsg msg, const json::value& details) const
+{
+    if (msg != AsstMsg::SubTaskStart || details.get("subtask", std::string()) != "ProcessTask") {
+        return false;
+    }
+    const std::string& task = details.get("details", "task", "");
+    if (task.ends_with("Roguelike@LastReward-EnterPoint")) {
+        return true;
+    }
+
+    return false;
+}
+
+bool asst::RoguelikeLastRewardSelectTaskPlugin::_run()
+{
+    LogTraceFunction;
+
+    std::vector<std::string> tasks_list;
+    if (m_config->get_start_with_hot_water()) {
+        tasks_list.emplace_back(m_config->get_theme() + "@Roguelike@LastReward");
+    }
+    if (m_config->get_start_with_shield()) {
+        tasks_list.emplace_back(m_config->get_theme() + "@Roguelike@LastReward2");
+    }
+    if (m_config->get_start_with_ingot()) {
+        tasks_list.emplace_back(m_config->get_theme() + "@Roguelike@LastReward3");
+    }
+    if (m_config->get_start_with_hope()) {
+        tasks_list.emplace_back(m_config->get_theme() + "@Roguelike@LastReward4");
+    }
+
+    if (m_config->get_start_with_random()) {
+        tasks_list.emplace_back(m_config->get_theme() + "@Roguelike@LastRewardRand");
+    }
+
+    if (m_config->get_theme() == RoguelikeTheme::Mizuki && m_config->get_start_with_key()) {
+        tasks_list.emplace_back("Mizuki@Roguelike@LastReward5");
+    }
+    if (m_config->get_theme() == RoguelikeTheme::Mizuki && m_config->get_start_with_dice()) {
+        tasks_list.emplace_back("Mizuki@Roguelike@LastReward6");
+    }
+
+    // 未加入
+    /* if (m_config->get_theme() == RoguelikeTheme::Sarkaz && m_config->get_start_with_ideas()) {
+        tasks_list.emplace_back( "Sarkaz@Roguelike@LastReward5");
+    }*/
+
+    if (tasks_list.empty()) {
+        ProcessTask(*this, { m_config->get_theme() + "@Roguelike@LastReward-Strategy" })
+            .set_times_limit("Roguelike@LastRewardConfirm", 0)
+            .run();
+        ProcessTask(*this, { "Roguelike@LastReward-Confirm" }).run();
+        return true;
+    }
+
+    // 处理选择顺序
+    PipelineAnalyzer analyzer(ctrler()->get_image());
+    analyzer.set_tasks(tasks_list);
+    if (auto ret = analyzer.analyze(); ret) {
+        ctrler()->click(ret->rect);
+        ProcessTask(*this, { "Roguelike@LastReward-Confirm" }).run();
+    }
+    else {
+        m_control_ptr->exit_then_stop(true);
+    }
+
+    return true;
+}
---
+++ src/MaaCore/Task/Roguelike/RoguelikeLastRewardSelectTaskPlugin.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "AbstractRoguelikeTaskPlugin.h"
+
+namespace asst
+{
+class RoguelikeLastRewardSelectTaskPlugin : public AbstractRoguelikeTaskPlugin
+{
+public:
+    using AbstractRoguelikeTaskPlugin::AbstractRoguelikeTaskPlugin;
+    virtual ~RoguelikeLastRewardSelectTaskPlugin() = default;
+
+    virtual bool verify(AsstMsg msg, const json::value& details) const override;
+
+protected:
+    virtual bool _run() override;
+};
+}
