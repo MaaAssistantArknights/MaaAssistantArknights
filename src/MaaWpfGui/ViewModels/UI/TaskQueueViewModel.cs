e5cbec1751ba5ff1aea163dfddcbc8ecbe14cf73
status102 <102887808+status102@users.noreply.github.com>
2025-02-09 08:02:10 +0800

    fix: UI同步

--- src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
@@ -1822,10 +1822,8 @@ namespace MaaWpfGui.ViewModels.UI
 
         private static bool AppendRoguelike()
         {
-            _ = int.TryParse(RoguelikeTask.RoguelikeMode, out var mode);
-
             return Instances.AsstProxy.AsstAppendRoguelike(
-                mode,
+                RoguelikeTask.RoguelikeMode,
                 RoguelikeTask.RoguelikeDifficulty,
                 RoguelikeTask.RoguelikeStartsCount,
                 RoguelikeTask.RoguelikeInvestmentEnabled,
