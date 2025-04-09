cb803282d586bdf3b86ff83af9cb0ec2048ff11a
BxFS <15606086+BxFS@users.noreply.github.com>
2025-04-08 22:17:24 -0400

    feat: 生成问题反馈压缩包&打开debug文件夹按钮 (#12282)
    * feat: generate support payload and open debug folder option
    
    * feat: zh-tw and en-us i18n, along with ai-translated ja-jp and ko-kr
    
    * i18n: EN tweak
    
    * fix: 移除重复的常见问题
    
    * chore: UI 调整
    
    * fix: 修复绑定，调整顺序
    
    * chore: 移除不再使用的i18n内容
    
    * perf: 调整关于界面
    
    * fix: 文件过滤
    
    ---------
    
    Co-authored-by: Constrat <56174894+Constrat@users.noreply.github.com>
    Co-authored-by: status102 <102887808+status102@users.noreply.github.com>

---
+++ src/MaaWpfGui/Views/UserControl/Settings/IssueReportUserControl.xaml.cs
@@ -0,0 +1,16 @@
+namespace MaaWpfGui.Views.UserControl.Settings
+{
+    /// <summary>
+    /// Interaction logic for IssueReportUserControl.xaml
+    /// </summary>
+    public partial class IssueReportUserControl : System.Windows.Controls.UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IssueReportUserControl"/> class.
+        /// </summary>
+        public IssueReportUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
