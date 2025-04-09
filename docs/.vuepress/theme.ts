3bb0833704ab437a1aec02231cbae45d0d25e189
Rbqwow <55343783+Rbqwow@users.noreply.github.com>
2025-04-09 21:09:31 +0800

    build: 更新文档主题库 (#11627)
    * build: 更新网站依赖
    
    * build:更新vphope版本
    
    * build: 怎么忘记加这个了
    
    * fix: 修复构建错误
    
    * chore: fix Vite CJS Node API deprecated warning
    
    * chore: log
    
    * perf: turbo 添加文档 cache
    
    * perf: 注释掉新版不读取的切换语言公告内容
    
    但公告这个属性得留着 不留着就没有切换语言的弹窗了
    
    * fix: 修复字体问题
    
    * build: update package version
    
    * docs: remove root locale
    
    * docs: add author
    
    * ci: update node version
    
    * build: turbo一直叫我更新，于是我更新了
    
    * docs: update package
    
    * docs: 再见吧牛牛今晚我就要远航
    
    * docs: fix root locale problem
    
    * docs: remove unused command
    
    * feat: 牛兮归来！
    
    * build: update turbo
    
    * chore: 语言提醒怎么又没了 加回来
    
    ---------
    
    Co-authored-by: SherkeyXD <57581480+SherkeyXD@users.noreply.github.com>

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -5,10 +5,14 @@ import { zhcnSidebar, zhtwSidebar, enusSidebar, jajpSidebar, kokrSidebar } from 
 
 export default hopeTheme({
   hostname: "https://maa.plus",
-  iconAssets: "iconify",
+  author: "MaaAssistantArknights",
+  license: "AGPL-3.0",
   repo: "MaaAssistantArknights/MaaAssistantArknights",
   docsBranch: "dev",
   docsDir: "/docs",
+
+  lastUpdated: false,
+  contributors: false,
 
   locales: {
     "/zh-cn/": {
@@ -23,6 +27,7 @@ export default hopeTheme({
       navbar: zhtwNavbar,
       sidebar: zhtwSidebar,
       displayFooter: false,
+      title: '繁体中文',
       metaLocales: {
         editLink: "在 Github 上編輯此頁面",
       },
@@ -53,6 +58,33 @@ export default hopeTheme({
     },
   },
 
+  markdown: {
+    align: true,
+    codeTabs: true,
+    //echarts: true,
+    footnote: true,
+    gfm: true,
+    hint: true,
+    imgLazyload: true,
+    imgMark: true,
+    imgSize: true,
+    //mathjax: true,
+    mark: true,
+    //mermaid: true,
+    tabs: true,
+    tasklist: true,
+    vPre: true,
+    component: true,
+
+    highlighter: {
+      'type': 'shiki',
+      themes: {
+        light: "light-plus",
+        dark: "nord",
+      }
+    },
+  },
+
   plugins: {
     activeHeaderLinks: false,
 
@@ -66,111 +98,88 @@ export default hopeTheme({
       strict: true,
     },
 
-    copyright: {
-      license: "AGPL-3.0",
-    },
-
     docsearch: DocSearchConfig,
 
-    mdEnhance: {
-      align: true,
-      codetabs: true,
-      //echarts: true,
-      footnote: true,
-      gfm: true,
-      hint: true,
-      imgLazyload: true,
-      imgMark: true,
-      imgSize: true,
-      //mathjax: true,
-      mark: true,
-      //mermaid: true,
-      tabs: true,
-      tasklist: true,
-      vPre: true,
-      component: true,
-    },
-
-    shiki: {
-      themes: {
-        light: "light-plus",
-        dark: "nord",
-      }
+    icon: {
+      assets: "iconify",
     },
 
     sitemap: true,
+    seo: true,
 
     notice: [
-      {
-        path: "/zh-tw/",
-        title: "翻译警告",
-        content: "MAA 的文檔以簡體中文為主，其他語言的文檔可能品質低或尚未翻譯，請諒解。",
-        fullscreen: true,
-        confirm: true,
-        showOnce: true,
-        actions: [
-          {
-            text: "我知道了",
-            type: "primary",
-          },
-          { text: "前往簡體中文",
-            link: "/zh-cn/",
-          },
-        ],
-      },
-      {
-        path: "/en-us/",
-        title: "Translation Warning",
-        content: "MAA's documents are mainly in Simplified Chinese. Documents in other languages may be of low quality or not yet translated. Please understand.",
-        fullscreen: true,
-        confirm: true,
-        showOnce: true,
-        actions: [
-          {
-            text: "Okay",
-            type: "primary",
-          },
-          { text: "Take me to zh-CN",
-            link: "/zh-cn/",
-          },
-        ],
-      },
-      {
-        path: "/ja-jp/",
-        title: "翻訳に関する警告",
-        content: "MAA のドキュメントは主に簡体字中国語で書かれており、他の言語のドキュメントは低品質であるか、翻訳されていない可能性がありますので、ご了承ください。",
-        fullscreen: true,
-        confirm: true,
-        showOnce: true,
-        actions: [
-          {
-            text: "OK",
-            type: "primary",
-          },
-          { text: "中国語サイトへ行く",
-            link: "/zh-cn/",
-          },
-        ],
-      },
-      {
-        path: "/ko-kr/",
-        title: "번역 경고",
-        content: "MAA의 문서는 주로 중국어 간체로 되어 있습니다. 다른 언어로 된 문서는 번역이 이상하거나, 번역이 되어있지 않을 수 있습니다.",
-        fullscreen: true,
-        confirm: true,
-        showOnce: true,
-        actions: [
-          {
-            text: "OK",
-            type: "primary",
-          },
-          { text: "중국어 간체로 이동",
-            link: "/zh-cn/",
-          },
-        ],
-      },
+        {
+          path: "/zh-tw/",
+          title: "翻译警告",
+          content: "MAA 的文檔以簡體中文為主，其他語言的文檔可能品質低或尚未翻譯，請諒解。",
+          fullscreen: true,
+          confirm: true,
+          showOnce: true,
+          actions: [
+            {
+              text: "我知道了",
+              type: "primary",
+            },
+            {
+              text: "前往簡體中文",
+              link: "/zh-cn/",
+            },
+          ],
+        },
+        {
+          path: "/en-us/",
+          title: "Translation Warning",
+          content: "MAA's documents are mainly in Simplified Chinese. Documents in other languages may be of low quality or not yet translated. Please understand.",
+          fullscreen: true,
+          confirm: true,
+          showOnce: true,
+          actions: [
+            {
+              text: "Okay",
+              type: "primary",
+            },
+            {
+              text: "Take me to zh-CN",
+              link: "/zh-cn/",
+            },
+          ],
+        },
+        {
+          path: "/ja-jp/",
+          title: "翻訳に関する警告",
+          content: "MAA のドキュメントは主に簡体字中国語で書かれており、他の言語のドキュメントは低品質であるか、翻訳されていない可能性がありますので、ご了承ください。",
+          fullscreen: true,
+          confirm: true,
+          showOnce: true,
+          actions: [
+            {
+              text: "OK",
+              type: "primary",
+            },
+            {
+              text: "中国語サイトへ行く",
+              link: "/zh-cn/",
+            },
+          ],
+        },
+        {
+          path: "/ko-kr/",
+          title: "번역 경고",
+          content: "MAA의 문서는 주로 중국어 간체로 되어 있습니다. 다른 언어로 된 문서는 번역이 이상하거나, 번역이 되어있지 않을 수 있습니다.",
+          fullscreen: true,
+          confirm: true,
+          showOnce: true,
+          actions: [
+            {
+              text: "OK",
+              type: "primary",
+            },
+            {
+              text: "중국어 간체로 이동",
+              link: "/zh-cn/",
+            },
+          ],
+        },
     ],
-
   },
-
-});
+}, { custom: true });
