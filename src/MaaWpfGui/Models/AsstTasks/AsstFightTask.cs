90ed19b192f3ef44766ea3b9123454860a0f03c6
uye <99072975+ABA2396@users.noreply.github.com>
2025-04-09 16:25:11 +0800

    fix: typo
    [skip changelog]

--- src/MaaWpfGui/Models/AsstTasks/AsstFightTask.cs
+++ src/MaaWpfGui/Models/AsstTasks/AsstFightTask.cs
@@ -121,6 +121,6 @@ public class AsstFightTask : AsstBaseTask
             param["drops"] = JObject.FromObject(Drops);
         }
 
-        return (AsstTaskType.Custom, param);
+        return (AsstTaskType.Fight, param);
     }
 }
