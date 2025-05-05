5c9ae13811d9fad04ee546c43620495313e43808
Yoake <120039624+Yoak3n@users.noreply.github.com>
2025-05-05 15:07:38 +0800

    feat:实现自适应调整连战次数 (#12555)
    * feat:实现自适应调整连战次数
    
    * fix: redundant operation
    
    * fix: 掉落物回报、理智回报失效
    
    * chore: Auto update by pre-commit hooks [skip changelog]
    
    * fix: typo
    
    * feat: 添加无理智药时的识别
    
    * chore: 删除无用 include
    
    * chore: Auto update by pre-commit hooks [skip changelog]
    
    * chore: 异常时返回 -1
    
    ---------
    
    Co-authored-by: uye <99072975+ABA2396@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/MaaCore/Task/Interface/FightTask.h
+++ src/MaaCore/Task/Interface/FightTask.h
@@ -12,6 +12,7 @@ class StageNavigationTask;
 class DrGrandetTaskPlugin;
 class SideStoryReopenTask;
 class MedicineCounterTaskPlugin;
+class FightSeriesAdjustPlugin;
 
 class FightTask final : public InterfaceTask
 {
@@ -28,6 +29,7 @@ protected:
     std::shared_ptr<StageNavigationTask> m_stage_navigation_task_ptr = nullptr;
     std::shared_ptr<ProcessTask> m_fight_task_ptr = nullptr;
     std::shared_ptr<FightTimesTaskPlugin> m_fight_times_task_plugin_prt = nullptr;
+    std::shared_ptr<FightSeriesAdjustPlugin> m_fight_series_adjust_plugin_ptr = nullptr;
     std::shared_ptr<MedicineCounterTaskPlugin> m_medicine_plugin = nullptr;
     std::shared_ptr<StageDropsTaskPlugin> m_stage_drops_plugin_ptr = nullptr;
     std::shared_ptr<DrGrandetTaskPlugin> m_dr_grandet_task_plugin_ptr = nullptr;
