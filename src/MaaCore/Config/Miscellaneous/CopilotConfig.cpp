361dcf1caeba2914b7d5d77df18b6080af547f44
travellerse <travellers0238@outlook.com>
2025-05-28 23:01:42 +0800

    feat: 支持编队添加干员时选择模组

--- src/MaaCore/Config/Miscellaneous/CopilotConfig.cpp
+++ src/MaaCore/Config/Miscellaneous/CopilotConfig.cpp
@@ -88,6 +88,15 @@ asst::battle::copilot::OperUsageGroups asst::CopilotConfig::parse_groups(const j
             oper.skill_usage = static_cast<battle::SkillUsage>(oper_info.get("skill_usage", 0));
             oper.skill_times = oper_info.get("skill_times", 1); // 使用技能的次数，默认为 1，兼容曾经的作业
 
+            // 解析练度需求
+            if (auto req_opt = oper_info.find("requirements")) {
+                oper.requirements.elite = req_opt->get("elite", 0);
+                oper.requirements.level = req_opt->get("level", 0);
+                oper.requirements.skill_level = req_opt->get("skill_level", 0);
+                oper.requirements.module = req_opt->get("module", 0);
+                oper.requirements.potentiality = req_opt->get("potentiality", 0);
+            }
+
             // 单个干员的，干员名直接作为组名
             std::string group_name = oper.name;
             groups.emplace_back(OperUsageGroup { std::move(group_name), std::vector { std::move(oper) } });
@@ -104,6 +113,16 @@ asst::battle::copilot::OperUsageGroups asst::CopilotConfig::parse_groups(const j
                 oper.skill = oper_info.get("skill", 1);
                 oper.skill_usage = static_cast<battle::SkillUsage>(oper_info.get("skill_usage", 0));
                 oper.skill_times = oper_info.get("skill_times", 1); // 使用技能的次数，默认为 1，兼容曾经的作业
+                
+                // 解析练度需求
+                if (auto req_opt = oper_info.find("requirements")) {
+                    oper.requirements.elite = req_opt->get("elite", 0);
+                    oper.requirements.level = req_opt->get("level", 0);
+                    oper.requirements.skill_level = req_opt->get("skill_level", 0);
+                    oper.requirements.module = req_opt->get("module", 0);
+                    oper.requirements.potentiality = req_opt->get("potentiality", 0);
+                }
+                
                 oper_vec.emplace_back(std::move(oper));
             }
             groups.emplace_back(OperUsageGroup { std::move(group_name), std::move(oper_vec) });
