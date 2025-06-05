b91bc9dfe8b5e20913453cc397f9772f9bbc2025
uye <99072975+ABA2396@users.noreply.github.com>
2025-06-05 17:20:01 +0800

    chore: MirrorChyan更新支持判断完整包

--- src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
@@ -1074,6 +1074,11 @@ public class VersionUpdateViewModel : Screen
             return CheckUpdateRetT.UnknownError;
         }
 
+        if (data["data"]?["update_type"]?.ToObject<string>() == "full")
+        {
+            return CheckUpdateRetT.NewVersionNoOtaPackage;
+        }
+
         var version = data["data"]?["version_name"]?.ToString();
         if (string.IsNullOrEmpty(version))
         {
