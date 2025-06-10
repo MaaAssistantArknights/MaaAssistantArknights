3db8613a434c7d6d31bb85fb7cc51f88dd427f17
status102 <102887808+status102@users.noreply.github.com>
2025-06-10 23:33:35 +0800

    chore: wpf各种提示 (#12930)
    * chore: 给喜欢指定参数不看说明的加个提示
    
    * chore: 给代理倍率加个?
    
    * fix: 减重繁中
    
    * chore: KR tweak translation
    
    * i18n: EN dot symbol
    
    * feat: 带提示控件使用 TooltipBlock
    
    * chore: 调整无序列表显示
    
    * chore: 调整显示效果
    
    * perf: 简化自定干员提示
    
    * fix: TooltipText 内容错误
    
    * chore: 调整 TooltipBlock 默认 Margin
    
    * chore: 重置 TextBox InitialShowDelay 默认行为
    
    * chore: 修改描述
    
    * chore: 修改自动战斗描述
    
    * perf: 优化控件布局
    
    * feat: 新增强制Github下载描述
    
    * chore: 调整绑定
    
    ---------
    
    Co-authored-by: HX3N <scarlet7518@gmail.com>
    Co-authored-by: Constrat <56174894+Constrat@users.noreply.github.com>
    Co-authored-by: uye <99072975+ABA2396@users.noreply.github.com>

--- src/MaaWpfGui/Styles/Controls/TooltipBlock.xaml.cs
+++ src/MaaWpfGui/Styles/Controls/TooltipBlock.xaml.cs
@@ -11,8 +11,10 @@
 // but WITHOUT ANY WARRANTY
 // </copyright>
 #nullable enable
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace MaaWpfGui.Styles.Controls;
 
@@ -25,11 +27,22 @@ public partial class TooltipBlock : UserControl
     {
         InitializeComponent();
         DataContext = this;
+
+        Opacity = NormalOpacity;
+
+        MouseEnter += OnMouseEnter;
+        MouseLeave += OnMouseLeave;
     }
 
-    public static readonly DependencyProperty TooltipTextProperty = DependencyProperty.Register(nameof(TooltipText), typeof(string), typeof(TooltipBlock), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty TooltipTextProperty = DependencyProperty.Register(nameof(TooltipText), typeof(string), typeof(TooltipBlock), new(string.Empty));
 
-    public static readonly DependencyProperty TooltipMaxWidthProperty = DependencyProperty.Register(nameof(TooltipMaxWidth), typeof(double), typeof(TooltipBlock), new PropertyMetadata(double.MaxValue));
+    public static readonly DependencyProperty TooltipMaxWidthProperty = DependencyProperty.Register(nameof(TooltipMaxWidth), typeof(double), typeof(TooltipBlock), new(double.MaxValue));
+
+    public static readonly DependencyProperty NormalOpacityProperty = DependencyProperty.Register(nameof(NormalOpacity), typeof(double), typeof(TooltipBlock), new(0.7, OnOpacityChanged));
+
+    public static readonly DependencyProperty HoverOpacityProperty = DependencyProperty.Register(nameof(HoverOpacity), typeof(double), typeof(TooltipBlock), new(1.0, OnOpacityChanged));
+
+    public static readonly DependencyProperty InitialShowDelayProperty = DependencyProperty.Register(nameof(InitialShowDelay), typeof(int), typeof(TooltipBlock), new(200));
 
     public string TooltipText
     {
@@ -43,5 +56,53 @@ public partial class TooltipBlock : UserControl
     {
         get => (double)GetValue(TooltipMaxWidthProperty);
         set => SetValue(TooltipMaxWidthProperty, value);
+    }
+
+    public double NormalOpacity
+    {
+        get => (double)GetValue(NormalOpacityProperty);
+        set => SetValue(NormalOpacityProperty, value);
+    }
+
+    public double HoverOpacity
+    {
+        get => (double)GetValue(HoverOpacityProperty);
+        set => SetValue(HoverOpacityProperty, value);
+    }
+
+    public int InitialShowDelay
+    {
+        get => (int)GetValue(InitialShowDelayProperty);
+        set => SetValue(InitialShowDelayProperty, value);
+    }
+
+    private static void OnOpacityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is TooltipBlock { IsMouseOver: false } tooltipBlock)
+        {
+            tooltipBlock.Opacity = tooltipBlock.NormalOpacity;
+        }
+    }
+
+    private void OnMouseEnter(object sender, EventArgs e)
+    {
+        AnimateOpacity(HoverOpacity);
+    }
+
+    private void OnMouseLeave(object sender, EventArgs e)
+    {
+        AnimateOpacity(NormalOpacity);
+    }
+
+    private void AnimateOpacity(double targetOpacity)
+    {
+        var animation = new DoubleAnimation
+        {
+            To = targetOpacity,
+            Duration = new(TimeSpan.FromMilliseconds(InitialShowDelay)),
+            FillBehavior = FillBehavior.HoldEnd,
+        };
+
+        BeginAnimation(OpacityProperty, animation);
     }
 }
