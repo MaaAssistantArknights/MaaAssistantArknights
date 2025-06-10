839835fcba05eff3fe7bc88427b2102d9db1c6c7
soundofautmn <62000745+soundofautumn@users.noreply.github.com>
2025-06-10 14:47:31 +0800

    chore: 统一更新copyright

--- src/MaaWpfGui/Services/Notification/DummyNotificationProvider.cs
+++ src/MaaWpfGui/Services/Notification/DummyNotificationProvider.cs
@@ -1,3 +1,16 @@
+// <copyright file="DummyNotificationProvider.cs" company="MaaAssistantArknights">
+// Part of the MaaWpfGui project, maintained by the MaaAssistantArknights team (Maa Team)
+// Copyright (C) 2021-2025 MaaAssistantArknights Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License v3.0 only as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+// </copyright>
+
 ﻿// <copyright file="DummyNotificationProvider.cs" company="MaaAssistantArknights">
 // MaaWpfGui - A part of the MaaCoreArknights project
 // Copyright (C) 2021 MistEO and Contributors
