650a48b8a53a3a7cfde99a194f25d038fef534e7
uye <99072975+ABA2396@users.noreply.github.com>
2025-07-06 12:46:03 +0800

    fix: 高版本检测到低版本更新时错误提示无更新包

--- src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/VersionUpdateViewModel.cs
@@ -1073,14 +1073,6 @@ public class VersionUpdateViewModel : Screen
             return CheckUpdateRetT.UnknownError;
         }
 
-        if (data["data"]?["update_type"]?.ToObject<string>() == "full")
-        {
-            using var toast = new ToastNotification(LocalizationHelper.GetString("NewVersionNoOtaPackage"));
-            toast.Show(30);
-            _logger.Warning("No OTA package found, but full package found.");
-            Instances.TaskQueueViewModel.AddLog(LocalizationHelper.GetString("NewVersionNoOtaPackage"), UiLogColor.Warning);
-        }
-
         var version = data["data"]?["version_name"]?.ToString();
         if (string.IsNullOrEmpty(version))
         {
@@ -1090,6 +1082,14 @@ public class VersionUpdateViewModel : Screen
         if (!NeedToUpdate(version))
         {
             return CheckUpdateRetT.AlreadyLatest;
+        }
+
+        if (data["data"]?["update_type"]?.ToObject<string>() == "full")
+        {
+            using var toast = new ToastNotification(LocalizationHelper.GetString("NewVersionNoOtaPackage"));
+            toast.Show(30);
+            _logger.Warning("No OTA package found, but full package found.");
+            Instances.TaskQueueViewModel.AddLog(LocalizationHelper.GetString("NewVersionNoOtaPackage"), UiLogColor.Warning);
         }
 
         // 到这里已经确定有新版本了
