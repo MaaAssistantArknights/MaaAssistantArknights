97461409dce6185828db9ec006808a057657201b
status102 <102887808+status102@users.noreply.github.com>
2025-07-11 09:20:16 +0800

    chore: 新Config长草任务存储已完成部分预载

---
+++ src/MaaWpfGui/Configuration/Single/MaaTask/InfrastTask.cs
@@ -0,0 +1,29 @@
+// <copyright file="InfrastTask.cs" company="MaaAssistantArknights">
+// MaaWpfGui - A part of the MaaCoreArknights project
+// Copyright (C) 2021 MistEO and Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License v3.0 only as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+// </copyright>
+
+#nullable enable
+using MaaWpfGui.ViewModels.UserControl.TaskQueue;
+using static MaaWpfGui.Main.AsstProxy;
+
+namespace MaaWpfGui.Configuration.Single.MaaTask;
+public class InfrastTask : BaseTask
+{
+    public InfrastTask() => TaskType = TaskType.Infrast;
+
+    /// <summary>
+    /// Gets or sets 基建切换模式
+    /// </summary>
+    public InfrastMode Mode { get; set; } = InfrastMode.Normal;
+
+
+}
