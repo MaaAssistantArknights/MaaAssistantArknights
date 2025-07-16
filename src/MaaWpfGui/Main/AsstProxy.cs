686af793a00a68f2a7e13c045be636a79c62af73
status102 <102887808+status102@users.noreply.github.com>
2025-07-16 17:08:29 +0800

    perf: 干员因模组不可选中时, 加个输出 (#13230)
    * perf: 干员因模组不可选中时, 加个输出
    
    临时措施，总感觉还要改
    
    * chore: Auto update by pre-commit hooks [skip changelog]
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/MaaWpfGui/Main/AsstProxy.cs
+++ src/MaaWpfGui/Main/AsstProxy.cs
@@ -1707,15 +1707,24 @@ namespace MaaWpfGui.Main
                     break;
 
                 case "BattleFormationSelected":
-                    var oper_name = DataHelper.GetLocalizedCharacterName(subTaskDetails!["selected"]?.ToString());
-                    var group_name = subTaskDetails!["group_name"]?.ToString();
-                    if (group_name is not null && oper_name != group_name)
                     {
-                        oper_name = $"{group_name} => {oper_name}";
+                        var oper_name = DataHelper.GetLocalizedCharacterName(subTaskDetails!["selected"]?.ToString());
+                        var group_name = subTaskDetails!["group_name"]?.ToString();
+                        if (group_name is not null && oper_name != group_name)
+                        {
+                            oper_name = $"{group_name} => {oper_name}";
+                        }
+
+                        Instances.CopilotViewModel.AddLog(LocalizationHelper.GetString("BattleFormationSelected") + oper_name);
+                        break;
                     }
 
-                    Instances.CopilotViewModel.AddLog(LocalizationHelper.GetString("BattleFormationSelected") + oper_name);
-                    break;
+                case "BattleFormationOperUnavailable":
+                    {
+                        var oper_name = DataHelper.GetLocalizedCharacterName(subTaskDetails!["oper_name"]?.ToString());
+                        Instances.CopilotViewModel.AddLog(string.Format(LocalizationHelper.GetString("BattleFormationOperUnavailable"), oper_name, subTaskDetails["requirement_type"]));
+                        break;
+                    }
 
                 case "CopilotAction":
                     {
