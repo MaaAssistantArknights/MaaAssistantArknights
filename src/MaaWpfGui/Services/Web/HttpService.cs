5c5867c2c067018f66be8f2427f5c730bcb215f4
MistEO <mistereo@hotmail.com>
2025-07-22 10:16:10 +0800

    chore: make GUI UserAgent simple

--- src/MaaWpfGui/Services/Web/HttpService.cs
+++ src/MaaWpfGui/Services/Web/HttpService.cs
@@ -56,7 +56,7 @@ namespace MaaWpfGui.Services.Web
         public HttpService()
         {
             string uiVersion = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Split('+')[0] ?? "0.0.1";
-            UserAgent = $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76 MaaWpfGui/{uiVersion}";
+            UserAgent = $"MaaWpfGui/{uiVersion}";
 
             ConfigurationHelper.ConfigurationUpdateEvent += (key, old, value) =>
             {
