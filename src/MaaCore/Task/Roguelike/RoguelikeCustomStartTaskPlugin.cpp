f07d3eb845ee54d7c027a804db8a3dc24a579d5f
uye <99072975+ABA2396@users.noreply.github.com>
2025-07-29 01:33:29 +0800

    fix: 无法选中 `坚不可摧` 与 `随心所欲` 分队

--- src/MaaCore/Task/Roguelike/RoguelikeCustomStartTaskPlugin.cpp
+++ src/MaaCore/Task/Roguelike/RoguelikeCustomStartTaskPlugin.cpp
@@ -134,7 +134,7 @@ bool asst::RoguelikeCustomStartTaskPlugin::hijack_squad()
             .run();
     }
 
-    constexpr size_t SwipeTimes = 7;
+    constexpr size_t SwipeTimes = 2;
     for (size_t i = 0; i != SwipeTimes; ++i) {
         if (need_exit()) {
             return false;
@@ -190,18 +190,33 @@ bool asst::RoguelikeCustomStartTaskPlugin::hijack_reward()
 
 bool asst::RoguelikeCustomStartTaskPlugin::hijack_roles()
 {
-    auto image = ctrler()->get_image();
-    OCRer analyzer(image);
-    analyzer.set_task_info("RoguelikeCustom-HijackRoles");
-    analyzer.set_required({ m_customs[RoguelikeCustomType::Roles] });
+    constexpr size_t SwipeTimes = 2;
+    const std::string& required_role = m_customs[RoguelikeCustomType::Roles];
 
-    if (!analyzer.analyze()) {
-        return false;
+    for (size_t i = 0; i != SwipeTimes; ++i) {
+        if (need_exit()) {
+            return false;
+        }
+
+        auto image = ctrler()->get_image();
+        OCRer analyzer(image);
+        analyzer.set_task_info("RoguelikeCustom-HijackRoles");
+        analyzer.set_required({ required_role });
+
+        if (analyzer.analyze()) {
+            const auto& rect = analyzer.get_result().front().rect;
+            ctrler()->click(rect);
+            return true;
+        }
+
+        ProcessTask(*this, { "Roguelike@SquadSlowlySwipeToTheRight" }).run();
+        sleep(Task.get("RoguelikeCustom-HijackRoles")->post_delay);
     }
-    const auto& rect = analyzer.get_result().front().rect;
-    ctrler()->click(rect);
-    return true;
+
+    ProcessTask(*this, { "SwipeToTheLeft" }).run();
+    return false;
 }
+
 
 bool asst::RoguelikeCustomStartTaskPlugin::hijack_core_char()
 {
