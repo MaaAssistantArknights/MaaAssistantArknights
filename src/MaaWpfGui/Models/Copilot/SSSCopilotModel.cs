25b2cc159447dd8a96dc094f153392b5ed08c9c6
soundofautmn <62000745+soundofautumn@users.noreply.github.com>
2025-08-18 20:06:43 +0800

    chore: 拆分干员职业类型枚举

--- src/MaaWpfGui/Models/Copilot/SSSCopilotModel.cs
+++ src/MaaWpfGui/Models/Copilot/SSSCopilotModel.cs
@@ -14,6 +14,7 @@
 #nullable enable
 using System.Collections.Generic;
 using System.Linq;
+using MaaWpfGui.Constants.Enums;
 using MaaWpfGui.Helper;
 using Newtonsoft.Json;
 
@@ -80,57 +81,6 @@ public class SSSCopilotModel : CopilotBase
     /// </summary>
     [JsonProperty("stages")]
     public List<Stage>? Stages { get; set; }
-
-    /// <summary>
-    /// 干员职业类型枚举。
-    /// </summary>
-    private enum Role
-    {
-        /// <summary>
-        /// 近卫/Guard
-        /// </summary>
-        Warrior,
-
-        /// <summary>
-        /// 先锋/Vanguard
-        /// </summary>
-        Pioneer,
-
-        /// <summary>
-        /// 医疗/Medic
-        /// </summary>
-        Medic,
-
-        /// <summary>
-        /// 重装/Defender/坦克
-        /// </summary>
-        Tank,
-
-        /// <summary>
-        /// 狙击/Sniper
-        /// </summary>
-        Sniper,
-
-        /// <summary>
-        /// 术师/术士/法师/Caster
-        /// </summary>
-        Caster,
-
-        /// <summary>
-        /// 辅助/支援/Supporter
-        /// </summary>
-        Support,
-
-        /// <summary>
-        /// 特种/Specialist
-        /// </summary>
-        Special,
-
-        /// <summary>
-        /// 无人机/召唤物/Drone/Summon
-        /// </summary>
-        Drone,
-    }
 
     // 在 SSSCopilotModel 类中嵌入 NameToRole 映射
     private static readonly Dictionary<string, Role> NameToRole = new()
