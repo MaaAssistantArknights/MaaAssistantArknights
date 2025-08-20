1429e042c3b65fd8a77b1ac35913be0b4d2d309e
SherkeyXD <57581480+SherkeyXD@users.noreply.github.com>
2025-08-20 14:50:52 +0800

    docs: theme config migration

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,13 +1,14 @@
-import { defineUserConfig } from "vuepress";
-import { getDirname, path } from "vuepress/utils";
-import { viteBundler } from "@vuepress/bundler-vite";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from 'vuepress'
 import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
-import Theme from "./theme";
+import { plumeTheme } from 'vuepress-theme-plume'
 
-const __dirname = getDirname(import.meta.url);
+import DocSearchConfig from './plugins/search';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineUserConfig({
-  base: "/docs/",
+  base: "/",
   lang: "zh-CN",
   title: "MaaAssistantArknights",
   description: "MAA",
@@ -35,27 +36,6 @@ export default defineUserConfig({
       description: "선적 서류 비치",
     },
   },
-
-  markdown: {
-    headers: {
-      level: [2, 3, 4, 5],
-    },
-  },
-
-  theme: Theme,
-
-  alias: {
-    "@theme-hope/modules/navbar/components/LanguageDropdown": path.resolve(
-      __dirname,
-      "./components/LanguageDropdown.ts",
-    ),
-  },
-
-  plugins: [
-    googleAnalyticsPlugin({
-      id: "G-FJQDKG394Z",
-    }),
-  ],
 
   head: [
     ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
@@ -90,4 +70,53 @@ export default defineUserConfig({
     viteOptions: {},
     vuePluginOptions: {},
   }),
+
+  shouldPrefetch: false,
+
+  theme: plumeTheme({
+    hostname: "https://docs.maa.plus",
+
+    docsRepo: "MaaAssistantArknights/MaaAssistantArknights",
+    docsDir: "/docs",
+    docsBranch: "dev",
+
+    editLink: true,
+    lastUpdated: false,
+    contributors: false,
+    changelog: false,
+
+    blog: false,
+
+    cache: "filesystem",
+
+    search: DocSearchConfig,
+
+    codeHighlighter: {
+      themes: { light: 'one-light', dark: 'one-dark-pro' },
+    },
+
+    // markdown: todo
+
+    watermark: false,
+
+    comment: {
+      provider: "Giscus",
+      repo: "MaaAssistantArknights/maa-website",
+      repoId: "R_kgDOHY7Gyg",
+      category: "Comments",
+      categoryId: "DIC_kwDOHY7Gys4CgoVH",
+      mapping: "pathname",
+      strict: true,
+      lazyLoading: true,
+    },
+
+    //replaceAssets: isProd ? "https://cdn.maa.plus" : false,
+
+  }),
+
+  plugins: [
+    googleAnalyticsPlugin({
+      id: "G-FJQDKG394Z",
+    }),
+  ],
 });
