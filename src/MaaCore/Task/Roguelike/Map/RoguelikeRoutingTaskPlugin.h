e0777ec199a615fed80317ed94e8a431355cd8de
travellerse <travellers0238@outlook.com>
2025-08-25 15:26:05 +0800

    feat: 支持识别常乐节点的子类型

--- src/MaaCore/Task/Roguelike/Map/RoguelikeRoutingTaskPlugin.h
+++ src/MaaCore/Task/Roguelike/Map/RoguelikeRoutingTaskPlugin.h
@@ -62,8 +62,8 @@ private:
     // ———————— constants and variables ———————————————————————————————————————————————
     RoutingStrategy m_routing_strategy = RoutingStrategy::None;
     RoguelikeMap m_map;
-    // 界园树洞平面地图
-    RoguelikeBoskyPassageMap m_bosky_map;
+    // 界园树洞平面地图 - 单例模式
+    RoguelikeBoskyPassageMap& m_bosky_map = RoguelikeBoskyPassageMap::get_instance();
     bool m_need_generate_map = true;
     size_t m_selected_column = 0;  // 当前选中节点所在列
     int m_selected_x = 0;          // 当前选中节点的横坐标 (Rect.x)
