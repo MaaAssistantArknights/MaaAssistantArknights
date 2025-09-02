7a35735593000ea83d17ad4367b345f8d5024967
Lucien Shaw <myxlc55@outlook.com>
2025-09-02 13:44:41 +0800

    i18n: 添加语言切换按钮

--- website/apps/web/src/App.tsx
+++ website/apps/web/src/App.tsx
@@ -1,11 +1,13 @@
 import { HomeHero } from './components/home/HomeHero'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { ThemeToggle } from './components/ThemeToggle'
+import { LanguageToggle } from './components/LanguageToggle'
 
 function App() {
   return (
     <ThemeProvider>
       <main className="h-full max-h-screen overflow-hidden dark:bg-[#080808] bg-[#f5f5f5] transition-colors duration-300">
+        <LanguageToggle />
         <ThemeToggle />
         <section className="h-screen min-h-[20rem] w-full relative">
           <HomeHero />
