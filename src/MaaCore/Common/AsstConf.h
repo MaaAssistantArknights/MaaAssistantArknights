b9f1e53057ac08ed7bb5333c44b003da53bafcdd
MistEO <mistereo@hotmail.com>
2025-09-03 16:53:02 +0800

    fix: build error

--- src/MaaCore/Common/AsstConf.h
+++ src/MaaCore/Common/AsstConf.h
@@ -38,18 +38,3 @@
 #define ASST_SUPPRESS_CV_WARNINGS_START
 #define ASST_SUPPRESS_CV_WARNINGS_END
 #endif
-
-// #ifdef __GNUC__
-// #def ine ASST_AUTO_DEDUCED_ZERO_INIT_START \
-//    ASST_DO_PRAGMA(GCC diagnostic push)   \
-//    ASST_DO_PRAGMA(GCC diagnostic ignored "-Wmissing-field-initializers")
-// #define ASST_AUTO_DEDUCED_ZERO_INIT_END ASST_DO_PRAGMA(GCC diagnostic pop)
-// #elif defined(__clang__)
-// #def ine ASST_AUTO_DEDUCED_ZERO_INIT_START \
-//    ASST_DO_PRAGMA(clang diagnostic push) \
-//    ASST_DO_PRAGMA(clang diagnostic ignored "-Wmissing-field-initializers")
-// #define ASST_AUTO_DEDUCED_ZERO_INIT_END ASST_DO_PRAGMA(clang diagnostic pop)
-// #else
-// #define ASST_AUTO_DEDUCED_ZERO_INIT_START
-// #define ASST_AUTO_DEDUCED_ZERO_INIT_END
-// #endif
