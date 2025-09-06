72e4e03133cb4266959d88e11cc3179d759dea1c
Weiyou Wang <44151844+Alan-Charred@users.noreply.github.com>
2025-09-06 22:38:11 +1000

    refactor: 维护信用作战相关功能及其文档

--- src/MaaCore/Task/Miscellaneous/CreditFightTask.h
+++ src/MaaCore/Task/Miscellaneous/CreditFightTask.h
@@ -2,6 +2,7 @@
 
 #include "Task/PackageTask.h"
 
+#include "Task/Fight/StageNavigationTask.h"
 #include "Task/Interface/CopilotTask.h"
 #include "Utils/WorkingDir.hpp"
 
@@ -18,9 +19,10 @@ public:
     CreditFightTask(const AsstCallback& callback, Assistant* inst, std::string_view task_chain);
     virtual ~CreditFightTask() override = default;
 
-    void set_select_formation(int index);
+    bool set_params(int formation_index = 0);
 
 private:
-    std::shared_ptr<asst::CopilotTask> m_copilot_task_ptr = nullptr;
+    std::shared_ptr<CopilotTask> m_copilot_task_ptr = nullptr;
+    std::shared_ptr<StageNavigationTask> m_stage_navigation_task_ptr = nullptr;
 };
 } // namespace asst
