c6f5e99992cab2da02e9ba13a699ad94cc30eab8
MistEO <mistereo@hotmail.com>
2025-09-09 00:22:02 +0800

    chore: 再糊点注释

--- src/Cpp/main.cpp
+++ src/Cpp/main.cpp
@@ -11,8 +11,8 @@ int main([[maybe_unused]] int argc, char** argv)
     auto working_path = std::filesystem::path(argv[0]).parent_path();
 
 #ifdef ASST_DEBUG
-    // <path>/build/bin/Debug or <path>/build/bin/Release
     if (!std::filesystem::exists(working_path / "resource")) {
+        // <ProjectDir>/build/bin/Debug or <ProjectDir>/build/bin/Release
         working_path = working_path.parent_path().parent_path().parent_path();
     }
 #endif
