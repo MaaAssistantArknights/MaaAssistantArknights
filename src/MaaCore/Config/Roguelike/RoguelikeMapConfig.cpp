8afb7bf65bfd49097fde6e525860dd20edfc9f8b
travellerse <travellers0238@outlook.com>
2025-09-10 20:33:21 +0800

    refactor: 重命名节点类型获取函数

--- src/MaaCore/Config/Roguelike/RoguelikeMapConfig.cpp
+++ src/MaaCore/Config/Roguelike/RoguelikeMapConfig.cpp
@@ -12,7 +12,7 @@ bool asst::RoguelikeMapConfig::parse(const json::value& json)
     m_templ_type_mappings.erase(theme);
 
     for (const auto& node_json : json.at("node").as_array()) {
-        const RoguelikeNodeType node_type = get_node_type(node_json.at("type").as_string());
+        const RoguelikeNodeType node_type = name2type(node_json.at("type").as_string());
         for (const auto& template_json : node_json.at("template").as_array()) {
             const std::string node_template = template_json.as_string();
             m_templ_type_mappings[theme].emplace(node_template, node_type);
