83dcd3d5c44b5579743d869d58e445b63a77f66a
status102 <102887808+status102@users.noreply.github.com>
2025-09-17 00:57:06 +0800

    chore: file header

--- src/MaaWpfGui/Styles/Controls/NoAutomationDataGrid.cs
+++ src/MaaWpfGui/Styles/Controls/NoAutomationDataGrid.cs
@@ -1,4 +1,4 @@
-// <copyright file="NoAutomationDataGrid .cs" company="MaaAssistantArknights">
+// <copyright file="NoAutomationDataGrid.cs" company="MaaAssistantArknights">
 // Part of the MaaWpfGui project, maintained by the MaaAssistantArknights team (Maa Team)
 // Copyright (C) 2021-2025 MaaAssistantArknights Contributors
 //
