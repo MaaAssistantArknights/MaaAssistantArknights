976838a755c281337b43cc24a53d99e47958d92e
Lucien Shaw <myxlc55@outlook.com>
2025-09-30 17:10:35 +0800

    feat: 文档站的开发者工具console会打印随机字符画

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,5 +1,7 @@
 import { defineClientConfig } from 'vuepress/client';
 
+import { getAsciiArt } from './asciiArtService';
+
 import AsciiArt from './components/AsciiArt.vue';
 import ImageGrid from './components/ImageGrid.vue';
 
@@ -9,5 +11,12 @@ export default defineClientConfig({
   enhance: ({ app }) => {
     app.component('AsciiArt', AsciiArt);
     app.component('ImageGrid', ImageGrid);
+    
+    // 输出一个随机字符画
+    const art = getAsciiArt()
+    console.log(
+      '%c' + art,
+      'white-space: pre;'
+    )
   },
 });
