9b5445d1a986e2a05e9ce34fc51df567f6652658
晓丶梦丶仁 <74444214+Daydreamer114@users.noreply.github.com>
2025-12-27 20:41:51 +0800

    perf: move_camera 允许微调镜头 (#15220)

--- src/MaaCore/Task/BattleHelper.cpp
+++ src/MaaCore/Task/BattleHelper.cpp
@@ -973,8 +973,8 @@ bool asst::BattleHelper::move_camera(const std::pair<double, double>& delta)
 
     update_kills(m_inst_helper.ctrler()->get_image());
 
-    // 还没转场的时候
-    if (m_kills != 0) {
+    // 等待转场，但排除微调
+    if (m_kills != 0 && delta.first > 2) {
         wait_until_end(false);
     }
 
