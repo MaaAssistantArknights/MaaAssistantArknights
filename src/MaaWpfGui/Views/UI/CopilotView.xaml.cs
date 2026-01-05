2af35681ebc7e95058df2f81bc1efb22e882d004
HY <45387477+momomochi987@users.noreply.github.com>
2026-01-05 20:29:13 +0800

    feat: 增強自动战斗文件选择功能，支持多级路径和相对目录 (#15174)
    * chore: 先依照保全派駐週期粗略分類
    
    * chore: 換個位置
    
    * chore: 換成用地圖名稱分類
    
    * chore: 再改 再改
    
    * chore: 別空白了 伺服器會被殺死的
    
    * chore: 補上下拉選單顯示的 json 檔案名稱，避免解析錯誤
    
    * chore: 幫展開後的下拉選單加上寬度限制
    
    * Revert "chore: 幫展開後的下拉選單加上寬度限制"
    
    This reverts commit d5f80c9d6e9950eb23aceeac3b06ae46082e786b.
    
    * feat: 新增文件树视图项样式
    
    * perf: 优化自动战斗下拉列表
    
    * feat: 统一样式
    
    * chore: 删除多余调用
    
    ---------
    
    Co-authored-by: uye <99072975+ABA2396@users.noreply.github.com>

---
+++ src/MaaWpfGui/Views/UI/CopilotView.xaml.cs
@@ -0,0 +1,37 @@
+// <copyright file="CopilotView.xaml.cs" company="MaaAssistantArknights">
+// Part of the MaaWpfGui project, maintained by the MaaAssistantArknights team (Maa Team)
+// Copyright (C) 2021-2025 MaaAssistantArknights Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License v3.0 only as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+// </copyright>
+
+using System.Windows;
+using MaaWpfGui.Models;
+using MaaWpfGui.ViewModels.UI;
+
+namespace MaaWpfGui.Views.UI;
+
+/// <summary>
+/// Interaction logic for CopilotView.xaml
+/// </summary>
+public partial class CopilotView
+{
+    public CopilotView()
+    {
+        InitializeComponent();
+    }
+
+    private void FileTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    {
+        if (DataContext is CopilotViewModel viewModel && e.NewValue is CopilotFileItem fileItem && !fileItem.IsFolder)
+        {
+            viewModel.OnFileSelected(fileItem);
+        }
+    }
+}
