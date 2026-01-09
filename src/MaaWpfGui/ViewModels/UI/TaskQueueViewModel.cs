5a301973622e0f3676c65f9c887e2488556f7b20
uye <99072975+ABA2396@users.noreply.github.com>
2026-01-09 20:41:56 +0800

    debug: 测试代码
    [skip changelog]

--- src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
@@ -1503,8 +1503,6 @@ public class TaskQueueViewModel : Screen
     /// <returns>Task</returns>
     public async Task LinkStart()
     {
-        AddLog(1.ToString());
-        return;
         if (!_runningState.GetIdle())
         {
             _logger.Information("Not idle, return.");
