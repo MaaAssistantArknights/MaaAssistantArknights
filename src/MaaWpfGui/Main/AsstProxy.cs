45b977574cca4f4561a1a9fc7b9d110dc11e2f7f
uye <99072975+ABA2396@users.noreply.github.com>
2026-01-17 18:05:33 +0800

    fix: 怎么把成就注释掉了

--- src/MaaWpfGui/Main/AsstProxy.cs
+++ src/MaaWpfGui/Main/AsstProxy.cs
@@ -1434,11 +1434,10 @@ public class AsstProxy
                             Instances.CopilotViewModel.AddLog(LocalizationHelper.GetString("MissingOperators"), UiLogColor.Error);
                         }
 
-                        /*
                         if (missingOpers is not null && missingOpers.Count >= 2)
                         {
                             AchievementTrackerHelper.Instance.Unlock(AchievementIds.Irreplaceable);
-                        }*/
+                        }
                     }
 
                     break;
