a76ac22c1376a329f87f71b3135eeeb06744f49a
status102 <102887808+status102@users.noreply.github.com>
2026-02-09 22:39:32 +0800

    fix: 干员等级跨精英化时判断出错修复

--- src/MaaCore/Task/Miscellaneous/BattleFormationTask.cpp
+++ src/MaaCore/Task/Miscellaneous/BattleFormationTask.cpp
@@ -662,7 +662,7 @@ bool asst::BattleFormationTask::check_oper_level(
         _level = 0;
     }
 
-    if (_elite < oper.requirements.elite || _level < oper.requirements.level) {
+    if (_elite < oper.requirements.elite || (_elite == oper.requirements.elite && _level < oper.requirements.level)) {
         LogWarn << __FUNCTION__ << "| Elite" << _elite << "level" << _level << ", require:" << oper.requirements.elite
                 << oper.requirements.level;
         json::value info = basic_info_with_what("BattleFormationOperUnavailable");
