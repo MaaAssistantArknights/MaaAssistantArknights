10e3c286457f9d043257fe94f0d93aef17b8e382
Aliothmoon <aliothmoon@outlook.com>
2026-02-11 01:40:16 +0800

    fix: add __ANDROID__ macro for Android platform

--- src/MaaCore/Assistant.cpp
+++ src/MaaCore/Assistant.cpp
@@ -142,11 +142,13 @@ bool asst::Assistant::set_instance_option(InstanceOptionKey key, const std::stri
             m_ctrler->set_touch_mode(TouchMode::MacPlayTools);
             return true;
         }
+#ifdef __ANDROID__
         else if (constexpr std::string_view Android = "Android"; value == Android) {
             m_ctrler->set_touch_mode(TouchMode::Android);
             return true;
         }
         break;
+#endif
     case InstanceOptionKey::DeploymentWithPause:
         if (constexpr std::string_view Enable = "1"; value == Enable) {
             m_ctrler->set_swipe_with_pause(true);
