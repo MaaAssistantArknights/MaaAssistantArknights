c825f823de4b67658fb620a4bc952e1b4705d507
status102 <102887808+status102@users.noreply.github.com>
2026-04-11 17:08:23 +0800

    Revert "fix: prts.plus改为zoot.plus"
    This reverts commit d9b6eb7020661986783ebec1c434b931965a6485.

--- src/MaaWpfGui/Constants/MaaUrls.cs
+++ src/MaaWpfGui/Constants/MaaUrls.cs
@@ -33,7 +33,7 @@ public static class MaaUrls
 
     public const string Discord = "https://discord.gg/23DfZ9uA4V";
 
-    public const string PrtsPlus = "https://zoot.plus";
+    public const string PrtsPlus = "https://prts.plus";
 
     public const string PrtsPlusCopilotGet = "https://prts.maa.plus/copilot/get/";
 
