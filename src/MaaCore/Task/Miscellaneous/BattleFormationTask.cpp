2a5cdf24e71e06801d3330814e4c87acf3b5cd47
status102 <102887808+status102@users.noreply.github.com>
2026-04-16 00:12:10 +0800

    perf: 自动编队预编队后检查选中情况

--- src/MaaCore/Task/Miscellaneous/BattleFormationTask.cpp
+++ src/MaaCore/Task/Miscellaneous/BattleFormationTask.cpp
@@ -266,6 +266,36 @@ void asst::BattleFormationTask::formation_with_last_opers()
         m_opers_in_formation->emplace(oper_name, group_name);
         it = last_formation.erase(it);
     }
+
+    const auto& ret = analyzer_opers(ctrler()->get_image());
+    for (auto oper_it = m_opers_in_formation->begin(); oper_it != m_opers_in_formation->end();) {
+        const std::string& oper_name = oper_it->first;
+        const auto& in_page_it =
+            std::ranges::find_if(ret, [&](const QuickFormationOper& op) { return op.text == oper_name; });
+        if (in_page_it != ret.end() && in_page_it->is_selected) [[likely]] {
+            ++oper_it; // oper is selected, expected
+            continue;
+        }
+        if (in_page_it == ret.end()) {
+            LogWarn << __FUNCTION__ << "| After fast selection, oper" << oper_name << "is not found in current page";
+        }
+        else {
+            LogWarn << __FUNCTION__ << "| After fast selection, oper" << oper_name << "is not selected in current page";
+        }
+
+        if (auto group_it = formation_view.find(oper_it->second); group_it != formation_view.end()) {
+            // 在找到的组中查找具体的干员
+            auto oper_in_group_it =
+                std::ranges::find_if(*(group_it->second), [&](battle::OperUsage& op) { return op.name == oper_name; });
+            if (oper_in_group_it != group_it->second->end()) [[likely]] {
+                LogInfo << __FUNCTION__ << "| Oper" << oper_name << "is expected to be selected, but not. Reset status";
+
+                oper_in_group_it->status = battle::OperStatus::Unchecked;
+            }
+        }
+
+        oper_it = m_opers_in_formation->erase(oper_it);
+    }
 }
 
 bool asst::BattleFormationTask::add_formation(battle::Role role, const std::vector<OperGroup*>& oper_group)
