39e01179cefc68716b4c078e99f3030588a3475e
H2O_MERO <60950002+H2O-MERO@users.noreply.github.com>
2026-05-27 02:38:59 +0800

    feat: 对一键长草列表进行了交互优化，并添加了复制按钮 (#16733)
    * feat: 对一键长草列表进行了交互优化
    
    * feat: 增加了删除和编辑两种矢量图标并使用
    
    * fix:修复了按钮颜色没有及时恢复的问题
    
    * feat:加入了复制按钮
    
    * fix:修复了复制任务后部分任务丢失数据和出现渲染异常的问题
    
    * refactor: 跟进和优化了引导界面的任务设置部分样式
    
    * perf: 任务index设置
    
    * perf:
    
    * revert
    
    * fix: NameDisplay -> NameOrTaskType
    
    * fix: 额外的图标
    
    * fix: GroupName
    
    * rft: 提取通用属性，修复 RadioButton 绑定错误，非 Idle 时不显示对应操作按钮
    
    * chore: 删除左键提示
    
    * fix: 高亮背景色
    
    ---------
    
    Co-authored-by: status102 <102887808+status102@users.noreply.github.com>
    Co-authored-by: uye <99072975+ABA2396@users.noreply.github.com>

--- src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
+++ src/MaaWpfGui/ViewModels/UI/TaskQueueViewModel.cs
@@ -22,12 +22,14 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using JetBrains.Annotations;
+using MaaWpfGui.Configuration;
 using MaaWpfGui.Configuration.Factory;
 using MaaWpfGui.Configuration.Single.MaaTask;
 using MaaWpfGui.Constants;
@@ -176,6 +178,11 @@ public class TaskQueueViewModel : Screen
                 TaskItemViewModels[e.NewStartingIndex].Index = e.NewStartingIndex;
                 TaskItemViewModels.FirstOrDefault(i => i.EnableSetting)?.EnableSetting = false;
                 TaskItemViewModels[e.NewStartingIndex].EnableSetting = true;
+
+                for (int i = e.NewStartingIndex + 1; i < TaskItemViewModels.Count; i++)
+                {
+                    TaskItemViewModels[i].Index = i;
+                }
             }
             else if (e.Action == NotifyCollectionChangedAction.Remove)
             {
@@ -1415,6 +1422,37 @@ public class TaskQueueViewModel : Screen
         {
             task.IsEnable = originalIsEnable;
         }
+    }
+
+    /// <summary>
+    /// 复制任务
+    /// </summary>
+    /// <param name="taskItem">任务项</param>
+    [UsedImplicitly]
+    public void CopyTask(TaskItemViewModel taskItem)
+    {
+        if (taskItem == null || !Idle)
+        {
+            return;
+        }
+
+        var index = taskItem.Index;
+        if (index < 0 || index >= ConfigFactory.CurrentConfig.TaskQueue.Count)
+        {
+            return;
+        }
+
+        var oldTask = ConfigFactory.CurrentConfig.TaskQueue[index];
+        var oldTaskJson = JsonSerializer.Serialize(oldTask);
+        if (JsonSerializer.Deserialize(oldTaskJson, oldTask.GetType()) is not BaseTask newTask)
+        {
+            AddLog("Task copy failed", UiLogColor.Error);
+            return;
+        }
+        newTask.Name = newTask.NameOrTaskType + " (2)";
+        ConfigFactory.CurrentConfig.TaskQueue.Insert(index + 1, newTask);
+        TaskItemViewModels.Insert(index + 1, new TaskItemViewModel(newTask.NameOrTaskType));
+        AddLog(LocalizationHelper.GetStringFormat("TaskCopied", newTask.NameOrTaskType), UiLogColor.Info);
     }
 
     /// <summary>
