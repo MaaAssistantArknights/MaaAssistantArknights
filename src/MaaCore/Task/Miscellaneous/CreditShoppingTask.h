a10391e9d4eaa58026e36b3190c9744298038326
uye <99072975+ABA2396@users.noreply.github.com>
2026-05-30 17:12:59 +0800

    perf: 优化信用商店复核逻辑 (#16932)
    * perf: 优化信用商店复核逻辑
    
    * chore: 增加对 OCR 结果为空的处理

--- src/MaaCore/Task/Miscellaneous/CreditShoppingTask.h
+++ src/MaaCore/Task/Miscellaneous/CreditShoppingTask.h
@@ -30,12 +30,13 @@ protected:
     bool m_only_buy_discount = false;             // 设置只购买折扣信用商品（未打折的白名单物品仍会购买）
     bool m_reserve_max_credit = false;            // 设置消耗信用点至300以下时停止购买商品（仍然会购买白名单物品）
     bool m_info_credit_full = false;              // 设置是否在不购买黑名单物品阶段通知信用点溢出
+    bool verify_commodity_name(const TextRect& commodity) const;
     int credit_ocr();
 
     // 用于在信用商店界面识别商品信息左上角的折扣信息
     int discount_ocr(const Rect& commodity);
 
-    bool credit_shopping(bool white_list_enabled, bool credit_ocr_enabled);
+    bool credit_shopping(bool white_list_enabled, bool should_stop_on_credit);
 
     std::vector<std::string> m_shopping_list;
     bool m_is_white_list = false;
