8f62eec199fc7264bb1409d187989f181607ba0e
uye <99072975+ABA2396@users.noreply.github.com>
2026-07-23 02:45:41 +0800

    fix: 初始化时使用了运行时参数

--- src/MaaWpfGui/Extensions/DateTimeExtension.cs
+++ src/MaaWpfGui/Extensions/DateTimeExtension.cs
@@ -16,7 +16,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using MaaWpfGui.Configuration.Factory;
-using MaaWpfGui.Constants;
 using MaaWpfGui.Constants.Enums;
 using MaaWpfGui.Helper;
 
