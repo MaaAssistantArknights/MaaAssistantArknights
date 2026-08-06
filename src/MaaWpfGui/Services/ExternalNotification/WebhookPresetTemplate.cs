c5b191a272a6eaa0f8921bfa5f57808d85f6a0bf
moranfanhua <moxiaosoffical@outlook.com>
2026-08-06 22:40:07 +0800

    feat(wpf): 为 CustomWebhook 预置模板新增 KOOK 频道与私聊

--- src/MaaWpfGui/Services/ExternalNotification/WebhookPresetTemplate.cs
+++ src/MaaWpfGui/Services/ExternalNotification/WebhookPresetTemplate.cs
@@ -54,6 +54,22 @@ public class WebhookPresetTemplate
             Name = "Discord Webhook",
             Body = $"{{\"content\": \"{{content}}\"}}",
         },
+        new()
+        {
+            Id = "Kook Channel",
+            Name = "KOOK 频道",
+            Url = "https://www.kookapp.cn/api/v3/message/create",
+            Headers = "Authorization: Bot <在此填写机器人 Token>",
+            Body = "{\"type\": 9, \"target_id\": \"<在此填写频道 ID>\", \"content\": \"**{title}**\\n{content}\"}",
+        },
+        new()
+        {
+            Id = "Kook Direct",
+            Name = "KOOK 私聊",
+            Url = "https://www.kookapp.cn/api/v3/direct-message/create",
+            Headers = "Authorization: Bot <在此填写机器人 Token>",
+            Body = "{\"type\": 9, \"target_id\": \"<在此填写用户 ID>\", \"content\": \"**{title}**\\n{content}\"}",
+        },
     ];
 
     public static IReadOnlyList<WebhookPresetTemplate> BuiltInTemplates => _builtInTemplates;
