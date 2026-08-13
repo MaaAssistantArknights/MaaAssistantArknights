f7fbdf85a6daab162dd18052598c2bf9daf30a48
Hao Guan <10684225+hguandl@users.noreply.github.com>
2026-08-13 19:58:45 +0800

    perf: code cleanup

--- src/MaaCore/AsstCallerExtra.cpp
+++ src/MaaCore/AsstCallerExtra.cpp
@@ -11,19 +11,13 @@ extern "C"
         if (!key) {
             return { nullptr, nullptr, nullptr, nullptr };
         }
-
         const auto& level_metadata = asst::Tile.find(key);
         if (!level_metadata) {
             return { nullptr, nullptr, nullptr, nullptr };
         }
-
-        const auto& level_key = level_metadata->first;
-        thread_local auto stage_id = level_key.stageId;
-        thread_local auto code = level_key.code;
-        thread_local auto level_id = level_key.levelId;
-        thread_local auto name = level_key.name;
-
-        return { stage_id.c_str(), code.c_str(), level_id.c_str(), name.c_str() };
+        thread_local Map::LevelKey level_key;
+        level_key = level_metadata->first;
+        return { level_key.stageId.c_str(), level_key.code.c_str(), level_key.levelId.c_str(), level_key.name.c_str() };
     }
 }
 
