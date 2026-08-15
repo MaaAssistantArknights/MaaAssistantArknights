89d3366ccaebda42eb6baa6b2e193c6e1939192c
uye <99072975+ABA2396@users.noreply.github.com>
2026-08-15 17:24:30 +0800

    fix: 修复 ziplist 目录条目过滤失效，zipota 复用完整包自带清单

--- tools/OTAPacker/ziplist.sh
+++ tools/OTAPacker/ziplist.sh
@@ -4,5 +4,4 @@ set -e
 zipinfo -O utf8 -v $1 \
     | sed -n -e '/^Central directory entry/,+3p' -e '/32-bit CRC value (hex):/p' | grep '^  ' \
     | sed -e 's/^  //g' | sed -z 's/\n32-bit CRC value (hex):\s*/\t/g' \
-    | awk '{last = $NF; $NF=""; print last,$0}' \
-    | grep -v '/$'
+    | awk '$1 !~ /\/$/ {last = $NF; $NF=""; print last,$0}'
