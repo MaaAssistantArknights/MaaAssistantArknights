5667a1d9fa406c21cc8e51aed2609486aa060c7f
Weiyou Wang <44151844+Alan-Charred@users.noreply.github.com>
2026-09-08 23:46:02 +0800

    refactor: 基建心情笑脸识别模块换用多模版任务

--- src/MaaCore/Vision/Infrast/InfrastSmileyImageAnalyzer.cpp
+++ src/MaaCore/Vision/Infrast/InfrastSmileyImageAnalyzer.cpp
@@ -1,37 +1,33 @@
 #include "InfrastSmileyImageAnalyzer.h"
-
 #include "MaaUtils/NoWarningCV.hpp"
-
-#include "Utils/StringMisc.hpp"
 #include "Vision/MultiMatcher.h"
 
 bool asst::InfrastSmileyImageAnalyzer::analyze()
 {
-    const static std::unordered_map<infrast::SmileyType, std::string> smiley_map = {
-        { infrast::SmileyType::Rest, "InfrastSmileyOnRest" },
-        { infrast::SmileyType::Work, "InfrastSmileyOnWork" },
-        { infrast::SmileyType::Distract, "InfrastSmileyOnDistract" }
+    const static std::unordered_map<std::string, infrast::SmileyType> templ_name_to_smiley_type = {
+        { "SmileyOnRest.png", infrast::SmileyType::Rest },
+        { "SmileyOnWork.png", infrast::SmileyType::Work },
+        { "SmileyOnDistract.png", infrast::SmileyType::Distract }
     };
 
     m_result.clear();
 
-    MultiMatcher mm_analyzer(m_image);
+    MultiMatcher analyzer(m_image);
+    analyzer.set_task_info("InfrastSmiley");
+    analyzer.set_roi(m_roi);
 
-    decltype(m_result) temp_result;
-    for (const auto& [type, task_name] : smiley_map) {
-        mm_analyzer.set_task_info(task_name);
-        mm_analyzer.set_roi(m_roi);
-        if (!mm_analyzer.analyze()) {
-            continue;
-        }
-        auto& res = mm_analyzer.get_result();
-        for (const MatchRect& mr : res) {
-            temp_result.emplace_back(infrast::Smiley { type, mr.rect });
-#ifdef ASST_DEBUG
-            cv::rectangle(m_image_draw, make_rect<cv::Rect>(mr.rect), cv::Scalar(0, 0, 255), 2);
-#endif
-        }
+    if (!analyzer.analyze()) {
+        return false;
     }
-    m_result = std::move(temp_result);
+
+    std::vector<infrast::Smiley> result;
+    for (const auto& [rect, _, templ_name] : analyzer.get_result()) {
+        result.emplace_back(infrast::Smiley { templ_name_to_smiley_type.at(templ_name), rect });
+#ifdef ASST_DEBUG
+        cv::rectangle(m_image_draw, make_rect<cv::Rect>(rect), cv::Scalar(0, 0, 255), 2);
+#endif
+    }
+
+    m_result = std::move(result);
     return true;
 }
