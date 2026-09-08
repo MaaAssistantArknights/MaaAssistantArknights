ae3d12049de9df0ca0ec0402763308702243639c
Lancarus <duzhen92@qq.com>
2026-09-08 23:32:42 +0800

    fix: 专精入口改为干员档案跳转

--- src/MaaCore/Task/AutoRaise/AutoRaiseProcessTask.h
+++ src/MaaCore/Task/AutoRaise/AutoRaiseProcessTask.h
@@ -38,7 +38,6 @@ private:
     Result execute_mastery(const AutoRaiseTarget& target);
     Result find_and_open_operator(const AutoRaiseTarget& target);
     bool select_operator_role(const std::string& operator_name);
-    bool enter_training_room();
     bool analyze_training_context(std::string& operator_name, std::string& skill_name, int& level);
     bool select_training_trainee(const AutoRaiseTarget& target);
     bool select_training_trainer(const AutoRaiseTarget& target);
