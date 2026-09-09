03bd03a78c0208b4ef50306f894ed361bbf3410b
Radekyspec <w41k3r15347@gmail.com>
2026-09-09 17:59:26 +0800

    fix: 肉鸽开局招募的Swipe参数未除以10

--- src/MaaCore/Task/Roguelike/RoguelikeRecruitTaskPlugin.cpp
+++ src/MaaCore/Task/Roguelike/RoguelikeRecruitTaskPlugin.cpp
@@ -740,7 +740,7 @@ void asst::RoguelikeRecruitTaskPlugin::slowly_swipe(bool to_left, int swipe_dist
         { StartPoint.x + swipe_dist - StartPoint.width, StartPoint.y, StartPoint.width, StartPoint.height },
         swipe_task->special_params.empty() ? 0 : swipe_task->special_params.at(0),
         (swipe_task->special_params.size() < 2) ? false : swipe_task->special_params.at(1),
-        (swipe_task->special_params.size() < 3) ? 1 : swipe_task->special_params.at(2),
-        (swipe_task->special_params.size() < 4) ? 1 : swipe_task->special_params.at(3));
+        (swipe_task->special_params.size() < 3) ? 1 : swipe_task->special_params.at(2) / 10.0,
+        (swipe_task->special_params.size() < 4) ? 1 : swipe_task->special_params.at(3) / 10.0);
     sleep(swipe_task->post_delay);
 }
