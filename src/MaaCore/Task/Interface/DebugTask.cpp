c521746d8b3496236e9860c4dc6e78a2556b9190
status102 <102887808+status102@users.noreply.github.com>
2026-09-11 22:17:20 +0800

    perf: 铲点

--- src/MaaCore/Task/Interface/DebugTask.cpp
+++ src/MaaCore/Task/Interface/DebugTask.cpp
@@ -210,14 +210,9 @@ bool asst::DebugTask::set_params_impl(const json::value& params)
             else {
                 m_eval_threshold = params.get("threshold", 0.8);
             }
-            if (auto resize_opt = params.find<json::array>("resize"); resize_opt) {
-                if (resize_opt->size() != 2 ||
-                    std::ranges::any_of(*resize_opt, [](const json::value& v) { return !v.is_number(); })) {
-                    LogError << "set_params failed, resize must be 2 numbers";
-                    return false;
-                }
-                int resize_w = (*resize_opt)[0].as_integer();
-                int resize_h = (*resize_opt)[1].as_integer();
+            if (auto resize_opt = params.find<std::array<int,2>>("resize"); resize_opt) {
+                int resize_w = (*resize_opt)[0];
+                int resize_h = (*resize_opt)[1];
                 if (resize_w <= 0 || resize_h <= 0) {
                     LogError << "set_params failed, invalid resize:" << resize_w << resize_h;
                     return false;
