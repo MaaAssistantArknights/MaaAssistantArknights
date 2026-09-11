214054b914a2e02f8becab33faf4214f26d5b8e5
uye <99072975+ABA2396@users.noreply.github.com>
2026-09-12 04:07:13 +0800

    chore: 移除一图流复用企鹅物流上报 id

--- src/MaaWpfGui/ViewModels/UserControl/TaskQueue/RecruitSettingsUserControlModel.cs
+++ src/MaaWpfGui/ViewModels/UserControl/TaskQueue/RecruitSettingsUserControlModel.cs
@@ -301,7 +301,7 @@ public class RecruitSettingsUserControlModel : TaskSettingsViewModel, RecruitSet
                 ReportToPenguin = SettingsViewModel.ThirdPartyServiceSettings.EnablePenguin,
                 ReportToYituliu = SettingsViewModel.ThirdPartyServiceSettings.EnableYituliu,
                 PenguinId = SettingsViewModel.ThirdPartyServiceSettings.PenguinId,
-                YituliuId = SettingsViewModel.ThirdPartyServiceSettings.PenguinId,
+                /* YituliuId = SettingsViewModel.ThirdPartyServiceSettings.PenguinId, */
                 ServerType = Instances.SettingsViewModel.ServerType,
             };
 
