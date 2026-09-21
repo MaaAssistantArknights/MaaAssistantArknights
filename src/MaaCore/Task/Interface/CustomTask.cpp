9e513039e85a952c3bd845bd81e0294b275af803
Lancarus <duzhen92@qq.com>
2026-09-21 19:29:15 +0800

    chore: 代码风格

--- src/MaaCore/Task/Interface/CustomTask.cpp
+++ src/MaaCore/Task/Interface/CustomTask.cpp
@@ -1,8 +1,8 @@
 #include "CustomTask.h"
 
 #include "Config/TaskData.h"
-#include "Task/MiniGame/MaterialSynthesisTaskPlugin.h"
 #include "Task/MiniGame/AutoRaisePotentialTaskPlugin.h"
+#include "Task/MiniGame/MaterialSynthesisTaskPlugin.h"
 #include "Task/MiniGame/PixelPaintTaskPlugin.h"
 #include "Task/MiniGame/SecretFrontTaskPlugin.h"
 #include "Task/Miscellaneous/ScreenshotTaskPlugin.h"
@@ -41,7 +41,7 @@ bool asst::CustomTask::set_params(const json::value& params)
             Log.info("Parsed and registered SecretFront task: ", task_name, " -> ", resolved_task);
         }
         else if (parse_and_register_auto_raise_potential(task_name, params)) {
-            Log.info("Parsed and registered AutoRaisePotential task: ", task_name);
+            LogInfo << "Parsed and registered AutoRaisePotential task:" << task_name;
         }
         else if (parse_and_register_pixel_paint(task_name, params)) {
             Log.info("Parsed and registered PixelPaint task: ", task_name);
@@ -81,7 +81,7 @@ bool asst::CustomTask::parse_and_register_auto_raise_potential(const std::string
 
     auto plugin_ptr = m_custom_task_ptr->register_plugin<AutoRaisePotentialTaskPlugin>();
     if (!plugin_ptr) {
-        Log.error("Failed to register AutoRaisePotentialTaskPlugin");
+        LogError << __FUNCTION__ << "| failed to register AutoRaisePotentialTaskPlugin";
         return false;
     }
 
